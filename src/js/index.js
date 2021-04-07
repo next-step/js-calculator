6858cce8a9bb23dcdf75429c783249740ee175f9
Will <kode15333@gmail.com>
2021-04-07 21:49:29 +0900

    엘리먼트 지정

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 class App {
- constructor () {}
+  constructor (app) {
+    this.app = app;
+    this.$digit = app.querySelector('div.digit');
+    this.$modifier = app.querySelector('div.modifier');
+    this.$operation = app.querySelector('div.operation');
+  }
+
+
 }
 
 new App(document.querySelector('#app'));
