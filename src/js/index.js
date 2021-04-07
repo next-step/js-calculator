c2691c76a42e2ca085d133ad53c74a1ba6466186
Will <kode15333@gmail.com>
2021-04-07 21:52:56 +0900

    린트

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,33 @@
 class App {
   constructor (app) {
-    this.app = app;
-    this.$digit = app.querySelector('div.digit');
-    this.$modifier = app.querySelector('div.modifier');
-    this.$operation = app.querySelector('div.operation');
+    this.app = app
+    this.$digit = app.querySelector('div.digits')
+    this.$modifier = app.querySelector('div.modifiers')
+    this.$operation = app.querySelector('div.operations')
     this.addDomEvent()
   }
 
-  addDomEvent() {
-    this.$digit.addEventListener('click', this.handleDigit);
-    this.$modifier.addEventListener('click', this.handleModifier);
-    this.$operation.addEventListener('click', this.handleOperation);
+  addDomEvent () {
+    this.$digit.addEventListener('click', this.handleDigit)
+    this.$modifier.addEventListener('click', this.handleModifier)
+    this.$operation.addEventListener('click', this.handleOperation)
   }
+
+  handleDigit (e) {
+    console.log(e, this)
+  }
+
+  handleModifier (e) {
+    console.log(e, this)
+
+  }
+
+  handleOperation (e) {
+    console.log(e, this)
+
+  }
+
+
 }
 
-new App(document.querySelector('#app'));
+new App(document.querySelector('#app'))
