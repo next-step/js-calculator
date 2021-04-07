41e7917319367fbd659714a8bbbe5a7d45f9a7c1
Will <kode15333@gmail.com>
2021-04-07 21:50:03 +0900

    이벤트 리스너 등록(handler 미구현)

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,14 @@ class App {
     this.$digit = app.querySelector('div.digit');
     this.$modifier = app.querySelector('div.modifier');
     this.$operation = app.querySelector('div.operation');
+    this.addDomEvent()
   }
 
-
+  addDomEvent() {
+    this.$digit.addEventListener('click', this.handleDigit);
+    this.$modifier.addEventListener('click', this.handleModifier);
+    this.$operation.addEventListener('click', this.handleOperation);
+  }
 }
 
 new App(document.querySelector('#app'));
