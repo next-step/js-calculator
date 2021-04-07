cea4d0241bf9a3d5bab1295b618d33254eb9df19
Will <kode15333@gmail.com>
2021-04-07 23:29:18 +0900

    sum 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 class App {
   constructor (app) {
-    this.app = app;
-    this.result = 0;
-    this.$total = app.querySelector('#total');
+    this.app = app
+    this.result = 0
+    this.digitlen = 0;
+    this.operator = ''
+    this.$total = app.querySelector('#total')
     this.$digit = app.querySelector('div.digits')
     this.$modifier = app.querySelector('div.modifiers')
     this.$operation = app.querySelector('div.operations')
@@ -15,20 +17,48 @@ class App {
     this.$operation.addEventListener('click', this.handleOperation)
   }
 
-  handleDigit (e) {
-    console.log(e, this)
+  handleDigit = (e) => {
+    const value = e.target.textContent
+    let total = this.$total.textContent;
+    total.length === 1 && total === '0' &&  (this.$total.textContent = '')
+    if (this.digitlen < 3) {
+      this.$total.textContent += value;
+      this.digitlen += 1;
+    } else {
+      alert('숫자는 세 자리까지만 입력 가능합니다!')
+    }
+    console.log(e, this, this.$total.textContent)
   }
 
-  handleModifier (e) {
+  handleModifier = () => {
+    this.$total.textContent = 0;
+    this.digitlen = 0;
+  }
+
+  handleOperation = (e) => {
+
+    const oper = e.target.textContent
+    let total = this.$total.textContent;
+    if (oper === '=') {
+      const [first, second] = total.split(this.operator);
+      switch (this.operator) {
+        case 'X' :
+          let sum = ( +first) * (+second)
+          this.$total.textContent = sum
+      }
+      return ;
+    }
+    if (isNaN(total[total.length - 1])) {
+      alert('숫자를 먼저 입력한 후 연산자를 입력해주세요!')
+    } else {
+      this.$total.textContent += oper;
+      this.operator = oper;
+      this.digitlen = 0;
+    }
+
     console.log(e, this)
 
   }
-
-  handleOperation (e) {
-    console.log(e, this)
-
-  }
-
 
 }
 
