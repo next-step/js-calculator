7adfd7a2a24343d8e9712c0af0101894109c8850
Will <kode15333@gmail.com>
2021-04-08 23:21:41 +0900

    리펙토링

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
+const operations = {
+  '+': (a, b) => Number(a) + Number(b),
+  '-': (a, b) => Number(a) - Number(b),
+  'X': (a, b) => Number(a) * Number(b),
+  '/': (a, b) => Math.floor(Number(a) / Number(b)),
+}
+
+const OPERATORS = ['/', 'X', '-', '+']
+
 class App {
   constructor (app) {
-    this.app = app
-    this.result = 0
-    this.digitlen = 0;
-    this.operator = ''
     this.$total = app.querySelector('#total')
     this.$digit = app.querySelector('div.digits')
     this.$modifier = app.querySelector('div.modifiers')
@@ -17,46 +22,51 @@ class App {
     this.$operation.addEventListener('click', this.handleOperation)
   }
 
-  handleDigit = (e) => {
-    const value = e.target.textContent
-    let total = this.$total.textContent;
-    total.length === 1 && total === '0' &&  (this.$total.textContent = '')
-    if (this.digitlen < 3) {
-      this.$total.textContent += value;
-      this.digitlen += 1;
-    } else {
-      alert('숫자는 세 자리까지만 입력 가능합니다!')
+  handleDigit = ({ target }) => {
+    if (!this.isValidLength()) {
+      return alert('숫자는 세 자리까지만 입력 가능합니다!')
     }
-    console.log(e, this, this.$total.textContent)
+    if (this.$total.innerText === '0') {
+      return (this.$total.innerText = target.innerText)
+    }
+    return (this.$total.innerText += target.innerText)
+  }
+
+  isValidLength = () => {
+    const displayValue = this.$total.innerText
+    const operator = displayValue.split('').find((v) => OPERATORS.includes(v))
+
+    if (!operator) {
+      return displayValue.length < 3
+    }
+    return displayValue.split(operator)[1].length < 3
   }
 
   handleModifier = () => {
-    this.$total.textContent = 0;
-    this.digitlen = 0;
+    this.$total.innerText = 0
   }
 
-  handleOperation = (e) => {
-
-    const oper = e.target.textContent
-    let total = this.$total.textContent;
-    if (oper === '=') {
-      const [first, second] = total.split(this.operator);
-      switch (this.operator) {
-        case 'X' :
-          let sum = ( +first) * (+second)
-          this.$total.textContent = sum
-      }
-      return ;
+  handleOperation = ({target}) => {
+    const operateValue = target.innerText
+    const displayValue = this.$total.innerText
+    const operator = displayValue.split('').find((v) => OPERATORS.includes(v))
+    const total = this.$total.innerText
+    if (!operator && operateValue === '=') {
+      return false
     }
-    if (isNaN(total[total.length - 1])) {
+
+    if (total === '0' || isNaN(total[total.length - 1])) {
       alert('숫자를 먼저 입력한 후 연산자를 입력해주세요!')
-    } else {
-      this.$total.textContent += oper;
-      this.operator = oper;
-      this.digitlen = 0;
+      return false
     }
 
-    console.log(e, this)
+    if (operateValue === '=') {
+      const [first = 0, second = 0] = total.split(operator)
+      this.$total.innerText = operations[operator](first, second)
+      return false
+    }
+
+    this.$total.innerText += operateValue
 
   }
 
