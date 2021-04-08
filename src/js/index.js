9872f05b6ee9d5ac2272b77249ee2e622c86825e
jhpark <qkr13wlgus57@gmail.com>
2021-04-08 15:54:11 +0900

    [refactor]

--- src/js/index.js
+++ src/js/index.js
@@ -4,81 +4,72 @@ const total = document.getElementById("total");
 let num = "";
 const calcuList = [];
 
-console.log(digits);
-digits.forEach((digit) => {
-  digit.addEventListener("click", (e) => {
-    if (num.length === 3) {
-      alert("숫자는 세 자리까지만 입력 가능합니다!");
-      return false;
+function onClickDigit(e) {
+  if (num.length === 3) {
+    alert("숫자는 세 자리까지만 입력 가능합니다!");
+    return false;
+  }
+  const input = e.target.innerText;
+
+  if (num === "" && input === "0") {
+    return false;
+  }
+
+  num += input;
+  setTotal(input);
+}
+
+function handleClickEqual() {
+  switch (calcuList.length) {
+    case 1: {
+      total.innerText = calcuList[0];
+      break;
     }
-    const input = e.target.innerText;
-
-    if (num === "" && input === "0") {
-      return false;
-    }
-
-    num += input;
-    setTotal(input);
-  });
-});
-
-operations.forEach((operation) => {
-  operation.addEventListener("click", (e) => {
-    const input = e.target.innerText;
-
-    if (input === "=") {
-      switch (calcuList.length) {
-        case 0: {
-          break;
-        }
-        case 1: {
-          total.innerText = calcuList[0];
-          break;
-        }
-        case 2: {
-          if (num === "") {
-            total.innerText = calcuList[0];
-            break;
-          } else {
-            calcuList.push(parseInt(num));
-            num = "";
-          }
-        }
-        case 3: {
-          const result = calculator();
-          total.innerText = result;
-          num = result;
-          calcuList.length = 0;
-        }
+    case 2: {
+      if (num === "") {
+        total.innerText = calcuList[0];
+        break;
+      } else {
+        calcuList.push(parseInt(num));
+        num = "";
       }
-
-      return false;
     }
-
-    if (num.length === 0) {
-      alert("숫자를 먼저 입력한 후 연산자를 입력해주세요!");
-      return false;
+    case 3: {
+      const result = calculator(...calcuList);
+      total.innerText = result;
+      num = result;
+      calcuList.length = 0;
     }
+  }
+}
 
-    if (calcuList.length === 3 || (calcuList.length === 2 && num !== "")) {
-      alert("2개의 숫자에 대해서만 계산 가능합니다.");
-      return false;
-    }
+function onClickOperation(e) {
+  const input = e.target.innerText;
 
-    calcuList.push(parseInt(num));
-    calcuList.push(input);
-    num = "";
-    setTotal(input);
-  });
-});
+  if (input === "=") {
+    handleClickEqual();
+    return false;
+  }
 
-function calculator() {
-  console.log(calcuList);
-  const first = calcuList[0];
-  const second = calcuList[2];
-  console.log(first, second);
+  if (num.length === 0) {
+    alert("숫자를 먼저 입력한 후 연산자를 입력해주세요!");
+    return false;
+  }
+
+  if (calcuList.length === 3 || (calcuList.length === 2 && num !== "")) {
+    alert("2개의 숫자에 대해서만 계산 가능합니다.");
+    return false;
+  }
+
+  calcuList.push(parseInt(num));
+  calcuList.push(input);
+  num = "";
+  setTotal(input);
+}
+
+function calculator(first, operator, second) {
   let result = 0;
-  switch (calcuList[1]) {
+  switch (operator) {
     case "/": {
       result = Math.floor(first / second);
       break;
@@ -109,5 +100,13 @@ function reset() {
   num = "";
   calcuList.length = 0;
 }
+
+digits.forEach((digit) => {
+  digit.addEventListener("click", onClickDigit);
+});
+
+operations.forEach((operation) => {
+  operation.addEventListener("click", onClickOperation);
+});
 
 document.querySelector(".modifier").addEventListener("click", reset);
