01d9d3a83c1a50247e26f1a8c8585f6824ece56c
jhpark <qkr13wlgus57@gmail.com>
2021-04-08 19:55:00 +0900

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,88 +1,88 @@
-const digits = document.querySelectorAll(".digit");
-const operations = document.querySelectorAll(".operation");
-const total = document.getElementById("total");
-let num = "";
-const calcuList = [];
+const digits = document.querySelectorAll('.digit');
+const operations = document.querySelectorAll('.operation');
+const total = document.getElementById('total');
+let tempNumString = '';
+const calculatorParam = [];
 
 function onClickDigit(e) {
-  if (num.length === 3) {
-    alert("숫자는 세 자리까지만 입력 가능합니다!");
+  if (tempNumString.length === 3) {
+    alert('숫자는 세 자리까지만 입력 가능합니다!');
     return false;
   }
-  const input = e.target.innerText;
+  const inputValue = e.target.innerText;
 
-  if (num === "" && input === "0") {
+  if (tempNumString === '' && inputValue === '0') {
     return false;
   }
 
-  num += input;
-  setTotal(input);
+  tempNumString += inputValue;
+  displayInputValue(inputValue);
 }
 
-function handleClickEqual() {
-  switch (calcuList.length) {
+function getCalculationResult() {
+  switch (calculatorParam.length) {
     case 1: {
-      total.innerText = calcuList[0];
+      total.innerText = calculatorParam[0];
       break;
     }
     case 2: {
-      if (num === "") {
-        total.innerText = calcuList[0];
+      if (tempNumString === '') {
+        total.innerText = calculatorParam[0];
         break;
       } else {
-        calcuList.push(parseInt(num));
-        num = "";
+        calculatorParam.push(parseInt(tempNumString));
+        tempNumString = '';
       }
     }
     case 3: {
-      const result = calculator(...calcuList);
+      const result = calculator(...calculatorParam);
       total.innerText = result;
-      num = result;
-      calcuList.length = 0;
+      tempNumString = result;
+      calculatorParam.length = 0;
     }
   }
 }
 
 function onClickOperation(e) {
-  const input = e.target.innerText;
+  const inputValue = e.target.innerText;
 
-  if (input === "=") {
-    handleClickEqual();
+  if (inputValue === '=') {
+    getCalculationResult();
     return false;
   }
 
-  if (num.length === 0) {
-    alert("숫자를 먼저 입력한 후 연산자를 입력해주세요!");
+  if (tempNumString.length === 0) {
+    alert('숫자를 먼저 입력한 후 연산자를 입력해주세요!');
     return false;
   }
 
-  if (calcuList.length === 3 || (calcuList.length === 2 && num !== "")) {
-    alert("2개의 숫자에 대해서만 계산 가능합니다.");
+  if (calculatorParam.length === 3 || (calculatorParam.length === 2 && num !== '')) {
+    alert('2개의 숫자에 대해서만 계산 가능합니다.');
     return false;
   }
 
-  calcuList.push(parseInt(num));
-  calcuList.push(input);
-  num = "";
-  setTotal(input);
+  calculatorParam.push(parseInt(tempNumString));
+  calculatorParam.push(inputValue);
+  tempNumString = '';
+  displayInputValue(inputValue);
 }
 
 function calculator(first, operator, second) {
   let result = 0;
   switch (operator) {
-    case "/": {
+    case '/': {
       result = Math.floor(first / second);
       break;
     }
-    case "X": {
+    case 'X': {
       result = first * second;
       break;
     }
-    case "-": {
+    case '-': {
       result = first - second;
       break;
     }
-    case "+": {
+    case '+': {
       result = first + second;
       break;
     }
@@ -90,23 +90,27 @@ function calculator(first, operator, second) {
   return result;
 }
 
-function setTotal(text) {
+function displayInputValue(text) {
   const currentTotal = total.innerText;
-  total.innerText = currentTotal === "0" ? text : currentTotal + text;
+  total.innerText = currentTotal === '0' ? text : currentTotal + text;
 }
 
 function reset() {
-  total.innerText = "0";
-  num = "";
-  calcuList.length = 0;
+  total.innerText = '0';
+  tempNumString = '';
+  calculatorParam.length = 0;
 }
 
-digits.forEach((digit) => {
-  digit.addEventListener("click", onClickDigit);
-});
+function init() {
+  digits.forEach((digit) => {
+    digit.addEventListener('click', onClickDigit);
+  });
 
-operations.forEach((operation) => {
-  operation.addEventListener("click", onClickOperation);
-});
+  operations.forEach((operation) => {
+    operation.addEventListener('click', onClickOperation);
+  });
 
-document.querySelector(".modifier").addEventListener("click", reset);
+  document.querySelector('.modifier').addEventListener('click', reset);
+}
+
+init();
