3769debc78de1d30841322fd51629a339fb5252a
skid901 <tlrudejr1993@gmail.com>
2021-04-11 06:17:30 +0900

    test: write tests that fails for requirements

--- cypress/integration/js-calculator.spec.js
+++ cypress/integration/js-calculator.spec.js
@@ -1,21 +1,55 @@
-import {} from '../../src/js/settings';
+import { id, opChr, text, msg } from '../../src/js/settings';
 
 describe('js-calculator', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {});
+  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+    const [cmds, expected] = ['999+999=', 999 + 999];
+    [...cmds].forEach(cmd => cy.clkBtnByTxt(cmd));
+    cy.getById(id.total).should('have.text', expected);
+  });
 
-  it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {});
+  it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+    const [cmds, expected] = ['999-99=', 999 - 99];
+    [...cmds].forEach(cmd => cy.clkBtnByTxt(cmd));
+    cy.getById(id.total).should('have.text', expected);
+  });
 
-  it('2개의 숫자에 대해 곱셈이 가능하다.', () => {});
+  it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+    const [cmds, expected] = [`999${opChr.multi}999=`, 999 * 999];
+    [...cmds].forEach(cmd => cy.clkBtnByTxt(cmd));
+    cy.getById(id.total).should('have.text', expected);
+  });
 
-  it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {});
+  it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+    const [cmds, expected] = ['999/9=', 999 / 9];
+    [...cmds].forEach(cmd => cy.clkBtnByTxt(cmd));
+    cy.getById(id.total).should('have.text', expected);
+  });
 
-  it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {});
+  it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+    const cmds = '999';
+    [...cmds].forEach(cmd => cy.clkBtnByTxt(cmd));
+    cy.getById(id.total).should('have.text', cmds);
+    cy.clkBtnByTxt(text.ac);
+    cy.getById(id.total).should('have.text', 0);
+  });
 
-  it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {});
+  it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
+    const [cmds, stub] = ['999', cy.stub()];
+    cy.on('window:alert', stub);
+    [...cmds].forEach(cmd => cy.clkBtnByTxt(cmd));
+    cy.clkBtnByTxt(9).then(() => {
+      expect(stub.getCall(0)).to.be.calledWith(msg.alert);
+      cy.getById(id.total).should('have.text', cmds);
+    });
+  });
 
-  it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {});
+  it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+    const [cmds, expected] = ['999/99=', (999 / 99) ^ 0];
+    [...cmds].forEach(cmd => cy.clkBtnByTxt(cmd));
+    cy.getById(id.total).should('have.text', expected);
+  });
 });
--- src/js/settings.js
+++ src/js/settings.js
@@ -17,6 +17,9 @@ const SETTINGS = {
     divi: '/',
     eq: '=',
   },
+  msg: {
+    alert: '숫자는 세 자리까지만 입력 가능합니다!',
+  },
 };
 
 export default SETTINGS;
