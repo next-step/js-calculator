c3aab2514ec3066eaa49fc46377b6cc99ad0e493
yujo <bedro27@gmail.com>
2021-04-10 18:47:36 +0900

    test: 가독성을 위한 함수의 역할 축소

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -6,61 +6,67 @@ describe('calculator', () => {
     cy.visit('http://localhost:8080/');
   });
 
-  const clickNumberAndCalculation = ({ num1 = '999', num2 = '999', operator = '' }) => {
-    if (!operator) {
-      throw Error(MESSAGE.NEED_OPERATOR);
-    }
-
-    num1.split('').map((number) => {
-      cy.get('.digits').contains(number).click();
-    });
-    cy.contains(operator).click();
-    num2.split('').map((number) => {
-      cy.get('.digits').contains(number).click();
-    });
-    cy.contains('=').click();
+  const clickNumber = (number = 999) => {
+    String(number)
+      .split('')
+      .map((num) => cy.get('.digits').contains(num).click());
   };
 
   it('2개의 숫자에 대해 덧셈이 가능해야 한다.', () => {
-    clickNumberAndCalculation({ operator: PLUS });
+    clickNumber(999);
+    cy.contains(PLUS).click();
+    clickNumber(999);
+    cy.contains('=').click();
+
     cy.get('#total').should('have.text', calculation({ num1: 999, num2: 999, operator: PLUS }));
   });
 
   it('2개의 숫자에 대해 뺄셈이 가능해야 한다.', () => {
-    clickNumberAndCalculation({ operator: MINUS });
+    clickNumber(999);
+    cy.contains(MINUS).click();
+    clickNumber(999);
+    cy.contains('=').click();
+
     cy.get('#total').should('have.text', calculation({ num1: 999, num2: 999, operator: MINUS }));
   });
 
   it('2개의 숫자에 대해 곱셈이 가능해야 한다.', () => {
-    clickNumberAndCalculation({ operator: MULTIPLICATION });
+    clickNumber(999);
+    cy.contains(MULTIPLICATION).click();
+    clickNumber(999);
+    cy.contains('=').click();
+
     cy.get('#total').should('have.text', calculation({ num1: 999, num2: 999, operator: MULTIPLICATION }));
   });
 
   it('2개의 숫자에 대해 나눗셈이 가능해야 한다.', () => {
-    clickNumberAndCalculation({ operator: DIVISION });
+    clickNumber(999);
+    cy.contains(DIVISION).click();
+    clickNumber(999);
+    cy.contains('=').click();
+
     cy.get('#total').should('have.text', calculation({ num1: 999, num2: 999, operator: DIVISION }));
   });
 
   it('나눗셈을 할 때 소수점 이하는 버려야 한다.', () => {
-    clickNumberAndCalculation({ num2: '8', operator: DIVISION });
+    clickNumber(999);
+    cy.contains(DIVISION).click();
+    clickNumber(8);
+    cy.contains('=').click();
+
     cy.get('#total').should('have.text', Math.round(999 / 8));
   });
 
   it('숫자는 3자리까지만 입력이 가능해야 한다.', () => {
     cy.window().then((win) => cy.stub(win, 'alert').as('windowAlert'));
 
-    cy.get('.digits').contains('9').click();
-    cy.get('.digits').contains('9').click();
-    cy.get('.digits').contains('9').click();
-    cy.get('.digits').contains('9').click();
+    clickNumber(9999);
 
     cy.get('@windowAlert').should('be.calledWith', MESSAGE.INVALID_DIGIT_LENGTH);
   });
 
   it('"AC" 버튼을 클릭했을 때 결과화면이 0으로 초기화 되어야 한다.', () => {
-    cy.get('.digits').contains('9').click();
-    cy.get('.digits').contains('9').click();
-    cy.get('.digits').contains('9').click();
+    clickNumber(999);
     cy.contains('AC').click();
 
     cy.get('#total').should('have.text', INITIAL_VALUE);
