da63dfb3750719b9315bd33a8b87cfcc07667100
yujo <bedro27@gmail.com>
2021-04-11 21:39:23 +0900

    test: 0으로 나누는 경우에 대한 테스트 작성

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -48,13 +48,22 @@ describe('calculator', () => {
     cy.get('#total').should('have.text', calculation({ num1: 999, num2: 999, operator: DIVISION }));
   });
 
+  it('0으로 나눴을 때 "숫자 아님"이 DISPLAY에 표시되어야 한다.', () => {
+    clickNumber(999);
+    cy.contains(DIVISION).click();
+    clickNumber(0);
+    cy.contains('=').click();
+
+    cy.get('#total').should('have.text', MESSAGE.IS_NOT_NUMBER);
+  });
+
   it('나눗셈을 할 때 소수점 이하는 버려야 한다.', () => {
     clickNumber(999);
     cy.contains(DIVISION).click();
     clickNumber(8);
     cy.contains('=').click();
 
-    cy.get('#total').should('have.text', Math.round(999 / 8));
+    cy.get('#total').should('have.text', Math.floor(999 / 8));
   });
 
   it('숫자는 3자리까지만 입력이 가능해야 한다.', () => {
