6d1ba3c105c94bf0ddc63496fdbd5e9c23e554ee
tiaz0128 <tiaz0128.dev@gmail.com>
2021-04-11 16:28:18 +0900

    기능 추가 : 숫자가 0인 경우 0을 눌러도 0으로 표시

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -7,15 +7,17 @@ export function App($app) {
     $app.appendChild(this.$calculatorDom)
 
     this.state = {
-      firstNumber: 0,
-      secondNumber: 0,
-      totalNumber: 0,
+      firstNumber: '0',
+      secondNumber: '0',
+      totalNumber: '0',
     }
 
     this.calculator = new Calculator({
       $calculatorDom: this.$calculatorDom,
       initState: this.state,
       onDigitsClick: (e) => {
+        if (this.state.totalNumber === '0' && e.target.innerText === '0') return
+
         this.setState({ totalNumber: this.state.totalNumber + e.target.innerText })
       },
     })
