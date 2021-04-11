64e488bd9fd3ce587616cd3f3182fc29a7142d7d
tiaz0128 <tiaz0128.dev@gmail.com>
2021-04-11 23:49:38 +0900

    기능 요구사항 - 테스트 추가 : 2개의 숫자에 대해 곱셈이 가능

--- cypress/integration/calculatorTest.spec.js
+++ cypress/integration/calculatorTest.spec.js
@@ -43,8 +43,15 @@ describe('계산기 테스트', () => {
 
   // 기능 요구사항
   // - 2개의 숫자에 대해 뺄셈이 가능
-  it.only('2개의 숫자에 대해 뺄셈이 가능.', () => {
+  it('2개의 숫자에 대해 뺄셈이 가능.', () => {
     cy.inputRandomNumericalExpression('-', 4)
+    cy.calculateNumericalExpression()
+  })
+
+  // 기능 요구사항
+  // - 2개의 숫자에 대해 곱셈이 가능
+  it.only('2개의 숫자에 대해 곱셈이 가능.', () => {
+    cy.inputRandomNumericalExpression('X', 4)
     cy.calculateNumericalExpression()
   })
 })
