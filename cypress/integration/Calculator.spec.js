445f221f80e6d521a8778fcd51a143c0d6451040
YimJiYoung <chiy323@gmail.com>
2021-04-11 14:03:07 +0900

    test: cypress 설치 및 테스트 코드 작성

---
+++ cypress/integration/Calculator.spec.js
@@ -0,0 +1,60 @@
+describe('js-calculator', () => {
+    beforeEach(() => {
+        cy.visit('http://127.0.0.1:5501/js-calculator/')
+    })
+
+    it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+        cy.get('.digit').contains('4').click();
+        cy.get('.operation').contains('+').click();
+        cy.get('.digit').contains('2').click();
+        cy.get('.operation').contains('=').click();
+        cy.get('#total').should('have.text', '6');
+    });
+
+    it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+        cy.get('.digit').contains('4').click();
+        cy.get('.operation').contains('-').click();
+        cy.get('.digit').contains('2').click();
+        cy.get('.operation').contains('=').click();
+        cy.get('#total').should('have.text', '2');
+    });
+
+    it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+        cy.get('.digit').contains('4').click();
+        cy.get('.operation').contains('X').click();
+        cy.get('.digit').contains('2').click();
+        cy.get('.operation').contains('=').click();
+        cy.get('#total').should('have.text', '8');
+    });
+
+    it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+        cy.get('.digit').contains('4').click();
+        cy.get('.operation').contains('/').click();
+        cy.get('.digit').contains('2').click();
+        cy.get('.operation').contains('=').click();
+        cy.get('#total').should('have.text', '2');
+    });
+
+    it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+        cy.get('.digit').contains('1').click();
+        cy.get('.modifier').contains('AC').click();
+        cy.get('#total').should('have.text', '0');
+    })
+
+    it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
+        cy.get('.digit').contains('1').click();
+        cy.get('.digit').contains('2').click();
+        cy.get('.digit').contains('3').click();
+        cy.get('.digit').contains('4').click();
+        cy.get('#total').should('have.text', '123');
+    });
+
+    it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+        cy.get('.digit').contains('1').click();
+        cy.get('.digit').contains('0').click();
+        cy.get('.operation').contains('/').click();
+        cy.get('.digit').contains('3').click();
+        cy.get('.operation').contains('=').click();
+        cy.get('#total').should('have.text', '3');
+    });
+})
