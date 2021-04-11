812114116c6b4ccab8a638d661c21ebe0966e56f
tiaz0128 <tiaz0128.dev@gmail.com>
2021-04-11 16:18:47 +0900

    테스트 추가 : 숫자가 0인 경우 0을 계속 눌러도 0으로 표시한다.

--- cypress/integration/calculatorTest.spec.js
+++ cypress/integration/calculatorTest.spec.js
@@ -11,4 +11,13 @@ describe('계산기 테스트', () => {
     cy.get('.digit').click({ multiple: true })
     cy.get('#total').should('contain', '09876543210')
   })
+
+  // 숫자가 0인 경우 0을 계속 눌러도 0으로 표시한다.
+  it('숫자를 누르면 h1#total에 들어간다.', () => {
+    times(10, () => {
+      cy.get('.digit:nth-child(10)').click()
+    })
+
+    cy.get('#total').should('include.text').eq('0')
+  })
 })
