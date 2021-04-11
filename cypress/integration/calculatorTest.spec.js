b73243ab07139bda1ef4e4594491af14a6d71eef
tiaz0128 <tiaz0128.dev@gmail.com>
2021-04-11 16:01:03 +0900

    테스트 수정 : .digit 을 누르면 숫자가 h1#total에 들어간다.

--- cypress/integration/calculatorTest.spec.js
+++ cypress/integration/calculatorTest.spec.js
@@ -9,6 +9,6 @@ describe('계산기 테스트', () => {
   // 숫자를 누르면 h1#total에 들어간다.
   it('숫자를 누르면 h1#total에 들어간다.', () => {
     cy.get('.digit').click({ multiple: true })
-    cy.get('#total').should('contain', '1234567890')
+    cy.get('#total').should('contain', '09876543210')
   })
 })
