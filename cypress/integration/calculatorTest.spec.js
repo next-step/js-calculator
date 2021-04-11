b2b50d3b37068bcdd62b756104963faa962054da
tiaz0128 <tiaz0128.dev@gmail.com>
2021-04-11 16:38:37 +0900

    테스트 추가 : 초기값 숫자가 0인 경우 0이 아닌 숫자를 누르면 제일 앞에 0을 지우고 변경하기

--- cypress/integration/calculatorTest.spec.js
+++ cypress/integration/calculatorTest.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import { times } from 'lodash'
+import { times, random } from 'lodash'
 
 describe('계산기 테스트', () => {
   beforeEach(() => {
@@ -15,11 +15,19 @@ describe('계산기 테스트', () => {
   })
 
   // 초기값 숫자가 0인 경우 0을 계속 눌러도 0으로 표시한다.
-  it.only('초기값 숫자가 0인 경우 0을 계속 눌러도 0으로 표시한다.', () => {
+  it('초기값 숫자가 0인 경우 0을 계속 눌러도 0으로 표시한다.', () => {
     times(10, () => {
       cy.get('.digit:nth-child(10)').click()
     })
 
     cy.get('#total').contains('0')
+  })
+
+  // 초기값 숫자가 0인 경우 0이 아닌 숫자를 누르면 제일 앞에 0을 지우고 변경하기
+  it.only('초기값 숫자가 0인 경우 0이 아닌 숫자를 누르면 제일 앞에 0을 지우고 변경하기', () => {
+    cy.get('.digit').then((ele) => {
+      ele[random(0, 9)].click()
+    })
+    cy.get('#total').contains(/^[0-9]$/gim)
   })
 })
