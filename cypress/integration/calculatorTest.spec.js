1c61e3254c813dd65381abec63cbbddd4f55ef3c
tiaz0128 <tiaz0128.dev@gmail.com>
2021-04-11 16:28:48 +0900

    테스트 수정 : contains로 변경 테스트명 변경
    - should 명령어에서 contains로 변경
    - 초기값 숫자가 0인 경우 0을 계속 눌러도 0으로 표시한다.

--- cypress/integration/calculatorTest.spec.js
+++ cypress/integration/calculatorTest.spec.js
@@ -1,23 +1,25 @@
 /// <reference types="cypress" />
 
+import { times } from 'lodash'
+
 describe('계산기 테스트', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5500/index.html')
-    cy.get('#total').should('contain', '0')
+    cy.get('#total').contains('0')
   })
 
   // 숫자를 누르면 h1#total에 들어간다.
   it('숫자를 누르면 h1#total에 들어간다.', () => {
     cy.get('.digit').click({ multiple: true })
-    cy.get('#total').should('contain', '09876543210')
+    cy.get('#total').contains('09876543210')
   })
 
-  // 숫자가 0인 경우 0을 계속 눌러도 0으로 표시한다.
-  it('숫자를 누르면 h1#total에 들어간다.', () => {
+  // 초기값 숫자가 0인 경우 0을 계속 눌러도 0으로 표시한다.
+  it.only('초기값 숫자가 0인 경우 0을 계속 눌러도 0으로 표시한다.', () => {
     times(10, () => {
       cy.get('.digit:nth-child(10)').click()
     })
 
-    cy.get('#total').should('include.text').eq('0')
+    cy.get('#total').contains('0')
   })
 })
