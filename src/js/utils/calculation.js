5e48adb36abe7089b3fb50bcbc4580f0295ab2a1
yujo <bedro27@gmail.com>
2021-04-11 21:38:06 +0900

    fix: 나눗셈 연산시 소수점을 버리지 않고 반올림 하던 오류

--- src/js/utils/calculation.js
+++ src/js/utils/calculation.js
@@ -17,7 +17,7 @@ export const calculation = ({ num1 = 0, num2 = 0, operator = '' }) => {
     [PLUS]: () => Number(num1) + Number(num2),
     [MINUS]: () => Number(num1) - Number(num2),
     [MULTIPLICATION]: () => Number(num1) * Number(num2),
-    [DIVISION]: () => Math.round(num1 / num2),
+    [DIVISION]: () => Math.floor(num1 / num2),
   };
 
   return operators[operator]?.();
