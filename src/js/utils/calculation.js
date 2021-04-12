f450f47c861a79fa993470509d786a0c985def4f
yujo <bedro27@gmail.com>
2021-04-12 22:33:52 +0900

    refactor: 예외처리 로직을 통합해 코드의 응집도를 높임

--- src/js/utils/calculation.js
+++ src/js/utils/calculation.js
@@ -1,7 +1,7 @@
-import { PLUS, MINUS, MULTIPLICATION, DIVISION, MESSAGE } from './constant';
+import { PLUS, MINUS, MULTIPLICATION, DIVISION, MESSAGE, OPERATORS } from './constant';
 
 export const calculation = ({ num1 = 0, num2 = 0, operator = '' }) => {
-  if (!operator) {
+  if (!OPERATORS.includes(operator)) {
     throw Error(MESSAGE.NEED_OPERATOR);
   }
 
@@ -9,16 +9,12 @@ export const calculation = ({ num1 = 0, num2 = 0, operator = '' }) => {
     throw Error(MESSAGE.CHECK_INPUT_TYPE);
   }
 
-  if (operator === DIVISION && num2 == 0) {
-    return MESSAGE.IS_NOT_NUMBER;
-  }
-
   const operators = {
     [PLUS]: () => Number(num1) + Number(num2),
     [MINUS]: () => Number(num1) - Number(num2),
     [MULTIPLICATION]: () => Number(num1) * Number(num2),
-    [DIVISION]: () => Math.floor(Number(num1) / Number(num2)),
+    [DIVISION]: () => (num2 == 0 ? MESSAGE.IS_NOT_NUMBER : Math.floor(num1 / num2)),
   };
 
-  return operators[operator]?.();
+  return operators[operator]();
 };
