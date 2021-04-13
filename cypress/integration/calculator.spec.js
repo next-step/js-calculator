ba0b0b1897c61d2044219bc549dfe732d6a2df17
wnsrl56 <wnsrl56@gmail.com>
2021-04-13 13:15:15 +0900

    test: 7개의 테스트 케이스 작성

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -2,83 +2,117 @@ describe('ui-calculator', () => {
   beforeEach(() => {
     // run live server
     cy.visit('http://localhost:5500/');
+    cy.get('button').contains(`AC`).click();
   });
 
-  it('숫자를 클릭 시, 디스플레이에 표기됩니다.', () => {
-    const nums = [0,1,2,3,4,5,6,7,8,9];
-    nums.forEach((num) => {
-      cy.get('button').contains(`AC`).click(); 
+  afterEach(() => {
+    cy.get('button').contains(`AC`).click();
+  })
+
+  const runOperation = (op1, operator, op2) => {
+    op1.forEach((num) => {
       cy.get('button').contains(`${num}`).click();
-      cy.get('#total').should('have.text', `${num}`);
     });
+
+    cy.get('.operations').contains(operator).click();
+
+    op2.forEach((num) => {
+      cy.get('button').contains(`${num}`).click();
+    });
+    
+    cy.get('.operations').contains('=').click();
+  }
+  
+  const getResult = (op1, operator, op2) => {
+    let res;
+
+    switch(operator) {
+      case '+': res = op1 + op2; break;
+      case '-': res = op1 - op2; break;
+      case 'X': res = op1 * op2; break;
+      case '/': res = Math.floor(op1 / op2); break;
+    }
+
+    return res;
+  }
+
+  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+    const op1 = ['4', '3', '2'];
+    const op2 = ['5', '6', '1'];
+    const operator = '+';
+
+    runOperation(op1, operator, op2);
+    const res = getResult(Number(op1.join('')), operator, Number(op2.join('')));
+
+    cy.get('#total').should('have.text', res);
   });
 
-  it('숫자는 세 자리까지 입력 가능합니다.', () => {
+  it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+    const op1 = ['4', '3', '2'];
+    const op2 = ['5', '6', '1'];
+    const operator = 'X';
+
+    runOperation(op1, operator, op2);
+    const res = getResult(Number(op1.join('')), operator, Number(op2.join('')));
+
+    cy.get('#total').should('have.text', res);
+  });
+
+  it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+    const op1 = ['4', '3', '2'];
+    const op2 = ['5'];
+    const operator = '/';
+
+    runOperation(op1, operator, op2);
+    const res = getResult(Number(op1.join('')), operator, Number(op2.join('')));
+
+    cy.get('#total').should('have.text', res);
+  });
+
+  it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+    const op1 = ['4', '3', '2'];
+    const op2 = ['1', '5', '4'];
+    const operator = '-';
+
+    runOperation(op1, operator, op2);
+    const res = getResult(Number(op1.join('')), operator, Number(op2.join('')));
+
+    cy.get('#total').should('have.text', res);
+  });
+
+  it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+    const op1 = ['4', '3', '2'];
+    const op2 = ['1', '5', '4'];
+    const operator = '+';
+
+    runOperation(op1, operator, op2);
+    cy.get('button').contains(`AC`).click();
+    cy.get('#total').should('have.text', '0');
+  });
+
+  it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
     const errorMsg = '숫자는 세 자리까지 입력 가능합니다!';
 
     cy.get('button').contains(`1`).click();
     cy.get('button').contains(`1`).click();
     cy.get('button').contains(`1`).click();
-    
+
     const stub = cy.stub();
-    cy.on ('window:alert', stub);
+    cy.on('window:alert', stub);
     cy.get('button').contains('1').click()
-    .then(() => {
-      expect(stub.getCall(0)).to.be.calledWith(errorMsg);      
-    });
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(errorMsg);
+        });
   });
 
-  it('숫자 입력 후에 연산자를 입력 가능합니다.', () => {
-    const errorMsg = '숫자를 먼저 입력한 후 연산자를 입력해주세요!';
-    const operations = ['+' , '-', '/', 'X' ,'='];
+  it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+    const op1 = ['4', '3', '1'];
+    const op2 = ['3'];
+    const operator = '/';
 
-    operations.forEach((oper) => {
-      const stub = cy.stub();
-      cy.on ('window:alert', stub);
-      cy.get('button').contains(oper).click()
-      .then(() => {
-        expect(stub.getCall(0)).to.be.calledWith(errorMsg);      
-      });
-    });
+    runOperation(op1, operator, op2);
+    let res = getResult(Number(op1.join('')), operator, Number(op2.join('')));
+
+    cy.get('#total').should('have.text', res);
   });
-
-  // it('숫자를 10~99 까지 고를 수 있습니다.', () => {
-  //   let count = 100;
-  //   while(count-- > 10) {
-  //     cy.get('.modifier').contains(`AC`).click(); 
-  //     let nums = String(count).split('');
-  //     cy.get('.digit').contains(`${nums[0]}`).click();
-  //     cy.get('.digit').contains(`${nums[1]}`).click();              
-  //     cy.get('#total').should('have.text', `${count}`);
-  //   }
-  // });
-
-  // it('숫자를 100~999 까지 고를 수 있습니다.', () => {
-  //   let count = 1000;
-  //   while(count-- > 100) {
-  //     cy.get('.modifier').contains(`AC`).click(); 
-  //     let nums = String(count).split('');
-  //     cy.get('.digit').contains(`${nums[0]}`).click();
-  //     cy.get('.digit').contains(`${nums[1]}`).click();
-  //     cy.get('.digit').contains(`${nums[2]}`).click();              
-  //     cy.get('#total').should('have.text', `${count}`);
-  //   }
-  // });
-
-  it('최대 3자리 수 2개를 더합니다.', () => {
-    
-  });
-
-  it('최대 3자리 수 2개를 곱합니다.', () => {
-
-  });
-
-  it('최대 3자리 수 2개를 뺍니다.', () => {
-
-  });
-
-  it('최대 3자리 수 2개를 나눕니다.', () => {
-
-  });
-
-})
+});
