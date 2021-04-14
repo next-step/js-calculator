d7b0838dd4e52ac97d9d0b7dc9c103d4dec78026
HJ-Woo <hjwoou@gmail.com>
2021-04-14 19:39:07 +0900

    feat:계산결과를 표현할때 소수점 이하는 버린다 test: 두자리수 계산 추가

--- src/js/component/operator.js
+++ src/js/component/operator.js
@@ -5,7 +5,7 @@ export default function Operator(operator, operand1, operand2) {
     '+': () => op1 + op2,
     '-': () => op1 - op2,
     'X': () => op1 * op2, 
-    '/': () => op1 / op2
+    '/': () => Math.floor(op1 / op2)
   };
 
   return operators[operator]();
