9abdd69bfec0ee5aaf8a9effba50e61602bb7087
sujieim <sujieim@gmail.com>
2021-04-17 23:40:31 +0900

    refactor: operations to lambda

--- src/js/util.js
+++ src/js/util.js
@@ -1,16 +1,8 @@
 const OPERATIONS = {
-  '+': function (x, y) {
-    return x + y;
-  },
-  '-': function (x, y) {
-    return x - y;
-  },
-  X: function (x, y) {
-    return x * y;
-  },
-  '/': function (x, y) {
-    return Math.trunc(x / y);
-  },
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  X: (x, y) => x * y,
+  '/': (x, y) => Math.trunc(x / y),
 };
 
 export { OPERATIONS };
