3183d2a2b321fd598e1d275f424e1576b7328455
Sanghyeok Nam <sanam@c3r3s7.42seoul.kr>
2021-05-28 18:34:21 +0900

    feat: cypress.json의 env에 상수 저장 및 사용

--- cypress/integration/actions.spec.js
+++ cypress/integration/actions.spec.js
@@ -1,50 +1,48 @@
 describe('calculator test! hahaha', () => {
+  const REPEAT = Cypress.env('REPEAT');
   beforeEach(() => {
     cy.visit('/');
   })
 
   it('ac버튼을 누르면 0 으로 초기화 된다.', () => {
-    cy.allClearTest(5);
+    cy.allClearTest(REPEAT);
   })
 
   it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
-    const stub = cy.stub();
-    cy.on('window:alert', stub);
-    cy.clickDigits(100);
-    cy.get(`[data-cy=1]`)
-      .click()
-      .then(() => {
-        expect(stub.getCall(0)).to.be.calledWith('no more 3');
-      })
+    for (let i = 0; i <  REPEAT; i++) {
+      const stub = cy.stub();
+      const msg = Cypress.env('ALERT')['OVER'];
+      cy.on('window:alert', stub);
+      cy.clickDigits(100);
+      cy.get(`[data-cy=1]`)
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(msg);
+        })
+    }
   })
   
   it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
-    cy.clickDigits(1);
-    cy.clickOp('/');
-    cy.clickDigits(6);
-    cy.run();
-    cy.get('#total').should('have.text', 0);
+    for (let i = 0; i <  REPEAT; i++) {
+      cy.clickDigits(1);
+      cy.clickOp('/');
+      cy.clickDigits(6);
+      cy.run();
+      cy.get('#total').should('have.text', 0);
+    }
   })
 
- it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
-    const stub = cy.stub();
-    cy.on('window:alert', stub);
-    cy.clickDigits(100);
-    cy.get(`[data-cy=1]`)
-      .click()
-      .then(() => {
-        expect(stub.getCall(0)).to.be.calledWith('no more 3');
-      })
-  })
-  
   it('연속해서 연산자를 클릭하지 못한다.', () => {
-    cy.clear();
-    const stub = cy.stub();
-    cy.on('window:alert', stub);
-    cy.clickOp();
-    cy.clickOp()
-      .then(() => {
-        expect(stub.getCall(0)).to.be.calledWith('숫자를 먼저 입력한 후 연산자를 입력해주세요!');
-      })
+    for (let i = 0; i <  REPEAT; i++) {
+      cy.clear();
+      const msg = Cypress.env('ALERT')['DIGIT_FIRST'];
+      const stub = cy.stub();
+      cy.on('window:alert', stub);
+      cy.clickOp();
+      cy.clickOp()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(msg)
+        })
+    }
   })
 })
--- cypress/integration/artithmatic.spec.js
+++ cypress/integration/artithmatic.spec.js
@@ -1,21 +1,22 @@
 describe('사칙 연산 테스트', () => {
+  const REPEAT = Cypress.env('REPEAT');
   beforeEach(() => {
     cy.visit('/');
   })
 
   it('2개의 숫자에 대해 덧셈이 가능하다', () => {
-    cy.plusTest(1);
+    cy.plusTest(REPEAT);
   })
 
   it('2개의 숫자에 대해 뺄셈이 가능하다', () => {
-    cy.minusTest(1);
+    cy.minusTest(REPEAT);
   })
 
   it('2개의 숫자에 대해 곱셈이 가능하다', () => {
-    cy.multiplyTest(1);
+    cy.multiplyTest(REPEAT);
   })
 
   it('2개의 숫자에 대해 나눗셈이 가능하다', () => {
-    cy.divideTest(3);
+    cy.divideTest(REPEAT);
   })
 });
