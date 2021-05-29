539ccaabbfcdac0be4e4fbaabb8571e065527881
kimjanghu <kjh921030@gmail.com>
2021-05-29 21:08:45 +0900

    feat: Add operation object

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,10 @@
 const $ = (el) => document.querySelector(el)
 
-export { $ }
+const operation = {
+  '+': (a, b) => Number(a) + Number(b),
+  '-': (a, b) => Number(a) - Number(b),
+  'X': (a, b) => Number(a) * Number(b),
+  '/': (a, b) => ~~(Number(a) / Number(b))
+}
+
+export { $, operation }
