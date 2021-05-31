5aa7ecb51042dcd6361129cec7f98ffd279af86f
inhyuck <dlsgur1447@gmail.com>
2021-05-31 20:56:02 +0900

    feat: 연산자에 따른 연산 처리

--- src/js/Calculator.js
+++ src/js/Calculator.js
@@ -1,37 +1,42 @@
+import {DIGIT, OPERATIONS} from './consts.js';
+
 export default function Calculator({$el}) {
 
+    /**
+     * @type {{operation: string, enteredDigits: number[]}}
+     */
     const state = {
         enteredDigits: [],
         operation: '',
     };
 
     const render = () => {
-        const displayNumber = getDisplayNumber(state.enteredDigits);
+        const displayNumber = getDisplayNumber(state);
 
         $el.innerHTML = `
             <div class="calculator">
                 <h1 id="total">${displayNumber}</h1>
                 <div class="digits flex" data-click="digits">
-                    <button class="digit" value="9">9</button>
-                    <button class="digit" value="8">8</button>
-                    <button class="digit" value="7">7</button>
-                    <button class="digit" value="6">6</button>
-                    <button class="digit" value="5">5</button>
-                    <button class="digit" value="4">4</button>
-                    <button class="digit" value="3">3</button>
-                    <button class="digit" value="2">2</button>
-                    <button class="digit" value="1">1</button>
-                    <button class="digit" value="0">0</button>
+                    <button class="digit" value="${DIGIT.NINE}">${DIGIT.NINE}</button>
+                    <button class="digit" value="${DIGIT.EIGHT}">${DIGIT.EIGHT}</button>
+                    <button class="digit" value="${DIGIT.SEVEN}">${DIGIT.SEVEN}</button>
+                    <button class="digit" value="${DIGIT.SIX}">${DIGIT.SIX}</button>
+                    <button class="digit" value="${DIGIT.FIVE}">${DIGIT.FIVE}</button>
+                    <button class="digit" value="${DIGIT.FOUR}">${DIGIT.FOUR}</button>
+                    <button class="digit" value="${DIGIT.THREE}">${DIGIT.THREE}</button>
+                    <button class="digit" value="${DIGIT.TWO}">${DIGIT.TWO}</button>
+                    <button class="digit" value="${DIGIT.ONE}">${DIGIT.ONE}</button>
+                    <button class="digit" value="${DIGIT.ZERO}">${DIGIT.ZERO}</button>
                 </div>
                 <div class="modifiers subgrid" data-click="modifiers">
                     <button class="modifier">AC</button>
                 </div>
                 <div class="operations subgrid" data-click="operations">
-                    <button class="operation">/</button>
-                    <button class="operation">X</button>
-                    <button class="operation">-</button>
-                    <button class="operation">+</button>
-                    <button class="operation">=</button>
+                    <button class="operation" value="${OPERATIONS.DIVIDE}">${OPERATIONS.DIVIDE}</button>
+                    <button class="operation" value="${OPERATIONS.MULTIPLY}">${OPERATIONS.MULTIPLY}</button>
+                    <button class="operation" value="${OPERATIONS.MINUS}">${OPERATIONS.MINUS}</button>
+                    <button class="operation" value="${OPERATIONS.PLUS}">${OPERATIONS.PLUS}</button>
+                    <button class="operation" value="${OPERATIONS.CALCULATE}">${OPERATIONS.CALCULATE}</button>
                 </div>
             </div>
         `;
@@ -42,34 +47,89 @@ export default function Calculator({$el}) {
             if (target.closest('[data-click="digits"]')) {
                 enterDigit(target.value);
             }
+
+            if (target.closest('[data-click="modifiers"]')) {
+                clearDigit();
+            }
+
+            if (target.closest('[data-click="operations"]')) {
+                enterOperation(target.value);
+            }
         });
     };
 
     const enterDigit = (digit) => {
-        const {enteredDigits} = state;
-        if (enteredDigits.length === 0) {
-            enteredDigits.push(digit);
-            render();
+        const {enteredDigits, operation} = state;
+
+        const nowEnteredIndex = !operation ? 0 : 1;
+        if (!enteredDigits[nowEnteredIndex]) {
+            enteredDigits.push(0);
+        }
+        if (String(enteredDigits[nowEnteredIndex]).length > 2) {
+            alert('3자리 숫자까지만 입력 가능합니다.');
             return;
         }
-
-        if (enteredDigits[enteredDigits.length - 1].length + 1 > 3) {
-            alert('3자리를 초과할 수 없습니다.');
-            return;
-        }
-
-        enteredDigits[enteredDigits.length - 1] += digit;
+        enteredDigits[nowEnteredIndex] = parseInt(enteredDigits[nowEnteredIndex] + digit);
         render();
+    };
+
+    const clearDigit = () => {
+        state.enteredDigits = [];
+        state.operation = '';
+        render();
+    };
+
+    const enterOperation = (operation) => {
+        if (operation !== OPERATIONS.CALCULATE) {
+            state.operation = operation;
+            return;
+        }
+        if (!state.operation) {
+            return;
+        }
+
+        try {
+            state.enteredDigits = [computeOperation(state)];
+        } catch ({message}) {
+            alert(message);
+            return;
+        }
+        state.operation = '';
+        render();
+    };
+
+    const computeOperation = ({enteredDigits, operation}) => {
+        const [first, second] = enteredDigits;
+
+        switch (operation) {
+            case OPERATIONS.DIVIDE:
+                if (second === 0) {
+                    throw new Error('0으로 나눌 수 없습니다.');
+                }
+                return Math.floor(first / second);
+            case OPERATIONS.MULTIPLY:
+                return first * second;
+            case OPERATIONS.PLUS:
+                return first + second;
+            case OPERATIONS.MINUS:
+                return first - second;
+        }
+    };
+
+
+    /**
+     * 화면에 표시되는 숫자
+     * @param {array} enteredDigits
+     * @returns {number|*}
+     */
+    const getDisplayNumber = ({enteredDigits, operation}) => {
+        if (enteredDigits.length === 0) {
+            return 0;
+        }
+        const nowEnteredIndex = !operation ? 0 : 1;
+        return enteredDigits[nowEnteredIndex];
     };
 
     render();
     bindEvents();
 }
-
-const getDisplayNumber = (enteredDigits) => {
-    if (enteredDigits.length === 0) {
-        return 0;
-    }
-
-    return enteredDigits[enteredDigits.length - 1];
-};
---
+++ src/js/consts.js
@@ -0,0 +1,20 @@
+export const DIGIT = {
+    ZERO: 0,
+    ONE: 1,
+    TWO: 2,
+    THREE: 3,
+    FOUR: 4,
+    FIVE: 5,
+    SIX: 6,
+    SEVEN: 7,
+    EIGHT: 8,
+    NINE: 9,
+};
+
+export const OPERATIONS = {
+    DIVIDE: '/',
+    PLUS: '+',
+    MINUS: '-',
+    MULTIPLY: 'X',
+    CALCULATE: '=',
+};
