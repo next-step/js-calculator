4f5f79666d8a0fe91edb53cdbab1d4ef7a707d6a
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-09 18:32:00 +0900

    [feat] 숫자/연산자/AC 클릭 시 데이터 파싱 기능 구현
    -

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
+import { $ } from '../util.js';
+
+function clickNumber({ target }) {
+  console.log('click', target.textContent);
+}
+
+const clickOperation = ({ target }) => {
+  console.log('click', target.textContent);
+};
+
+const clickAllClear = ({ target }) => {
+  console.log('click', target.textContent);
+};
+
 console.log('Hello JS');
+
+const $digits = $('.digits');
+const $modifier = $('.modifier');
+const $operations = $('.operations');
+
+$digits.addEventListener('click', clickNumber);
+$operations.addEventListener('click', clickOperation);
+$modifier.addEventListener('click', clickAllClear);
