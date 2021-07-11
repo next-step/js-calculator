29a875b6799ca3f8d276ae5f8975faab2eeaeae4
sun.kist <sun.kist@kakaocorm.com>
2021-07-11 16:37:16 +0900

    class전환

--- src/js/index.js
+++ src/js/index.js
@@ -4,130 +4,145 @@ const operArr = document.querySelectorAll(".operation");
 const modifier = document.querySelector(".modifier");
 const total = document.querySelector("#total");
 
-const display = (result) => {
+class Calculator{
 
-    if(result || result === 0) {
-        total.textContent = result;
-    } else {
-        const str = store.equationStack.join('') + store.digitStack.join('');
-        total.textContent = str ? str : 0;
-    }
-    
-};
+    constructor({digitArr, operArr, modifier, total}){
+        this.total = total;
 
-const store = {
-    digitStack : [],
-    equationStack : [],
-    setDigitStack : digit => {
-        store.digitStack.push(digit);
-        display();
-    },
-    setEquationStack : oper => {
-        const num = store.digitStack.join('');
-        store.digitStack = [];
-        store.equationStack.push(num);
-        store.equationStack.push(oper);
-        display();
-    },
-    calculate : () => {
-        if(store.equationStack.length === 2) {
-            const prev = Number.parseInt(store.equationStack[0]);
-            const oper = store.equationStack[1];
-            const next = Number.parseInt(store.digitStack.join(''));
-            store.allClear();
-            switch(oper){
-                //division
-                case "/":
-                    display(Math.floor(prev/next));
-                    break;
-                //multiply
-                case "X":
-                    display(prev*next);
-                    break;
-                //minus
-                case "-":
-                    display(prev-next);
-                    break;
-                //plus
-                case "+":
-                    display(prev+next);
-                    break;
+        digitArr.forEach(digit => digit.addEventListener('click', (e) => {
+            const num = e.currentTarget.textContent;
+            
+            if(this.validDigit()) {
+                this.store.setDigitStack(num);
             }
+
+        }));
+
+        operArr.forEach(oper => {
+            oper.addEventListener('click', (e)=> {
+                const operation = e.currentTarget.textContent;
+
+                if(this.validOperation(operation)) {
+                    switch(operation){
+                        //division
+                        case "/":
+                            this.store.setEquationStack("/");
+                            break;
+                        //multiply
+                        case "X":
+                            this.store.setEquationStack("X");
+                            break;
+                        //minus
+                        case "-":
+                            this.store.setEquationStack("-");
+                            break;
+                        //plus
+                        case "+":
+                            this.store.setEquationStack("+");
+                            break;
+                        //equals
+                        case "=":
+                            this.store.calculate();
+                            break;
+                    }
+                }
+            });
+
+        });
+
+        modifier.addEventListener('click', (e) => {
+            this.store.allClear();
+        });
+        this.init();
+    }
+
+    init(){
+        this.store = {
+            digitStack : [],
+            equationStack : [],
+            setDigitStack : digit => {
+                this.store.digitStack.push(digit);
+                this.display();
+            },
+            setEquationStack : oper => {
+                const num = this.store.digitStack.join('');
+                this.store.digitStack = [];
+                this.store.equationStack.push(num);
+                this.store.equationStack.push(oper);
+                this.display();
+            },
+            calculate : () => {
+                if(this.store.equationStack.length === 2) {
+                    const prev = Number.parseInt(this.store.equationStack[0]);
+                    const oper = this.store.equationStack[1];
+                    const next = Number.parseInt(this.store.digitStack.join(''));
+                    this.store.allClear();
+                    switch(oper){
+                        //division
+                        case "/":
+                            this.display(Math.floor(prev/next));
+                            break;
+                        //multiply
+                        case "X":
+                            this.display(prev*next);
+                            break;
+                        //minus
+                        case "-":
+                            this.display(prev-next);
+                            break;
+                        //plus
+                        case "+":
+                            this.display(prev+next);
+                            break;
+                    }
+                }
+            },
+            allClear : () => {
+                this.store.digitStack = [];
+                this.store.equationStack = [];
+                this.total.textContent = 0;
+            },
+        };
+    }
+
+
+    validDigit(){
+        
+        if(this.store.digitStack.length == 3) {
+            alert('숫자는 세 자리까지만 입력가능합니다!');
+            return false;
         }
-    },
-    allClear : () => {
-        store.digitStack = [];
-        store.equationStack = [];
-        total.textContent = 0;
-    },
-};
-
-const validDigit = () => {
-    
-    if(store.digitStack.length == 3) {
-        alert('숫자는 세 자리까지만 입력가능합니다!');
-        return false;
-    }
 
 
-    return true;
-};
+        return true;
+    };
 
-const validOperation = oper => {
-    if((oper === '/' || oper === '+' || oper === '-' || oper === 'X') && store.digitStack.length === 0) {
-        alert('숫자를 먼저 입력한 후 연산자를 입력해주세요!');
-        return false;
-    } else if(store.digitStack.length === 0) {
-        return false;
-    } else if(store.equationStack.length === 2 && oper !== '=') {
-        return false;
-    }
-
-    return true;
-};
-
-
-digitArr.forEach(digit => digit.addEventListener('click', (e) => {
-    const num = e.currentTarget.textContent;
-    
-    if(validDigit()) {
-        store.setDigitStack(num);
-    }
-
-}));
-
-operArr.forEach(oper => {
-    oper.addEventListener('click', (e)=> {
-        const operation = e.currentTarget.textContent;
-
-        if(validOperation(operation)) {
-            switch(operation){
-                //division
-                case "/":
-                    store.setEquationStack("/");
-                    break;
-                //multiply
-                case "X":
-                    store.setEquationStack("X");
-                    break;
-                //minus
-                case "-":
-                    store.setEquationStack("-");
-                    break;
-                //plus
-                case "+":
-                    store.setEquationStack("+");
-                    break;
-                //equals
-                case "=":
-                    store.calculate();
-                    break;
-            }
+    validOperation(oper) {
+        if((oper === '/' || oper === '+' || oper === '-' || oper === 'X') && this.store.digitStack.length === 0) {
+            alert('숫자를 먼저 입력한 후 연산자를 입력해주세요!');
+            return false;
+        } else if(this.store.digitStack.length === 0) {
+            return false;
+        } else if(this.store.equationStack.length === 2 && oper !== '=') {
+            return false;
         }
-    });
 
-});
+        return true;
+    };
 
-modifier.addEventListener('click', (e) => {
-    store.allClear();
-});
+
+    display(result) {
+
+        if(result || result === 0) {
+            this.total.textContent = result;
+        } else {
+            const str = this.store.equationStack.join('') + this.store.digitStack.join('');
+            this.total.textContent = str ? str : 0;
+        }
+        
+    };
+
+}
+
+
+new Calculator({digitArr, operArr, modifier, total});
