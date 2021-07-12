1f824f02e0c8e2270494afc2695af01bf8864b60
Keu-ni <skcor8909@gmail.com>
2021-07-13 02:39:58 +0900

    <feat> 상수 파일 추가

---
+++ src/js/constants.js
@@ -0,0 +1,16 @@
+export const MAX_DIGIT_LENGTH = 3;
+export const DEFAULT_TOTAL_VALUE = '0';
+export const REGULAR_EXPRESSION = /[+/X-]/g;
+
+export const OPERATORS = {
+	PLUS: '+',
+	MINUS: '-',
+	MULTIPLE: 'X',
+	DIVISION: '/',
+	EQUAL: '=',
+};
+
+export const MESSAGES = {
+	ERROR100: '숫자는 세 자리까지만 입력 가능합니다!',
+	ERROR101: '숫자를 먼저 입력한 후 연산자를 입력해주세요!',
+};
