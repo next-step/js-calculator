f409a1d724597016ab2853787277b381ae0437aa
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-07-13 18:51:00 +0900

    feat: util 파일
    1. selector
    2. 계산 로직

---
+++ src/js/utils.js
@@ -0,0 +1,16 @@
+import { OPERATOR } from "./constants.js";
+
+export const $ = (selector) => document.querySelector(selector);
+
+export const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case OPERATOR.PLUS:
+      return +num1 + +num2;
+    case OPERATOR.MINUS:
+      return +num1 - +num2;
+    case OPERATOR.MULTIPLY:
+      return +num1 * +num2;
+    case OPERATOR.DIVIDE:
+      return Math.floor(num1 / num2);
+  }
+};
