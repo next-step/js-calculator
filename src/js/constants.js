c222294b6fb3c58ffe0ad2c0309a279a8b6f9ae3
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-07-13 18:51:11 +0900

    feat: 상수 파일

---
+++ src/js/constants.js
@@ -0,0 +1,13 @@
+export const MAX_NUMBER = 3;
+
+export const OPERATOR = {
+  PLUS: "+",
+  MINUS: "-",
+  MULTIPLY: "X",
+  DIVIDE: "/",
+  EQUAL: "=",
+};
+
+export const MESSAGE = {
+  MAX_NUMBER_OVER: `숫자는 ${MAX_NUMBER}자리를 넘길수는 없어요`,
+};
