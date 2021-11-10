7d6184a12c0fae17e211ff27ae2bba10b110dd46
amorfati0310 <lyhy0310@gmail.com>
2021-11-10 22:25:41 +0900

    린트 설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
