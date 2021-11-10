b13d79fbaecfdfc4aed6521aec25b07fa0c1bad7
Tanney <dhxodms89@gmail.com>
2021-11-11 00:14:13 +0900

    feat: 각 버튼에 data attribute 추가, 이에 따른 상수 추가

---
+++ src/js/constants/calculator.js
@@ -0,0 +1,13 @@
+export const MODIFIER = {
+  ALL_CLEAR: "all-clear",
+};
+
+export const OPERATION = {
+  DIVISTION: "division",
+  MULTIPLICATION: "multiplication",
+  SUBTRACTION: "subtraction",
+  ADDITION: "addition",
+  CALCULATION: "calculation",
+};
+
+export const DEFAULT_NUMBER = 0;
