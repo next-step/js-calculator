d4c6884a75d713ac1be2b39266d0025760589fb4
g1moon <gmail.com@gmail.com>
2021-11-11 23:13:47 +0900

    feat: 상수 추가.

---
+++ src/js/constants/index.js
@@ -0,0 +1,26 @@
+export const OPERATION = {
+  ADD: '+',
+  SUBTRACT: '-',
+  MULTIPLY: 'X',
+  DIVIDE: '/',
+  EQUAL: '=',
+}
+
+export const ZERO = '0';
+export const RES= 'res:';
+export const DOM_TYPE = {
+  DIGIT: 'digit',
+  OPERATION: 'operation',
+  MODIFIER: 'modifier',
+  TOTAL: 'total',
+}
+
+export const TARGET_NUM = {
+  NONE: 'none',
+  FIRST_NUM: 'firstNum',
+  SECOND_NUM: 'secondNum',
+}
+
+export const ERROR = {
+  MAX_NUMBER: '숫자는 3자리를 넘어 갈 수 없습니다!'
+}
