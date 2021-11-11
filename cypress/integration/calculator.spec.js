6f95b58ff3bad4ff0386b0458d446d06958c63a3
Heeji <limhizy15@ajou.ac.kr>
2021-11-11 23:28:40 +0900

    test: 사칙연산 테스트

---
+++ cypress/integration/calculator.spec.js
@@ -0,0 +1,54 @@
+describe("calculator", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5500/");
+  });
+
+  it("숫자를 눌렀을 때 디스플레이에 숫자가 표시된다", () => {
+    cy.get("#total").then(() => {
+      cy.get(".digits").contains("1").click();
+      cy.get("#total").should("have.text", "1");
+    });
+  });
+
+  it("덧셈 테스트: 1, +, 2를 입력하면 3이 표시된다", () => {
+    cy.get("#total").then(() => {
+      cy.get(".digits").contains("1").click();
+      cy.get(".operations").contains("+").click();
+      cy.get(".digits").contains("2").click();
+      cy.get(".operations").contains("=").click();
+      cy.get("#total").should("have.text", "3");
+    });
+  });
+
+  it("뺄셈 테스트: 3, -, 4를 입력하면 -1이 표시된다", () => {
+    cy.get("#total").then(() => {
+      cy.get(".digits").contains("3").click();
+      cy.get(".operations").contains("-").click();
+      cy.get(".digits").contains("4").click();
+      cy.get(".operations").contains("=").click();
+      cy.get("#total").should("have.text", "-1");
+    });
+  });
+
+  it("곱셈 테스트: 5, X, 6을 입력하면 30이 표시된다", () => {
+    cy.get("#total").then(() => {
+      cy.get(".digits").contains("5").click();
+      cy.get(".operations").contains("X").click();
+      cy.get(".digits").contains("6").click();
+      cy.get(".operations").contains("=").click();
+      cy.get("#total").should("have.text", "30");
+    });
+  });
+
+  it("나눗셈 테스트: 80, /, 6을 입력하면 13이 표시된다", () => {
+    cy.get("#total").then(() => {
+      cy.get(".digits").contains("8").click();
+      cy.get(".digits").contains("0").click();
+
+      cy.get(".operations").contains("/").click();
+      cy.get(".digits").contains("6").click();
+      cy.get(".operations").contains("=").click();
+      cy.get("#total").should("have.text", "13");
+    });
+  });
+});
