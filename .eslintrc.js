9a20523451b03840a19e63134f77a6d610b7bc7e
amorfati0310 <lyhy0310@gmail.com>
2021-11-14 21:53:31 +0900

    feat: TestCase 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,9 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
-  },
-  rules: {
   },
 };
