a1e71552e4afea29c4a4142557b9fc8bbc9c205f
정미니 <alsgml1640@hanmail.net>
2021-11-14 13:03:35 +0900

    테스트파일 리펙터링 정리

--- cypress/integration/calculate.spec.js
+++ cypress/integration/calculate.spec.js
@@ -9,41 +9,41 @@
 
 const BASE_URL = "http://127.0.0.1:5500";
 
+const calculate = (operand1, operator, operand2) => {
+  cy.get(`[data-digit="${operand1}"]`).click();
+  cy.get(`[data-oper="${operator}"]`).click();
+
+  if (operand2 !== undefined) {
+    cy.get(`[data-digit="${operand2}"]`).click();
+  }
+
+  cy.get('[data-oper="="]').click();
+};
+
 beforeEach(() => {
   cy.visit(BASE_URL);
 });
 
 describe("계산 기능", () => {
   it("2개의 숫자에 대해 덧셈이 가능하다.", () => {
-    cy.get('[data-digit="9"]').click();
-    cy.get('[data-oper="+"]').click();
-    cy.get('[data-digit="9"]').click();
-    cy.get('[data-oper="="]').click();
-
+    calculate("9", "+", "9");
     cy.get("#total").should("have.text", "18");
   });
 
   it("2개의 숫자에 대해 뺄셈이 가능하다.", () => {
-    cy.get('[data-digit="8"]').click();
-    cy.get('[data-oper="-"]').click();
-    cy.get('[data-digit="1"]').click();
-    cy.get('[data-oper="="]').click();
+    calculate("8", "-", "1");
     cy.get("#total").should("have.text", "7");
   });
 
   it("2개의 숫자에 대해 곱셉이 가능하다.", () => {
-    cy.get('[data-digit="8"]').click();
-    cy.get('[data-oper="x"]').click();
-    cy.get('[data-digit="2"]').click();
-    cy.get('[data-oper="="]').click();
+    calculate("8", "x", "2");
+
     cy.get("#total").should("have.text", "16");
   });
 
   it("2개의 숫자에 대해 나눗셈이 가능하다.", () => {
-    cy.get('[data-digit="8"]').click();
-    cy.get('[data-oper="/"]').click();
-    cy.get('[data-digit="2"]').click();
-    cy.get('[data-oper="="]').click();
+    calculate("8", "/", "2");
+
     cy.get("#total").should("have.text", "4");
   });
 });
@@ -70,20 +70,12 @@ describe("예외 처리", () => {
 
 describe("결과", () => {
   it("계산 결과를 표현할 때 소수점 이하는 버림한다.", () => {
-    cy.get('[data-digit="3"]').click();
-    cy.get('[data-digit="3"]').click();
-    cy.get('[data-oper="/"]').click();
-    cy.get('[data-digit="4"]').click();
-    cy.get('[data-oper="="]').click();
-    cy.get("#total").should("have.text", "8");
+    calculate("9", "/", "4");
+    cy.get("#total").should("have.text", "2");
   });
 
   it("0으로 나눌 수 없다..", () => {
-    cy.get('[data-digit="3"]').click();
-    cy.get('[data-digit="0"]').click();
-    cy.get('[data-oper="/"]').click();
-    cy.get('[data-digit="0"]').click();
-    cy.get('[data-oper="="]').click();
+    calculate("9", "/", "0");
     cy.get("#total").should("have.text", "0으로 나눌 수 없습니다.");
   });
 });
