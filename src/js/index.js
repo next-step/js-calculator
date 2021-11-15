ff62ca2443def2935e5ae7607f5859b6db5a4ff3
Soeun Lee <soeun@chartmogul.com>
2021-11-16 00:44:25 +0100

    innerText에서 textContent로 수정

--- src/js/index.js
+++ src/js/index.js
@@ -6,23 +6,23 @@
   const ac = document.querySelector('.modifier');
 
   digits.onclick = (e) => {
-    model.setCurrentNumber(e.target.innerText);
-    total.innerText = model.getCurrentNumber();
+    model.setCurrentNumber(e.target.textContent);
+    total.textContent = model.getCurrentNumber();
   };
 
   operations.onclick = (e) => {
-    const operator = e.target.innerText;
+    const operator = e.target.textContent;
     model.calculate();
     if (operator === OPERATOR.EQUAL) {
-      total.innerText = model.getTotal();
+      total.textContent = model.getTotal();
     } else {
-      model.setCurrentOperator(e.target.innerText);
-      total.innerText = model.getCurrentOperator();
+      model.setCurrentOperator(e.target.textContent);
+      total.textContent = model.getCurrentOperator();
     }
   };
 
   ac.onclick = () => {
     model.reset();
-    total.innerText = model.getTotal();
+    total.textContent = model.getTotal();
   };
 })();
