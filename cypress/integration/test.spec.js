559d4f9af6c4638993181ca29efa1a1950f0d2ee
che-ri <rnjs1135@gmail.com>
2021-11-21 19:03:31 +0900

    test: 모든 케이스에 대한 테스트코드 적용

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -49,4 +49,32 @@ describe("JS 계산기", function () {
     OPERATION.DIVISION,
     (a, b) => Math.trunc(a / b)
   );
+
+  it("AC(All Clear)버튼을 누르면 0으로 초기화 한다.", () => {
+    cy.clickModifierButton(MODIFIER.CLEAR);
+    cy.resultShouldBe(0);
+  });
+
+  it("숫자는 한번에 최대 3자리 수까지 입력 가능하다.", () => {
+    cy.clickNumberButton([7777]);
+
+    cy.get("#total").then(($total) => {
+      const val = $total.text();
+      expect(val).to.have.length(3);
+    });
+
+    // 아래와 같은 방법도 된다.
+    // cy.get("#total").invoke("text").then(parseFloat).should("eq", 777);
+  });
+
+  it("계산 결과를 표현할 때 소수점 이하는 버림한다.", () => {
+    cy.clickNumberButton(10);
+    cy.clickOperationButton(OPERATION.DIVISION);
+    cy.clickNumberButton(3);
+    cy.clickOperationButton(OPERATION.EQUAL);
+    cy.get("#total").then(($total) => {
+      const value = $total.text();
+      expect(value).not.to.include(".");
+    });
+  });
 });
