2d8284739e80078551a0880cb0bca73cc7ed8216
padawanR0k <padawanR0k@gmail.com>
2022-03-09 17:12:37 +0900

    up: 잘못된 테스트 케이스 수정

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,12 @@
 import { describe, it, expect } from "vitest";
-import calculate from "../src/js/index.js";
+import calculate from "../src/js/index.mjs";
 
 describe("계산기", () => {
   it("2개의 숫자에 대해 덧셈이 가능하다.", () => {
     expect(calculate(["1", "+", "2"])).eq(3);
   });
   it("2개의 숫자에 대해 뺄셈이 가능하다.", () => {
-    expect(calculate(["2", "-", "2"])).eq(1);
+    expect(calculate(["2", "-", "2"])).eq(0);
   });
   it("2개의 숫자에 대해 곱셈이 가능하다.", () => {
     expect(calculate(["2", "*", "2"])).eq(4);
