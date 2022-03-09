d37053f4367512dfe851e8e8abb528ab86faed28
정현수 <jung660317@naver.com>
2022-03-09 15:44:09 +0900

    chore: 환경 설정
    - eslint, prettier 설치 및 설정
    - index.js을 index.html에 연결

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
