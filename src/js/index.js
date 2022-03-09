bdf2abb7b2278f20d11f784aa9f4581f346d53f9
padawanR0k <padawanR0k@gmail.com>
2022-03-09 16:45:24 +0900

    up: 기능 요구사항 중 유닛테스트 추가

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const operatorFunctions = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+};
+const operators = Object.keys(operatorFunctions);
+
+const inputStore = [];
+
+/**
+ *
+ * @param {Array<number | string>} inputs
+ */
+const calculate = (inputs) => {};
+export default calculate;
