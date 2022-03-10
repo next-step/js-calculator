0f02e326cfed0e96c10748d7c889d9af50d2a05d
karl <elrion018@gmail.com>
2022-03-11 00:49:59 +0900

    feat : 기능 완성

--- src/js/Calculator.js
+++ src/js/Calculator.js
@@ -1,6 +1,6 @@
 export default class Calculator {
   constructor() {
-    this.inputNumbers = [0];
+    this.numbers = [0];
     this.operators = [];
     this.operatings = {
       '+': (numberA, numberB) => numberA + numberB,
@@ -10,10 +10,11 @@ export default class Calculator {
     };
   }
 
+  /** view 단에서 join 하여 렌더링할 numbers 와 operators의 배열을 만드는 메소드 */
   getNumberAndOperators() {
     let numberAndOperators = [];
 
-    this.inputNumbers.forEach((number, index) => {
+    this.numbers.forEach((number, index) => {
       if (this.operators[index]) {
         numberAndOperators = numberAndOperators.concat([
           number,
@@ -26,47 +27,78 @@ export default class Calculator {
       numberAndOperators = numberAndOperators.concat([number]);
     });
 
-    console.log(numberAndOperators);
-
     return numberAndOperators;
   }
 
-  /** 연산의 대상이 되는 numberText를 number로 바꿔 inputNumbers 배열의 마지막 원소에 더하는 메소드 */
-  inputNumber(numberText) {
+  /** input된 numberText를 처리하기 전에 실행하는 validate 메소드 */
+  validateNumberText(numberText) {
     if (
-      this.inputNumbers[this.inputNumbers.length - 1].toString().length >= 3
+      this.numbers[this.numbers.length - 1].toString().length >= 3 &&
+      this.numbers.length !== this.operators.length
     ) {
-      return alert('숫자는 세 자리까지만 입력 가능합니다!');
+      alert('숫자는 세 자리까지만 입력 가능합니다!');
+
+      return false;
     }
 
-    if (!isNaN(Number(numberText))) {
-      const copiedNumbers = [...this.inputNumbers];
+    if (isNaN(Number(numberText))) return false;
 
-      copiedNumbers[copiedNumbers.length - 1] =
-        copiedNumbers[copiedNumbers.length - 1] * 10 + Number(numberText);
-
-      this.inputNumbers = copiedNumbers;
-    }
+    return true;
   }
 
-  /** number들을 연산하는 메소드 */
+  /** 연산의 대상이 되는 numberText를 number로 바꿔 numbers 배열의 마지막 원소에 더하는 메소드 */
+  inputNumber(numberText) {
+    if (!this.validateNumberText(numberText)) return;
+
+    const number = Number(numberText);
+
+    if (this.numbers.length > this.operators.length) this.buildNumber(number);
+    else this.appendNumber(number);
+  }
+
+  /** numbers의 마지막 숫자를 만드는 메소드 */
+  buildNumber(number) {
+    const copiedNumbers = [...this.numbers];
+
+    copiedNumbers[copiedNumbers.length - 1] =
+      copiedNumbers[copiedNumbers.length - 1] * 10 + number;
+
+    this.numbers = copiedNumbers;
+  }
+
+  appendNumber(number) {
+    this.numbers = this.numbers.concat([number]);
+  }
+
+  inputOperator(operator) {
+    if (this.numbers.length === this.operators.length)
+      return alert('숫자를 먼저 입력한 후 연산자를 입력해주세요!');
+
+    if (operator === '=') return this.operateNumbers();
+
+    this.operators = this.operators.concat([operator]);
+  }
+
+  /** numbers을 연산하는 메소드 */
   operateNumbers() {
     // 2개 숫자 이상의 긴 연산에 대비한 반복문 로직
 
-    const calculated = this.inputNumbers.reduce((acc, number, index) => {
+    const calculated = this.numbers.reduce((acc, number, index) => {
       // 3개 이상의 숫자는 연산하지 않는다.
-      if (index > 0) return;
+      if (index === 0 || index > 1) return acc;
 
-      const operator = this.operators[index];
+      const operator = this.operators[index - 1];
 
       return this.operatings[operator](acc, number);
-    }, 0);
+    }, this.numbers[0]);
 
-    this.inputNumbers = [calculated];
+    this.numbers = [calculated];
+    this.operators = [];
   }
 
-  /** inputNumbers를 비우는 메소드 */
-  clearInputNumbers() {
-    this.inputNumbers = [0];
+  /** numbers와 Operators를 비우는 메소드 */
+  clearNumbersAndOperators() {
+    this.numbers = [0];
+    this.operators = [];
   }
 }
--- src/js/view.js
+++ src/js/view.js
@@ -10,14 +10,24 @@ export default class View {
     this.rootElement
       .querySelector('.digits')
       .addEventListener('click', this.handleNumberButtonClick.bind(this));
+
+    this.rootElement
+      .querySelector('.operations')
+      .addEventListener('click', this.handleOperationButtonClick.bind(this));
+
+    this.rootElement
+      .querySelector('.modifiers')
+      .addEventListener('click', this.handleClearButtonClick.bind(this));
   }
 
   /** 입력된 숫자들과 연산자들을 렌더링하는 메소드 */
   renderNumbers() {
-    this.rootElement.querySelector('#total').innerText =
-      this.calculator.getNumberAndOperators();
+    this.rootElement.querySelector('#total').innerText = this.calculator
+      .getNumberAndOperators()
+      .join('');
   }
 
+  /** 숫자 버튼 클릭 이벤트 리스너 */
   handleNumberButtonClick(event) {
     const numberText = event.target.innerText;
 
@@ -25,7 +35,17 @@ export default class View {
     this.renderNumbers();
   }
 
-  handleOperationButtonClick() {}
+  /** 연산자 버튼 클릭 이벤트 리스너 */
+  handleOperationButtonClick(event) {
+    const operator = event.target.innerText;
 
-  handleClearButtonClick() {}
+    this.calculator.inputOperator(operator);
+    this.renderNumbers();
+  }
+
+  /** AC 버튼 클릭 이벤트 리스너 */
+  handleClearButtonClick() {
+    this.calculator.clearNumbersAndOperators();
+    this.renderNumbers();
+  }
 }
