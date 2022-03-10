d19ea2b00e8802b427854927b6d2ecbc400f0aaa
정현수 <jung660317@naver.com>
2022-03-10 13:36:55 +0900

    test: 계산기 테스트 코드 추가
    - cypress를 이용해 테스트 구축

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -3,19 +3,65 @@ describe('js-calculator', () => {
     cy.visit('/');
   });
 
-  // it('+ 버튼 클릭시 1 증가한다.', () => {
-  //   // btn-inc 클래스를 가진 요소를 클릭
-  //   cy.get('.btn-inc').click();
+  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+    cy.contains('1').click();
+    cy.contains('+').click();
+    cy.contains('2').click();
+    cy.contains('=').click();
+    cy.get('#total').should('have.text', '3');
+  });
 
-  //   // value 클래스를 가진 요소의 텍스트가 10
-  //   cy.get('.value').should('have.text', '10');
-  // });
+  it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+    cy.contains('3').click();
+    cy.contains('-').click();
+    cy.contains('2').click();
+    cy.contains('=').click();
+    cy.get('#total').should('have.text', '1');
+  });
 
-  // 2개의 숫자에 대해 덧셈이 가능하다.
-  // 2개의 숫자에 대해 뺄셈이 가능하다.
-  // 2개의 숫자에 대해 곱셈이 가능하다.
-  // 2개의 숫자에 대해 나눗셈이 가능하다.
-  // AC(All Clear)버튼을 누르면 0으로 초기화 한다.
-  // 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
-  // 계산 결과를 표현할 때 소수점 이하는 버림한다.
+  it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+    cy.contains('3').click();
+    cy.contains('X').click();
+    cy.contains('2').click();
+    cy.contains('=').click();
+    cy.get('#total').should('have.text', '6');
+  });
+
+  it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+    cy.contains('6').click();
+    cy.contains('/').click();
+    cy.contains('2').click();
+    cy.contains('=').click();
+    cy.get('#total').should('have.text', '3');
+  });
+
+  it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+    cy.contains('6').click();
+    cy.contains('2').click();
+    cy.contains('AC').click();
+    cy.get('#total').should('have.text', '0');
+  });
+
+  it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
+    const alertStub = cy.stub();
+
+    cy.contains('6').click();
+    cy.contains('2').click();
+    cy.contains('2').click();
+    cy.contains('2')
+      .click()
+      .then(() => {
+        expect(alertStub.getCall(0)).to.be.calledWith('숫자는 최대 3자리 수까지 입력 가능합니다.');
+      });
+    cy.get('#total').should('have.text', '622');
+  });
+
+  it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+    cy.contains('1').click();
+    cy.contains('2').click();
+    cy.contains('/').click();
+    cy.contains('5').click();
+    cy.contains('=').click();
+    cy.get('#total').should('have.text', '2');
+  });
 });
