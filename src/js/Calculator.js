3ae861c8615ddd3b42db3f7f183d787c1cb90c0f
karl <elrion018@gmail.com>
2022-03-11 02:27:55 +0900

    refactor : Calculator 메소드 순서 변경

--- src/js/Calculator.js
+++ src/js/Calculator.js
@@ -11,17 +11,55 @@ export default class Calculator {
     this.operators = [];
   }
 
-  /** view 단에서 join 하여 렌더링할 numbers 와 operators의 배열을 만드는 메소드 */
-  getNumberAndOperators() {
-    const numberAndOperators = this.numbers.map((number, index) => {
-      if (this.operators[index]) {
-        return [number, this.operators[index]];
-      }
+  /** numberText를 number로 바꾸어 입력하는 메소드 */
+  inputNumber(numberText) {
+    if (!this._validateNumberText(numberText)) return;
 
-      return number;
-    });
+    const number = Number(numberText);
 
-    return numberAndOperators.flat();
+    if (this.numbers.length > this.operators.length) this._buildNumber(number);
+    else this._appendNumber(number);
+  }
+
+  /** operators에 operator를 입력하는 메소드. 그러나 '=' 가 입력될 시 연산을 시작한다. */
+  inputOperator(operator) {
+    if (!this._validateBeforeInputOperator()) return;
+
+    if (operator === '=') return this._operateNumbers();
+
+    this.operators = this.operators.concat([operator]);
+  }
+
+  /** numbers의 마지막 숫자를 만드는 메소드 */
+  _buildNumber(number) {
+    const copiedNumbers = [...this.numbers];
+
+    copiedNumbers[copiedNumbers.length - 1] =
+      copiedNumbers.at(-1) * 10 + number;
+
+    this.numbers = copiedNumbers;
+  }
+
+  /** numbers에 새로운 number를 추가하는 메소드 */
+  _appendNumber(number) {
+    this.numbers = this.numbers.concat([number]);
+  }
+
+  /** numbers을 연산하는 메소드 */
+  _operateNumbers() {
+    // 2개 숫자 이상의 긴 연산에 대비한 반복문 로직
+
+    const calculated = this.numbers.reduce((acc, number, index) => {
+      // 3개 이상의 숫자는 연산하지 않는다.
+      if (index === 0 || index > 1) return acc;
+
+      const operator = this.operators[index - 1];
+
+      return operations[operator](acc, number);
+    }, this.numbers[0]);
+
+    this.numbers = [calculated];
+    this.operators = [];
   }
 
   /** input된 numberText를 처리하기 전에 실행하는 validate 메소드 */
@@ -54,55 +92,17 @@ export default class Calculator {
     return true;
   }
 
-  /** numberText를 number로 바꾸어 입력하는 메소드 */
-  inputNumber(numberText) {
-    if (!this._validateNumberText(numberText)) return;
+  /** view 단에서 join 하여 렌더링할 numbers 와 operators의 배열을 만드는 메소드 */
+  getNumberAndOperators() {
+    const numberAndOperators = this.numbers.map((number, index) => {
+      if (this.operators[index]) {
+        return [number, this.operators[index]];
+      }
 
-    const number = Number(numberText);
+      return number;
+    });
 
-    if (this.numbers.length > this.operators.length) this._buildNumber(number);
-    else this._appendNumber(number);
-  }
-
-  /** numbers의 마지막 숫자를 만드는 메소드 */
-  _buildNumber(number) {
-    const copiedNumbers = [...this.numbers];
-
-    copiedNumbers[copiedNumbers.length - 1] =
-      copiedNumbers.at(-1) * 10 + number;
-
-    this.numbers = copiedNumbers;
-  }
-
-  /** numbers에 새로운 number를 추가하는 메소드 */
-  _appendNumber(number) {
-    this.numbers = this.numbers.concat([number]);
-  }
-
-  /** operators에 operator를 입력하는 메소드. 그러나 '=' 가 입력될 시 연산을 시작한다. */
-  inputOperator(operator) {
-    if (!this._validateBeforeInputOperator()) return;
-
-    if (operator === '=') return this._operateNumbers();
-
-    this.operators = this.operators.concat([operator]);
-  }
-
-  /** numbers을 연산하는 메소드 */
-  _operateNumbers() {
-    // 2개 숫자 이상의 긴 연산에 대비한 반복문 로직
-
-    const calculated = this.numbers.reduce((acc, number, index) => {
-      // 3개 이상의 숫자는 연산하지 않는다.
-      if (index === 0 || index > 1) return acc;
-
-      const operator = this.operators[index - 1];
-
-      return operations[operator](acc, number);
-    }, this.numbers[0]);
-
-    this.numbers = [calculated];
-    this.operators = [];
+    return numberAndOperators.flat();
   }
 
   /** numbers와 Operators를 비우는 메소드 */
