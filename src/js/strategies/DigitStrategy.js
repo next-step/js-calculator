823516e170c04f0746a914ebe8ee0871ff0d4b59
bomee.yoon <bomee.yoon@godo.co.kr>
2022-03-10 15:18:56 +0900

    ♻️  오류 발생 시 미사용하게되는 변수들 위치 변경

--- src/js/strategies/DigitStrategy.js
+++ src/js/strategies/DigitStrategy.js
@@ -5,17 +5,18 @@ class DigitStrategy {
   static mutateState($target, state) {
     const key = isNull(state.operator) ? 'x' : 'y';
 
-    const prevNumber = state[key];
     const currNumber = Number($target.dataset.value);
-
-    const isOverMaxSize = prevNumber?.toString().length >= MAX_DIGIT_SIZE;
 
     if (key === 'x' && !currNumber)
       throw new Error(ERROR_MSG.PLZ_SELECT_NUMBER);
 
+    const prevNumber = state[key];
+    const isOverMaxSize = prevNumber?.toString().length >= MAX_DIGIT_SIZE;
+
     if (isOverMaxSize) throw new Error(ERROR_MSG.PLZ_CHECK_MAX_NUMBER);
 
     state[key] = prevNumber * 10 + currNumber;
+
     return state;
   }
 }
