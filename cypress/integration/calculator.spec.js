9201a021b567da529ae40051eb21abcc206ca311
정현수 <jung660317@naver.com>
2022-03-11 14:35:28 +0900

    test: 테스트 코드 정리
    - 가독성을 높이기 위해 이벤트들을 함수로 만들어 사용
    - 실제 코드에서 사용하는 상수 값들을 사용

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,61 +1,75 @@
+import { MESSAGE, DOM, OPERATION, MODIFIER, INIT_STATE } from '../../src/js/constants.js';
+
+const CLICK_DIGIT = digit => cy.get(`${DOM.digits} > ${DOM.digit}`).contains(digit).click();
+const CLICK_OPERATOR = operator =>
+  cy.get(`${DOM.operations} > ${DOM.operation}`).contains(operator).click();
+const CLICK_MODIFIER = modifier =>
+  cy.get(`${DOM.modifiers} > ${DOM.modifier}`).contains(modifier).click();
+const CHECK_ALERT_MESSAGE = message =>
+  cy.on('window:alert', text => {
+    expect(text).to.contains(message);
+  });
+const THE_RESULT_SHOULD_BE = result => cy.get(DOM.total).should('have.text', result);
+
 describe('js-calculator', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
-    cy.get('.digits > .digit').contains('1').click();
-    cy.contains('+').click();
-    cy.get('.digits > .digit').contains('2').click();
-    cy.contains('=').click();
-    cy.get('#total').should('have.text', '3');
+    CLICK_DIGIT('1');
+    CLICK_OPERATOR(OPERATION.plus);
+    CLICK_DIGIT('2');
+    CLICK_OPERATOR(OPERATION.equal);
+    THE_RESULT_SHOULD_BE('3');
   });
 
   it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
-    cy.get('.digits > .digit').contains('3').click();
-    cy.contains('-').click();
-    cy.get('.digits > .digit').contains('2').click();
-    cy.contains('=').click();
-    cy.get('#total').should('have.text', '1');
+    CLICK_DIGIT('3');
+    CLICK_OPERATOR(OPERATION.minus);
+    CLICK_DIGIT('2');
+    CLICK_OPERATOR(OPERATION.equal);
+    THE_RESULT_SHOULD_BE('1');
   });
 
   it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
-    cy.get('.digits > .digit').contains('3').click();
-    cy.contains('X').click();
-    cy.get('.digits > .digit').contains('2').click();
-    cy.contains('=').click();
-    cy.get('#total').should('have.text', '6');
+    CLICK_DIGIT('3');
+    CLICK_OPERATOR(OPERATION.multiple);
+    CLICK_DIGIT('2');
+    CLICK_OPERATOR(OPERATION.equal);
+    THE_RESULT_SHOULD_BE('6');
   });
 
   it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
-    cy.get('.digits > .digit').contains('6').click();
-    cy.contains('/').click();
-    cy.get('.digits > .digit').contains('2').click();
-    cy.contains('=').click();
-    cy.get('#total').should('have.text', '3');
+    CLICK_DIGIT('6');
+    CLICK_OPERATOR(OPERATION.division);
+    CLICK_DIGIT('2');
+    CLICK_OPERATOR(OPERATION.equal);
+    THE_RESULT_SHOULD_BE('3');
   });
 
-  it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
-    cy.get('.digits > .digit').contains('6').click();
-    cy.get('.digits > .digit').contains('2').click();
-    cy.contains('AC').click();
-    cy.get('#total').should('have.text', '0');
+  it(`AC(All Clear)버튼을 누르면 ${INIT_STATE.currentTotal}으로 초기화 한다.`, () => {
+    CLICK_DIGIT('6');
+    CLICK_DIGIT('2');
+    CLICK_MODIFIER(MODIFIER.allClear);
+    THE_RESULT_SHOULD_BE(INIT_STATE.currentTotal);
   });
 
   it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
-    cy.get('.digits > .digit').contains('6').click();
-    cy.get('.digits > .digit').contains('2').click();
-    cy.get('.digits > .digit').contains('2').click();
-    cy.get('.digits > .digit').contains('2').click();
-    cy.get('#total').should('have.text', '622');
+    CLICK_DIGIT('6');
+    CLICK_DIGIT('2');
+    CLICK_DIGIT('2');
+    CLICK_DIGIT('2');
+    CHECK_ALERT_MESSAGE(MESSAGE.numberCanBeEnteredUpToThreeDigitsAtOnce);
+    THE_RESULT_SHOULD_BE('622');
   });
 
   it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
-    cy.get('.digits > .digit').contains('1').click();
-    cy.get('.digits > .digit').contains('2').click();
-    cy.contains('/').click();
-    cy.get('.digits > .digit').contains('5').click();
-    cy.contains('=').click();
-    cy.get('#total').should('have.text', '2');
+    CLICK_DIGIT('1');
+    CLICK_DIGIT('2');
+    CLICK_OPERATOR(OPERATION.division);
+    CLICK_DIGIT('5');
+    CLICK_OPERATOR(OPERATION.equal);
+    THE_RESULT_SHOULD_BE('2');
   });
 });
