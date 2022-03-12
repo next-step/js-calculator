009658415c5b1149f7f4004fcd8d8f5289f72413
padawanR0k <padawanR0k@gmail.com>
2022-03-12 10:21:16 +0900

    refactor: private 필드 추가

--- src/js/event.js
+++ src/js/event.js
@@ -5,9 +5,19 @@ import { InputStore } from "./model/inputStore";
 class CalculatorEventListener {
   #calculateCore;
 
+  #container;
+
+  #console;
+
+  #inputStore;
+
+  #currentNumber;
+
+  #operator;
+
   /**
    *
-   * @param {HTMLElement} $container
+   * @param {HTMLElement} $container리
    */
   constructor($container) {
     if (!$container) {
@@ -20,55 +30,57 @@ class CalculatorEventListener {
       throw new Error("$total is required");
     }
 
-    this.$container = $container;
-    this.$console = $total;
-    this.inputStore = new InputStore();
-    this.currentNumber = 0;
-    this.operator = new Operator();
+    this.#container = $container;
+    this.#console = $total;
+    this.#inputStore = new InputStore();
+    this.#currentNumber = 0;
+    this.#operator = new Operator();
     this.#calculateCore = new CalculateCore();
   }
 
   init() {
-    this.$container.addEventListener("click", ({ target }) => {
+    this.#container.addEventListener("click", ({ target }) => {
       const { innerText } = target;
-      const { innerText: currentConsole } = this.$console;
+      const { innerText: currentConsole } = this.#console;
 
       if (target.className === "digit") {
-        if (this.currentNumber > 99) {
+        if (this.#currentNumber > 99) {
           alert("숫자는 3자리까지만 입력할 수 있습니다.");
           return;
         }
 
         const parsedValue = parseInt(innerText, 10);
-        this.$console.innerText =
+        this.#console.innerText =
           currentConsole === "0" ? innerText : `${currentConsole}${innerText}`;
-        this.inputStore.push(innerText);
-        this.currentNumber = this.currentNumber * 10 + parsedValue;
+        this.#inputStore.push(innerText);
+        this.#currentNumber = this.#currentNumber * 10 + parsedValue;
         return;
       }
       if (target.className === "operation") {
-        if (!this.inputStore.length) {
+        if (!this.#inputStore.length) {
           alert("먼저 숫자를 입력해주세요!");
           return;
         }
         if (
-          this.operator.isOperator(this.inputStore[this.inputStore.length - 1])
+          this.#operator.isOperator(
+            this.#inputStore[this.#inputStore.length - 1]
+          )
         ) {
           alert("연산자를 연속적으로 입력할 수 없습니다!");
           return;
         }
 
         if (innerText === "=") {
-          const total = this.#calculateCore.calculate(this.inputStore);
-          this.$console.innerText = total;
-          this.inputStore = [total];
-          this.currentNumber = total;
+          const total = this.#calculateCore.calculate(this.#inputStore);
+          this.#console.innerText = total;
+          this.#inputStore = [total];
+          this.#currentNumber = total;
           return;
         }
 
-        this.$console.innerText += innerText;
-        this.inputStore.push(innerText);
-        this.currentNumber = 0;
+        this.#console.innerText += innerText;
+        this.#inputStore.push(innerText);
+        this.#currentNumber = 0;
 
         return;
       }
@@ -80,12 +92,12 @@ class CalculatorEventListener {
   }
 
   resetConsole() {
-    this.$console.innerText = 0;
-    this.currentNumber = 0;
+    this.#console.innerText = 0;
+    this.#currentNumber = 0;
   }
 
   clearInputStore() {
-    this.inputStore = [];
+    this.#inputStore = [];
   }
 }
 
