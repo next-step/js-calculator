8511e002ee332bc8bd83e47c220cbd8e48a15896
aawlgus <aawlgus@jaranda.kr>
2022-03-13 16:29:53 +0900

    set private to constants and variables

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,82 +1,90 @@
 export class Calculator {
+  #firstNumberString;
+  #secondNumberString;
+  #operator;
+  #displayText;
+  #hasClickedOperator;
+
   constructor() {
-    this.firstNumberString = "";
-    this.secondNumberString = "";
-    this.operator = "";
-    this.displayText = "";
-    this.hasClickedOperator = false;
+    this.#firstNumberString = "";
+    this.#secondNumberString = "";
+    this.#operator = "";
+    this.#displayText = "";
+    this.#hasClickedOperator = false;
   }
+
+  #updateDisplayText = (text) => {
+    const total = document.querySelector("#total");
+    total.innerText = text;
+  };
+
+  #initializeInputData = () => {
+    this.#firstNumberString = "";
+    this.#secondNumberString = "";
+    this.#operator = "";
+    this.#hasClickedOperator = false;
+  };
+
+  #isLessThanThreeDigits = (numberString) => {
+    if (numberString.length < 3) {
+      return true;
+    }
+    return false;
+  };
 
   setEventListeners() {
     const digits = document.getElementsByClassName("digit");
     for (const digit of digits) {
       digit.addEventListener("click", () => {
-        if (this.hasClickedOperator) {
-          if (this.isLessThanThreeDigits(this.secondNumberString)) {
-            this.secondNumberString += digit.textContent;
-            this.displayText =
-              this.firstNumberString + this.operator + this.secondNumberString;
+        if (this.#hasClickedOperator) {
+          if (this.#isLessThanThreeDigits(this.#secondNumberString)) {
+            this.#secondNumberString += digit.textContent;
+            this.#displayText =
+              this.#firstNumberString +
+              this.#operator +
+              this.#secondNumberString;
           } else {
             window.alert("한번에 최대 3자리 수까지 입력할 수 있어요. ");
           }
         } else {
-          if (this.isLessThanThreeDigits(this.firstNumberString)) {
-            this.firstNumberString += digit.textContent;
-            this.displayText = this.firstNumberString;
+          if (this.#isLessThanThreeDigits(this.#firstNumberString)) {
+            this.#firstNumberString += digit.textContent;
+            this.#displayText = this.#firstNumberString;
           } else {
             window.alert("한번에 최대 3자리 수까지 입력할 수 있어요. ");
           }
         }
-        this.updateDisplayText(this.displayText);
+        this.#updateDisplayText(this.#displayText);
       });
     }
 
     const operations = document.getElementsByClassName("operation");
     for (const operation of operations) {
       operation.addEventListener("click", () => {
-        this.hasClickedOperator = true;
-        this.operator = operation.textContent;
-        this.displayText += operation.textContent;
-        this.updateDisplayText(this.displayText);
+        this.#hasClickedOperator = true;
+        this.#operator = operation.textContent;
+        this.#displayText += operation.textContent;
+        this.#updateDisplayText(this.#displayText);
       });
     }
 
     const equal = document.getElementById("equal");
     equal.addEventListener("click", () => {
       // 연산
-      const firstNumber = Number(this.firstNumberString);
-      const secondNumber = Number(this.secondNumberString);
-      this.displayText = calculate(firstNumber, secondNumber, this.operator);
-      this.updateDisplayText(this.displayText);
-      this.initializeInputData();
+      const firstNumber = Number(this.#firstNumberString);
+      const secondNumber = Number(this.#secondNumberString);
+      this.#displayText = calculate(firstNumber, secondNumber, this.#operator);
+      this.#updateDisplayText(this.#displayText);
+      this.#initializeInputData();
     });
 
     const modifier = document.getElementsByClassName("modifier")[0];
     modifier.addEventListener("click", () => {
-      this.displayText = "0";
-      this.initializeInputData();
-      this.updateDisplayText(this.displayText);
+      this.#displayText = "0";
+      this.#initializeInputData();
+      this.#updateDisplayText(this.#displayText);
     });
   }
-
-  updateDisplayText = (text) => {
-    const total = document.querySelector("#total");
-    total.innerText = text;
-  };
-
-  initializeInputData = () => {
-    this.firstNumberString = "";
-    this.secondNumberString = "";
-    this.operator = "";
-    this.hasClickedOperator = false;
-  };
-
-  isLessThanThreeDigits = (numberString) => {
-    if (numberString.length < 3) {
-      return true;
-    }
-    return false;
-  };
 }
 
 export const Operator = {
