96cdeaa6704c5e7f8d5dec37a9200f867ac3d04d
dahye1013 <alrean@naver.com>
2022-03-14 08:14:34 +0900

    refactor: 소수점 버림처리 음수 케이스 감안하여 수정

--- src/js/utils/operation.js
+++ src/js/utils/operation.js
@@ -1,6 +1,6 @@
 export const calcOperation = (a, b) => {
   return {
-    ['/']: () => Math.floor(Number(a) / Number(b)),
+    ['/']: () => Math.trunc(Number(a) / Number(b)),
     ['X']: () => Number(a) * Number(b),
     ['-']: () => Number(a) - Number(b),
     ['+']: () => Number(a) + Number(b),
