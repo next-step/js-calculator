788e00d8ba944bad52a2c3cbdb598f452e0c980c
yukyung <s2018w39@e-mirim.hs.kr>
2022-03-13 22:55:18 +0900

    chore: eselint 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: ['airbnb'],
-  rules: {
-    'linebreak-style': 0,
-  },
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	extends: ['airbnb', 'prettier'],
+	rules: {
+		'linebreak-style': 0,
+		'import/prefer-default-export': 'off',
+		'import/extensions': ['off'],
+	},
 };
