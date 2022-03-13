999fffbea4e1678235995a5e99512bbf73259ebe
aawlgus <aawlgus@jaranda.kr>
2022-03-13 17:58:50 +0900

    remove duplicates in test code

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -3,48 +3,39 @@ describe("Calculator Test", function () {
     cy.visit("http://127.0.0.1:5500/index.html");
   });
 
-  it("2개의 숫자에 대해 덧셈이 가능하다.", function () {
-    cy.contains("9").click();
-    cy.contains("+").click();
-    cy.contains("8").click();
+  const runCalculator = (num1, operator, num2) => {
+    num1.forEach((num) => {
+      cy.contains(num).click();
+    });
+    cy.contains(operator).click();
+    num2.forEach((num) => {
+      cy.contains(num).click();
+    });
     cy.get("#equal").click();
+  };
 
+  it("2개의 숫자에 대해 덧셈이 가능하다.", function () {
+    runCalculator(["9"], "+", ["8"]);
     cy.get("#total").should("have.text", "17");
   });
 
   it("2개의 숫자에 대해 뺄셈이 가능하다.", function () {
-    cy.contains("9").click();
-    cy.contains("-").click();
-    cy.contains("8").click();
-    cy.get("#equal").click();
-
+    runCalculator(["9"], "-", ["8"]);
     cy.get("#total").should("have.text", "1");
   });
 
   it("2개의 숫자에 대해 곱셈이 가능하다.", function () {
-    cy.contains("9").click();
-    cy.contains("X").click();
-    cy.contains("8").click();
-    cy.get("#equal").click();
-
+    runCalculator(["9"], "X", ["8"]);
     cy.get("#total").should("have.text", "72");
   });
 
   it("2개의 숫자에 대해 나눗셈이 가능하다.", function () {
-    cy.contains("1").click();
-    cy.contains("0").click();
-    cy.contains("/").click();
-    cy.contains("5").click();
-    cy.get("#equal").click();
-
+    runCalculator(["1", "0"], "/", ["5"]);
     cy.get("#total").should("have.text", "2");
   });
 
   it("AC(All Clear)버튼을 누르면 0으로 초기화 한다.", function () {
-    cy.contains("1").click();
-    cy.contains("0").click();
-    cy.contains("/").click();
-    cy.contains("5").click();
+    runCalculator(["1", "0"], "/", ["5"]);
 
     cy.get(".modifier").click();
 
@@ -61,11 +52,7 @@ describe("Calculator Test", function () {
   });
 
   it("계산 결과를 표현할 때 소수점 이하는 버림한다.", function () {
-    cy.contains("1").click();
-    cy.contains("0").click();
-    cy.contains("/").click();
-    cy.contains("3").click();
-    cy.get("#equal").click();
+    runCalculator(["1", "0"], "/", ["3"]);
 
     cy.get("#total").should("have.text", "3");
   });
