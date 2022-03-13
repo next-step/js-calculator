47462a1231efa71f11570f112263f7d84243e8c0
정현수 <jung660317@naver.com>
2022-03-13 14:42:39 +0900

    test: cypress command 생성 및 테스트 수정
    - cypress commands를 생성해서 함수를 만듦
    - gitignore에서 cypress/support 제거

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,15 +1,4 @@
-import { MESSAGE, DOM, OPERATION, MODIFIER, INIT_STATE } from '../../src/js/constants.js';
-
-const CLICK_DIGIT = digit => cy.get(`${DOM.digits} > ${DOM.digit}`).contains(digit).click();
-const CLICK_OPERATOR = operator =>
-  cy.get(`${DOM.operations} > ${DOM.operation}`).contains(operator).click();
-const CLICK_MODIFIER = modifier =>
-  cy.get(`${DOM.modifiers} > ${DOM.modifier}`).contains(modifier).click();
-const CHECK_ALERT_MESSAGE = message =>
-  cy.on('window:alert', text => {
-    expect(text).to.contains(message);
-  });
-const THE_RESULT_SHOULD_BE = result => cy.get(DOM.total).should('have.text', result);
+import { MESSAGE, OPERATION, MODIFIER, INIT_STATE } from '../../src/js/constants.js';
 
 describe('js-calculator', () => {
   beforeEach(() => {
@@ -17,59 +6,59 @@ describe('js-calculator', () => {
   });
 
   it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
-    CLICK_DIGIT('1');
-    CLICK_OPERATOR(OPERATION.plus);
-    CLICK_DIGIT('2');
-    CLICK_OPERATOR(OPERATION.equal);
-    THE_RESULT_SHOULD_BE('3');
+    cy.clickDigit('1');
+    cy.clickOperation(OPERATION.plus);
+    cy.clickDigit('2');
+    cy.clickOperation(OPERATION.equal);
+    cy.totalShouldBe('3');
   });
 
   it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
-    CLICK_DIGIT('3');
-    CLICK_OPERATOR(OPERATION.minus);
-    CLICK_DIGIT('2');
-    CLICK_OPERATOR(OPERATION.equal);
-    THE_RESULT_SHOULD_BE('1');
+    cy.clickDigit('3');
+    cy.clickOperation(OPERATION.minus);
+    cy.clickDigit('2');
+    cy.clickOperation(OPERATION.equal);
+    cy.totalShouldBe('1');
   });
 
   it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
-    CLICK_DIGIT('3');
-    CLICK_OPERATOR(OPERATION.multiple);
-    CLICK_DIGIT('2');
-    CLICK_OPERATOR(OPERATION.equal);
-    THE_RESULT_SHOULD_BE('6');
+    cy.clickDigit('3');
+    cy.clickOperation(OPERATION.multiple);
+    cy.clickDigit('2');
+    cy.clickOperation(OPERATION.equal);
+    cy.totalShouldBe('6');
   });
 
   it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
-    CLICK_DIGIT('6');
-    CLICK_OPERATOR(OPERATION.division);
-    CLICK_DIGIT('2');
-    CLICK_OPERATOR(OPERATION.equal);
-    THE_RESULT_SHOULD_BE('3');
+    cy.clickDigit('6');
+    cy.clickOperation(OPERATION.division);
+    cy.clickDigit('2');
+    cy.clickOperation(OPERATION.equal);
+    cy.totalShouldBe('3');
   });
 
   it(`AC(All Clear)버튼을 누르면 ${INIT_STATE.currentTotal}으로 초기화 한다.`, () => {
-    CLICK_DIGIT('6');
-    CLICK_DIGIT('2');
-    CLICK_MODIFIER(MODIFIER.allClear);
-    THE_RESULT_SHOULD_BE(INIT_STATE.currentTotal);
+    cy.clickDigit('6');
+    cy.clickDigit('2');
+    cy.clickModifier(MODIFIER.allClear);
+    cy.totalShouldBe(INIT_STATE.currentTotal);
   });
 
   it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
-    CLICK_DIGIT('6');
-    CLICK_DIGIT('2');
-    CLICK_DIGIT('2');
-    CLICK_DIGIT('2');
-    CHECK_ALERT_MESSAGE(MESSAGE.numberCanBeEnteredUpToThreeDigitsAtOnce);
-    THE_RESULT_SHOULD_BE('622');
+    cy.clickDigit('6');
+    cy.clickDigit('2');
+    cy.clickDigit('2');
+    cy.clickDigit('2');
+    cy.checkAlertMessage(MESSAGE.numberCanBeEnteredUpToThreeDigitsAtOnce);
+    cy.totalShouldBe('622');
   });
 
   it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
-    CLICK_DIGIT('1');
-    CLICK_DIGIT('2');
-    CLICK_OPERATOR(OPERATION.division);
-    CLICK_DIGIT('5');
-    CLICK_OPERATOR(OPERATION.equal);
-    THE_RESULT_SHOULD_BE('2');
+    cy.clickDigit('1');
+    cy.clickDigit('2');
+    cy.clickOperation(OPERATION.division);
+    cy.clickDigit('5');
+    cy.clickOperation(OPERATION.equal);
+    cy.totalShouldBe('2');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,21 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { DOM } from '../../src/js/constants.js';
+
+Cypress.Commands.add('clickDigit', digit =>
+  cy.get(`${DOM.digits} > ${DOM.digit}`).contains(digit).click(),
+);
+
+Cypress.Commands.add('clickOperation', operation =>
+  cy.get(`${DOM.operations} > ${DOM.operation}`).contains(operation).click(),
+);
+
+Cypress.Commands.add('clickModifier', modifier =>
+  cy.get(`${DOM.modifiers} > ${DOM.modifier}`).contains(modifier).click(),
+);
+
+Cypress.Commands.add('checkAlertMessage', message =>
+  cy.on('window:alert', text => {
+    expect(text).to.contains(message);
+  }),
+);
+
+Cypress.Commands.add('totalShouldBe', result => cy.get(DOM.total).should('have.text', result));
