c2c8376dd451720cd485934dbd6c12d16ecefef9
parksil0 <sirisiri0809@gmail.com>
2022-03-14 00:35:23 +0900

    feat : 단순 계산 로직 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,56 @@
 import { qs, qsAll, on } from './helpers.js';
 
+const total = qs('#total');
 const digits = qsAll('.digit');
 const operations = qsAll('.operation');
 
 digits.forEach((element) => {
   on(element, 'click', () => {
-    console.log(element.innerText);
+    const totalValue = total.innerText;
+    const digitValue = element.innerText;
+    const prevTotal = total.innerText;
+
+    console.log(totalValue, digitValue, prevTotal);
+
+    total.innerText = prevTotal === '0' ? digitValue : prevTotal + digitValue;
   });
 });
 
 operations.forEach((element) => {
   on(element, 'click', () => {
-    console.log(element.innerText);
+    const totalValue = total.innerText;
+    const operationValue = element.innerText;
+
+    const prevTotal = total.innerText;
+    if (operationValue === '=') {
+      const calculatingTargets = totalValue.split(/[X+-/_]/);
+      const operation = getOperator(totalValue);
+      const result = calculate(calculatingTargets, operation);
+      total.innerText = result;
+      return;
+    }
+    total.innerText = prevTotal + operationValue;
   });
 });
+
+const calculate = (calculatingTargets, operation) => {
+  const [leftValue, rightValue] = calculatingTargets;
+
+  switch (operation) {
+    case '+':
+      return Number(leftValue) + Number(rightValue);
+    case '-':
+      return Number(leftValue) - Number(rightValue);
+    case 'X':
+      return Number(leftValue) * Number(rightValue);
+    case '/':
+      return Number(leftValue) / Number(rightValue);
+  }
+};
+
+const getOperator = (totalValue) => {
+  for (const value of totalValue) {
+    if (value === '+' || value === '-' || value === 'X' || value === '/')
+      return value;
+  }
+};
