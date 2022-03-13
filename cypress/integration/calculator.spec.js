3b8c8649e1cc6794545cb272d78720c45406d960
parksil0 <sirisiri0809@gmail.com>
2022-03-13 23:29:52 +0900

    test : 요구사항 테스트 케이스 추가

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -2,7 +2,21 @@ describe('계산기 초기 화면 테스트', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5500/index.html');
   });
+
   it('total의 초기 값은 0이다.', () => {
     cy.get('#total').should('have.text', '0');
+  });
+});
+
+describe('계산기 기능 요구사항', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+  });
+
+  it('2개의 숫자에 대해 덧셈이 가능하다', () => {
+    cy.get('.digit').contains('5').click();
+    cy.get('.operation').contains('+').click();
+    cy.get('.digit').contains('3').click();
+    cy.get('#total').should('have.text', '8');
   });
 });
