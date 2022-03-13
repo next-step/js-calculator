214c9d8f59a03c674bbcc13597442cf2bf254850
karl <elrion018@gmail.com>
2022-03-13 18:09:40 +0900

    feat : 덧셈에 대한 테스트 추가

--- cypress/integration/calculator_spec.js
+++ cypress/integration/calculator_spec.js
@@ -1,11 +1,27 @@
 /// <reference types="Cypress" />
 
 describe('계산기 테스트', () => {
+  const clickNumberButton = (number) =>
+    cy.get(`button[data-number='${number}']`).click();
+  const clickOperatorButton = (operator) =>
+    cy.get(`button[data-operator='${operator}']`).click();
+  const resultShouldBeExpected = (expected) =>
+    cy.get('#total').should('have.text', expected);
+  const clickAllClearButton = () => cy.get(`button[data-ac=ac]`).click();
+
   beforeEach(() => {
     cy.visit('/');
   });
 
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
+  afterEach(() => {
+    clickAllClearButton();
+  });
+
+  it('2개의 숫자에 대해 덧셈이 가능하다', () => {
+    clickNumberButton(1);
+    clickOperatorButton('+');
+    clickNumberButton(2);
+    clickOperatorButton('=');
+    resultShouldBeExpected(3);
   });
 });
