0534f445c75fcfa48e95764030041b3dc4981506
parksil0 <sirisiri0809@gmail.com>
2022-03-14 01:30:55 +0900

    feat : 최대 세자리수 입력 관련 유효성검사 로직 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import { ERROR_MESSAGE } from './constants.js';
 import { qs, qsAll, on } from './helpers.js';
 
 const total = qs('#total');
@@ -9,11 +10,13 @@ digits.forEach((element) => {
   on(element, 'click', () => {
     const totalValue = total.innerText;
     const digitValue = element.innerText;
-    const prevTotal = total.innerText;
 
-    console.log(totalValue, digitValue, prevTotal);
+    if (checkOverThreeDigits(totalValue, digitValue)) {
+      alert(ERROR_MESSAGE.OVER_THREE_DIGITS);
+      return;
+    }
 
-    total.innerText = prevTotal === '0' ? digitValue : prevTotal + digitValue;
+    total.innerText = totalValue === '0' ? digitValue : totalValue + digitValue;
   });
 });
 
@@ -58,4 +61,10 @@ const getOperator = (totalValue) => {
     if (value === '+' || value === '-' || value === 'X' || value === '/')
       return value;
   }
+};
+
+const checkOverThreeDigits = (totalValue, digitValue) => {
+  const digitValues = totalValue.split(/[X+-/_]/);
+  const target = digitValues[digitValues.length - 1];
+  return (target + digitValue).length === 4;
 };
