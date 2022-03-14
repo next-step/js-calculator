679a036beac2330ebaa9abc0335d373c95bafc1e
tolluset <dlqud19@gmail.com>
2022-03-14 19:27:18 +0900

    refactor(test): use wrapper function

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,89 +1,56 @@
-before(() => cy.visit('../../dist/index.html'));
-
 const clickNumber = (num) => cy.get('.digits').contains(num).click();
 const clickOperation = (oper) => cy.get('.operations').contains(oper).click();
 const clickAllClear = () => cy.get('.modifiers').contains('AC').click();
 const checkTotal = (value) => cy.get('#total').should('have.text', value);
 
+const calculate = (prev, operator, next, total) => {
+  [...prev.toString()].forEach((p) => clickNumber(p));
+  clickOperation(operator);
+  [...next.toString()].forEach((p) => clickNumber(p));
+  clickOperation('=');
+  checkTotal(total);
+
+  return total;
+};
+
+before(() => cy.visit('../../dist/index.html'));
+
 describe('요구사항 테스트', () => {
   it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
-    clickNumber(3);
-    clickOperation('+');
-    clickNumber(4);
-    clickOperation('=');
-    checkTotal(7);
+    calculate(3, '+', 4, 7);
   });
 
   it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
-    clickNumber(9);
-    clickOperation('-');
-    clickNumber(1);
-    clickOperation('=');
-    checkTotal(8);
+    calculate(9, '-', 1, 8);
   });
 
   it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
-    clickNumber(3);
-    clickOperation('X');
-    clickNumber(4);
-    clickOperation('=');
-    checkTotal(12);
+    calculate(3, 'X', 4, 12);
   });
 
   it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
-    clickNumber(3);
-    clickOperation('/');
-    clickNumber(4);
-    clickOperation('=');
-    checkTotal(0);
+    calculate(3, '/', 4, 0);
   });
 
   it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
-    clickNumber(3);
-    clickOperation('-');
-    clickNumber(1);
-    clickOperation('=');
-    checkTotal(2);
+    calculate(3, '-', 1, 2);
     clickAllClear();
+    checkTotal(0);
   });
 
   it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
-    clickNumber(1);
-    clickNumber(2);
-    clickNumber(3);
-    clickOperation('+');
-    clickNumber(4);
-    clickNumber(5);
-    clickNumber(6);
-    clickOperation('=');
-    checkTotal(579);
+    calculate(123, '+', 456, 579);
   });
 
   it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
-    clickNumber(3);
-    clickNumber(1);
-    clickOperation('/');
-    clickNumber(7);
-    clickOperation('=');
-    checkTotal(4);
+    calculate(31, '/', 7, 4);
   });
 });
 
 describe('추가사항 테스트', () => {
   it('연속적인 연산이 가능하다.', () => {
-    clickNumber(1);
-    clickNumber(2);
-    clickNumber(3);
-    clickOperation('+');
-    clickNumber(4);
-    clickNumber(5);
-    clickNumber(6);
-    clickOperation('-');
-    clickNumber(7);
-    clickNumber(8);
-    clickNumber(9);
-    clickOperation('=');
-    checkTotal(-210);
+    const total = calculate(123, '+', 456, 579);
+    calculate(total, '-', 789, -210);
   });
 
   it('마지막 값이 연산자인 경우에도 연산이 가능하다.', () => {
@@ -96,26 +63,13 @@ describe('추가사항 테스트', () => {
   });
 
   it('연산 이후 값을 입력하면 새로운 연산과정을 할 수 있다.', () => {
-    clickNumber(3);
-    clickOperation('X');
-    clickNumber(4);
-    clickOperation('=');
-    checkTotal(12);
-    clickNumber(9);
-    clickNumber(9);
-    clickOperation('-');
-    clickNumber(8);
-    clickNumber(8);
-    clickOperation('=');
-    checkTotal(11);
+    calculate(3, 'X', 4, 12);
+    calculate(99, '-', 88, 11);
   });
 
   it('연산 이후 연산자를 입력하면 새로운 연산과정을 할 수 있다.', () => {
-    clickNumber(3);
-    clickOperation('X');
-    clickNumber(4);
-    clickOperation('=');
-    checkTotal(12);
+    calculate(3, 'X', 4, 12);
+
     clickOperation('-');
     clickNumber(8);
     clickOperation('=');
@@ -124,11 +78,9 @@ describe('추가사항 테스트', () => {
 
   it('0을 입력 후 연산과정을 할 수 있다.', () => {
     clickNumber(0);
-    clickNumber(3);
-    clickOperation('X');
-    clickNumber(4);
-    clickOperation('=');
-    checkTotal(12);
+
+    calculate(3, 'X', 4, 12);
+
     clickOperation('-');
     clickNumber(8);
     clickOperation('=');
