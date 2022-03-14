190e44094d3013872237acd02da76c3a6029644a
taeyoungs <xoxodudwkd@gmail.com>
2022-03-14 20:56:31 +0900

    mod: 더 적극적인 상수 사용 피드백 반영

--- src/js/common/constants.js
+++ src/js/common/constants.js
@@ -1,4 +1,20 @@
-export const OPERAND_FIRST_MESSAGE =
-    '숫자를 먼저 입력한 후 연산자를 입력해주세요!';
-export const MAX_LENGTH_MESSAGE = '숫자는 세 자리까지만 입력 가능합니다!';
-export const ONLY_TWO_OPERAND_MESSAGE = '2개의 숫자 조합만 계산 가능합니다!';
+export const MESSAGE = {
+    OPERAND_FIRST: '숫자를 먼저 입력한 후 연산자를 입력해주세요!',
+    MAX_LENGTH: '숫자는 세 자리까지만 입력 가능합니다!',
+    ONLY_TWO_OPERAND: '2개의 숫자 조합만 계산 가능합니다!',
+};
+
+export const OPERATIONS = {
+    PLUS: '+',
+    MINUS: '-',
+    DIVIDE: '/',
+    MULTIPLE: 'X',
+    CALCULATE: '=',
+};
+
+export const CALCULATE = {
+    [OPERATIONS.PLUS]: (a, b) => +a + +b,
+    [OPERATIONS.MINUS]: (a, b) => +a - +b,
+    [OPERATIONS.MULTIPLE]: (a, b) => +a * +b,
+    [OPERATIONS.DIVIDE]: (a, b) => Math.floor(+a / +b),
+};
