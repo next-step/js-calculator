8bfdf3471dfde8c4b8539f03ff0608edcc94d366
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-03-14 20:08:20 +0900

    chore: add data attribute to index.html

---
+++ cypress/fixtures/calculator.spec.js
@@ -0,0 +1,7 @@
+import { range } from "../../src/js/utils/Range";
+
+const BASE_URL = cy.config('baseUrl');
+
+context("calculator", () => {
+    // here...
+})
---
+++ src/js/constants/calculator.js
@@ -0,0 +1,13 @@
+export const MODIFIER = {
+    AC: 'all_clear'
+}
+
+export const OPERATION = {
+    DIVISION: "division",
+    MULTIPLICATION: "multiplication",
+    SUBTRACTION: "subtraction",
+    ADDITION: "addition",
+    CALCULATION: "calculation"
+}
+
+export const DEFAULT_NUMBER = 0;
