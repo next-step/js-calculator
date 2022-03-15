c19bfcf650f6c834e428af0e0f7108d16f768550
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-03-15 19:19:41 +0900

    feat: 중복 연산자 벨리데이션 추가

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,10 +1,20 @@
 import { MAX_LENGTH } from './constants.js';
 
 export const INVALID_LENGTH = `숫자는 ${MAX_LENGTH}자리까지만 입력 가능합니다!`;
+export const NOT_OVERLAP_NUMBER = `연산자는 중복입력이 불가능합니다.`;
 
 export const isOverMaxNumber = (operators) => {
   if (operators.length > MAX_LENGTH - 1) {
     alert(INVALID_LENGTH);
+    return true;
+  }
+
+  return false;
+};
+
+export const isOverlapOperator = (num2,operator,clikckOperator) => {
+  if ((!num2 && operator) || (!num2 && clikckOperator===OPERATOR.EQUAL)) {
+    alert(NOT_OVERLAP_NUMBER);
     return true;
   }
 
