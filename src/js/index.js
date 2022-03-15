fb01c58fc3954bd55d8dc14d48929e2a7a75e7e8
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-03-15 20:44:22 +0900

    feat: index.js to connect querySelector and init

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,10 @@
-import { putNumber } from './srcs/putNumber.js';
-import { putOperator } from './srcs/putOperator.js';
-import { putResult } from './srcs/putResult.js';
-import { clearDisplay } from './srcs/clearDisplay.js';
+import Calculator from "./calculator.js";
 
-export default function Calculator() {
-  const $digits = document.querySelector('.digits');
-  const $operations = document.querySelector('.operations');
-  const $equalSign = document.querySelector('#equal-sign');
-  const $modifier = document.querySelector('.modifiers');
-  const $total = document.querySelector('#total');
+const calculator = new Calculator({
+  $total: document.querySelector("#total"),
+  $digits: document.querySelector(".js-digits"),
+  $operations: document.querySelector(".js-operations"),
+  $modifiers: document.querySelector(".js-modifiers"),
+});
 
-  $digits.addEventListener('click', putNumber);
-  $operations.addEventListener('click', putOperator);
-  $equalSign.addEventListener('click', putResult);
-  $modifier.addEventListener('click', clearDisplay);
-}
-
-window.onload = () => {
-  new Calculator();
-};
+calculator.init();
