400b019ecdb9edbb18655b4ae3d7f05eb67f98be
largeKim <kdw4458@gmail.com>
2022-06-29 21:11:54 +0900

    :sparkles: :white_check_mark: 기본 연산자 함수 추가, 요구사항 일부 테스트

---
+++ src/js/util/operators.js
@@ -0,0 +1,9 @@
+const add = (preOperand, postOperand) => preOperand + postOperand;
+
+const subtract = (preOperand, postOperand) => preOperand - postOperand;
+
+const multiply = (preOperand, postOperand) => preOperand * postOperand;
+
+const divide = (preOperand, postOperand) => preOperand / postOperand;
+
+export { add, subtract, multiply, divide };
