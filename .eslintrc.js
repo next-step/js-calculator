915b6b3b30d2a777a4a0bf91c0b07718d367d8c5
Charlie <ickyicky@gmail.com>
2022-06-29 17:33:57 +0900

    린트 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,29 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: [
+    'html',
+  ],
   rules: {
+    indent: ['error', 2],
+    'object-curly-newline': [
+      'error', {
+        ObjectExpression: 'always',
+        ObjectPattern: {
+          multiline: true,
+        },
+        ImportDeclaration: 'never',
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+      },
+    ],
+    'array-element-newline': ['error', {
+      ArrayExpression: 'consistent',
+      ArrayPattern: {
+        minItems: 3,
+      },
+    }],
   },
 };
