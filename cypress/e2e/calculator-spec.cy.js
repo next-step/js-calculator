18de6e56b88430be159d1c6de422d4fda0db19ab
junzero741 <junzero741@gmail.com>
2022-06-29 20:44:08 +0900

    [CHORE] : 테스트 코드 파일 내에 선택자 상수 선언 및 주석 작성 1. 테스트해야 할 task 목록 주석으로 작성했음

--- cypress/e2e/calculator-spec.cy.js
+++ cypress/e2e/calculator-spec.cy.js
@@ -1,3 +1,19 @@
+const totalSelector = '[id="total"]';
+const digitWrapperSelector = '[class="digits"]';
+const digitSelector = '[class="digit"]';
+const modifierSelector = '[class="modifier"]';
+const operationWrapperSelector = '[class="operations"]';
+const operationSelector = '[class="operation"]';
+
+// [] 2개의 숫자에 대해 덧셈이 가능하다.
+// [] 2개의 숫자에 대해 뺄셈이 가능하다.
+// [] 2개의 숫자에 대해 곱셈이 가능하다.
+// [] 2개의 숫자에 대해 나눗셈이 가능하다.
+// [] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
+// [] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
+// [] 계산 결과를 표현할 때 소수점 이하는 버림한다.
+
+// 계산기 렌더링 테스트
 describe('calculator render test', () => {
 	it('calculator is rendered on index.html', () => {
 		cy.visit('/index.html');
