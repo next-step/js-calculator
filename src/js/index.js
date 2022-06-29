9a3baa1cdb7ecf9a8a7dd0a8695a465b1f6dace5
junzero741 <junzero741@gmail.com>
2022-06-29 23:43:31 +0900

    [FEAT] : 버튼 엘리먼트들에 data-set 어트리뷰트 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-// window.alert('hello world');
+import { $, $$ } from './util.js';
+
+// 버튼 데이터 초기화
+(() => {
+	const digitList = $$('.digit');
+	const operationList = $$('.operation');
+	digitList.forEach((el) => (el.dataset.digitNumber = el.innerText));
+	operationList.forEach((el) => (el.dataset.operationRole = el.innerText));
+})();
