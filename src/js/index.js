e5a9f2437f5d1fc3440bc801776f2c9946bc3c6c
junzero741 <junzero741@gmail.com>
2022-06-30 23:11:20 +0900

    [FEAT] : updateTotalValue 함수 작성 1. 결과 창 내 값을 업데이트 하는 함수.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-import { $, $$ } from './util.js';
-import {DIGIT, OPERATION} from './selectors.js';
+import { $$ } from './util.js';
+import {DIGIT, OPERATION, TOTAL} from './selectors.js';
 
 // 버튼 데이터 초기화
 (() => {
@@ -8,3 +8,12 @@ import {DIGIT, OPERATION} from './selectors.js';
 	digitList.forEach((el) => (el.dataset.digitNumber = el.innerText));
 	operationList.forEach((el) => (el.dataset.operationRole = el.innerText));
 })();
+
+/* 
+	필요한 함수들 정의
+	정의 후 각 파일로 모듈화 할 생각입니다.
+*/
+const updateTotalValue = function (value) {
+	const total = $(TOTAL)
+	total.innerText = value;
+}
