4efa9ea65b106ea41ca7e6fe1682e8429c35cd94
junzero741 <junzero741@gmail.com>
2022-06-30 22:29:34 +0900

    [REFAC] : 텍스트 리터럴 선택자 대신 상수 불러와 사용하도록 리팩터링

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 import { $, $$ } from './util.js';
+import {DIGIT, OPERATION} from './selectors.js';
 
 // 버튼 데이터 초기화
 (() => {
-	const digitList = $$('.digit');
-	const operationList = $$('.operation');
+	const digitList = $$(DIGIT);
+	const operationList = $$(OPERATION);
 	digitList.forEach((el) => (el.dataset.digitNumber = el.innerText));
 	operationList.forEach((el) => (el.dataset.operationRole = el.innerText));
 })();
