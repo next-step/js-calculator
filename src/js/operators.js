6038ee2fcf53195c1252c8664a73476d39ab59e1
YunSeong Park <qkrdbstjd95@naver.com>
2022-07-01 12:47:02 +0900

    [New Feature] operator 함수 및 아이콘 정의

---
+++ src/js/operators.js
@@ -0,0 +1,6 @@
+export const operators = {
+  add: { func: (num1, num2) => num1 + num2, icon: "+" },
+  minus: { func: (num1, num2) => num1 - num2, icon: "-" },
+  multiply: { func: (num1, num2) => num1 * num2, icon: "X" },
+  divide: { func: (num1, num2) => Math.floor(num1 / num2), icon: "/" },
+};
