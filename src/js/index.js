c88d016ec58e2b33fbdd2bfa49c2a0fdd225cc34
YunSeong Park <qkrdbstjd95@naver.com>
2022-07-01 18:36:30 +0900

    [New Feature] total render로직 구현

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,29 @@ class App {
     this.model = new Model();
     this.total = new Total(appElement.querySelector("#total"));
   }
+
+  renderTotal() {
+    const { num1, num2, operation } = this.model;
+
+    if (num1 === undefined) {
+      this.total.setDetaultLabel();
+      return;
+    }
+
+    const label = getLabel(num1, num2, operation);
+
+    this.total.setLabel(label);
+  }
 }
 
 new App(document.querySelector("#app"));
+
+const getLabel = (num1, num2, operation) => {
+  const numLabel1 = num1 === undefined ? "" : num1.toString();
+  const numLabel2 = num2 === undefined ? "" : num2.toString();
+
+  const operationLabel =
+    operation === undefined ? "" : operators[operation].icon;
+
+  return `${numLabel1}${operationLabel}${numLabel2}`;
+};
