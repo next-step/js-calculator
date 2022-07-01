0002ddee9dd17e14e34fa0eae8242d3b5d13278a
doinki <doinki94@gmail.com>
2022-07-01 15:28:10 +0900

    feat: calculator 기능 구현

---
+++ src/js/calculator.js
@@ -0,0 +1,143 @@
+const operators = {
+  ADD: '+',
+  SUBTRACT: '-',
+  MULTIPLY: 'X',
+  DIVIDE: '/',
+  AC: 'AC',
+  CALCULATE: '=',
+};
+const operatorPrecedences = {
+  [operators.MULTIPLY]: 2,
+  [operators.DIVIDE]: 2,
+  [operators.ADD]: 1,
+  [operators.SUBTRACT]: 1,
+};
+const operatorFunctions = {
+  [operators.MULTIPLY]: (a, b) => b * a,
+  [operators.DIVIDE]: (a, b) => b / a,
+  [operators.ADD]: (a, b) => b + a,
+  [operators.SUBTRACT]: (a, b) => b - a,
+};
+export const errorMessages = Object.freeze({
+  SYNTAX_ERROR: '숫자를 먼저 입력한 후 연산자를 입력해주세요!',
+  MAX_LENGTH_ERROR: '숫자는 세 자리까지만 입력 가능합니다!',
+});
+
+class Calculator {
+  #buffer;
+
+  #infix;
+
+  #result;
+
+  get result() {
+    return this.#result;
+  }
+
+  constructor() {
+    this.clearAll();
+  }
+
+  #calculate() {
+    const postfix = Calculator.convertToPostfix(this.#infix);
+    let result = Calculator.calculatePostfix(postfix);
+
+    if (Number.isFinite(result)) result = parseInt(result, 10);
+
+    this.#result = result;
+    this.#buffer = result.toString();
+    this.#infix = [];
+  }
+
+  #pushBuffer() {
+    if (this.#buffer === '') return;
+
+    this.#infix.push(this.#buffer);
+    this.#buffer = '';
+  }
+
+  clearAll() {
+    this.#buffer = '';
+    this.#infix = [];
+    this.#result = 0;
+  }
+
+  push(value) {
+    // AC
+    if (value === operators.AC) {
+      this.clearAll();
+      return;
+    }
+
+    // Ifinity or NaN
+    if (!Number.isInteger(this.#result)) return;
+
+    // Number
+    if (Number.isInteger(+value)) {
+      if (this.#buffer.length >= 3)
+        throw new Error(errorMessages.MAX_LENGTH_ERROR);
+
+      // remove leading zero
+      this.#buffer = (+(this.#buffer + value)).toString();
+      return;
+    }
+
+    if (value === operators.CALCULATE) {
+      this.#pushBuffer();
+      this.#calculate();
+      return;
+    }
+
+    if (this.#buffer === '') throw new Error(errorMessages.SYNTAX_ERROR);
+
+    this.#pushBuffer();
+    this.#infix.push(value);
+  }
+
+  toString() {
+    return this.#infix.concat(this.#buffer).join('');
+  }
+
+  static convertToPostfix(infix) {
+    const postfix = [];
+    const stack = [];
+
+    infix.forEach((value) => {
+      if (Number.isInteger(+value)) {
+        postfix.push(value);
+        return;
+      }
+
+      while (
+        stack.length > 0 &&
+        operatorPrecedences[value] <=
+          operatorPrecedences[stack[stack.length - 1]]
+      ) {
+        postfix.push(stack.pop());
+      }
+
+      stack.push(value);
+    });
+
+    return postfix.concat(stack.reverse());
+  }
+
+  static calculatePostfix(postfix) {
+    const stack = [];
+
+    postfix.forEach((value) => {
+      if (Number.isInteger(+value)) {
+        stack.push(+value);
+        return;
+      }
+
+      const fn = operatorFunctions[value];
+
+      stack.push(fn(stack.pop(), stack.pop()));
+    });
+
+    return stack[0] ?? 0;
+  }
+}
+
+export default Calculator;
---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+import Calculator from './calculator.js';
+import $ from './utils.js';
+
+const calculator = new Calculator();
+const buttons = [$('.digits'), $('.modifiers'), $('.operations')];
+
+const renderResult = (string) => {
+  $('#total').textContent = string;
+};
+const onClick = (event) => {
+  try {
+    const { textContent } = event.target;
+    calculator.push(textContent);
+
+    renderResult(calculator.toString());
+  } catch (error) {
+    // eslint-disable-next-line no-alert
+    alert(error.message);
+  }
+};
+
+buttons.forEach((el) => {
+  el.addEventListener('click', onClick, true);
+});
+// AC
+$('.modifier').addEventListener('click', () => renderResult(calculator.result));
+// =
+$('.operation:last-child').addEventListener('click', () =>
+  renderResult(calculator.result)
+);
