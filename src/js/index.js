c1222f92f793ffbbc613efb394d562e5b7aeb38d
won junho <wnsgh1993@gmail.com>
2022-07-01 20:18:46 +0900

    delete /

--- src/js/index.js
+++ src/js/index.js
@@ -1,57 +1,61 @@
-import operate from './modules/operator.js'
+// import operate from './modules/operator.js'
 
 let number = ''
 let operator = ''
 let numberList = []
+export default function test() {
+    alert('a')
+}
 
-function caculator() {
+
+// function caculator() {
     
-    document.querySelectorAll('.digit').forEach(function(btn) {
-        btn.addEventListener('click', function() {
+//     document.querySelectorAll('.digit').forEach(function(btn) {
+//         btn.addEventListener('click', function() {
 
-            if(operator === '=') {
-                initialize()
-            }
+//             if(operator === '=') {
+//                 initialize()
+//             }
 
-            if(number.split('').length <= 2) {
-                if(!(number === '' && btn.innerText === '0')) {
-                    number = number.concat(btn.innerText)
-                    document.getElementById('total').innerText = number
-                }
-            }
-        })
-    })
+//             if(number.split('').length <= 2) {
+//                 if(!(number === '' && btn.innerText === '0')) {
+//                     number = number.concat(btn.innerText)
+//                     document.getElementById('total').innerText = number
+//                 }
+//             }
+//         })
+//     })
 
-    document.querySelectorAll('.operation').forEach(function(btn) {
-        btn.addEventListener('click', function() {
+//     document.querySelectorAll('.operation').forEach(function(btn) {
+//         btn.addEventListener('click', function() {
 
-            if(number !== '') {
-                numberList.push(number)
-                number = ''
-            }
-            if(numberList.length === 1) {
-                operator = btn.innerText
-            }
+//             if(number !== '') {
+//                 numberList.push(number)
+//                 number = ''
+//             }
+//             if(numberList.length === 1) {
+//                 operator = btn.innerText
+//             }
             
-            if(numberList.length === 2) {
-                let result = operate(numberList, operator) //operator : previous operator to avoid "=" operator
-                numberList = []
-                numberList.push(result) //get a result number in numberList index 0
-                operator = btn.innerText
-            }
-        })
-    })
+//             if(numberList.length === 2) {
+//                 let result = operate(numberList, operator) //operator : previous operator to avoid "=" operator
+//                 numberList = []
+//                 numberList.push(result) //get a result number in numberList index 0
+//                 operator = btn.innerText
+//             }
+//         })
+//     })
     
-    document.querySelector('.modifier').addEventListener('click', function(btn) {
-        initialize()
-        document.getElementById('total').innerText = 0
-    })
-}
+//     document.querySelector('.modifier').addEventListener('click', function(btn) {
+//         initialize()
+//         document.getElementById('total').innerText = 0
+//     })
+// }
 
-function initialize() {
-    operator = ''
-    number = ''
-    numberList = []
-}
+// function initialize() {
+//     operator = ''
+//     number = ''
+//     numberList = []
+// }
 
-caculator();
+// caculator();
