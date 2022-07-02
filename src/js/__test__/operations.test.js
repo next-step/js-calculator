ef0bebc255779639689859e0e7c6af37fc1926e7
Jaemin Ahn <jae7415@gmail.com>
2022-07-02 20:36:16 +0900

    use ESM in test environment

--- src/js/__test__/operations.test.js
+++ src/js/__test__/operations.test.js
@@ -1,4 +1,4 @@
-const { sum, subtract, multiple, divide } = require("../operations.js");
+import { sum, subtract, multiple, divide } from "../operations.js";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
---
+++ src/js/operations.js
@@ -0,0 +1,15 @@
+export const sum = (a, b) => {
+  return a + b;
+};
+
+export const subtract = (a, b) => {
+  return a - b;
+};
+
+export const multiple = (a, b) => {
+  return a * b;
+};
+
+export const divide = (a, b) => {
+  return Math.floor(a / b);
+};
