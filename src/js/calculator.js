2c8fdafc28a3084f456f298be89f251e8cfe8455
ag502 <ag502@naver.com>
2022-07-02 20:32:14 +0900

    add: 숫자 계산 로직 추가

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -6,8 +6,10 @@ class CalCulator {
     this.$numberDisplay = document.querySelector("#total");
 
     this.currentOperator = null;
-    this.inputNumbers = [null, null];
+    this.leftNumber = null;
+    this.rightNumber = null;
     this.isReset = false;
+    this.isSolved = false;
 
     this.initEvent();
   }
@@ -25,25 +27,67 @@ class CalCulator {
 
   handleOperatorBtnClick(e) {
     const { target } = e;
-    this.currentOperator = target.innerText;
+
+    if (target.innerText === "=") {
+      this.handleCalcBtnClick();
+    } else {
+      this.leftNumber = Number(this.$numberDisplay.innerText);
+      this.currentOperator = target.innerText;
+    }
+  }
+
+  handleCalcBtnClick() {
+    if (this.leftNumber === null || this.currentOperator === null) {
+      return;
+    }
+    this.rightNumber = Number(this.$numberDisplay.innerText);
+    const result = this.calculate();
+    this.isSolved = true;
+
+    this.updateDisplay(result);
   }
 
   resetDisplay() {
     this.$numberDisplay.innerText = 0;
     this.currentOperator = null;
     this.isReset = false;
+    this.isSolved = false;
   }
 
   updateDisplay(number) {
+    if (this.isSolved) {
+      this.$numberDisplay.innerText = number;
+      return;
+    }
+
     if (this.currentOperator && !this.isReset) {
       this.$numberDisplay.innerText = 0;
       this.isReset = true;
     }
+
     if (this.$numberDisplay.innerText.length >= 3) {
       return;
     }
+
     const nextNumber = Number(`${this.$numberDisplay.innerText}${number}`);
     this.$numberDisplay.innerText = nextNumber;
+  }
+
+  calculate() {
+    switch (this.currentOperator) {
+      case "/": {
+        return Math.floor(this.leftNumber / this.rightNumber);
+      }
+      case "X": {
+        return this.leftNumber * this.rightNumber;
+      }
+      case "-": {
+        return this.leftNumber - this.rightNumber;
+      }
+      case "+": {
+        return this.leftNumber + this.rightNumber;
+      }
+    }
   }
 }
 
