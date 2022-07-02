2c67d287f045dbef3c494d960648b0d86afcae17
wondonghwi <donghwi1289@gmail.com>
2022-07-02 16:24:48 +0900

    🔧 airbnb eslint, prettier 커스텀

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'no-restricted-syntax': ['error', 'ForInStatement'],
+    'no-alert': 'off',
+    'consistent-return': 'off',
+    'object-curly-newline': 'off',
+    'max-len': 'off',
   },
 };
