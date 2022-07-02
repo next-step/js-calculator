a76b68ccede1a5ed8c779a2349ad3cd21234e9ce
ag502 <ag502@naver.com>
2022-07-02 16:32:00 +0900

    add: 리셋 버튼에 이벤트 추가

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -2,10 +2,12 @@ class CalCulator {
   constructor() {
     const $numberBtnGrp = document.querySelector(".digits");
     const $operatorBtnGrp = document.querySelector(".operations");
+    const $resetBtn = document.querySelector(".modifier");
     this.$numberDisplay = document.querySelector("#total");
 
     $numberBtnGrp.addEventListener("click", this.handleNumberBtnClick.bind(this));
     $operatorBtnGrp.addEventListener("click", this.handleOperatorBtnClick);
+    $resetBtn.addEventListener("click", this.resetDisplay.bind(this));
   }
 
   handleNumberBtnClick(e) {
@@ -17,8 +19,12 @@ class CalCulator {
     const { target } = e;
   }
 
+  resetDisplay() {
+    this.$numberDisplay.innerHTML = 0;
+  }
+
   updateDisplay(number) {
-    const nextNumber = Number(`${$numberDisplay.innerText}${number}`);
+    const nextNumber = Number(`${this.$numberDisplay.innerText}${number}`);
     this.$numberDisplay.innerText = nextNumber;
   }
 }
