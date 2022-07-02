10ff5782a3a2413c8e65d785442f37ba9c358575
Jaemin Ahn <jae7415@gmail.com>
2022-07-02 19:49:55 +0900

    Setup test environment with jest

---
+++ src/js/operators.js
@@ -0,0 +1,22 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiple(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return Math.floor(a / b);
+}
+
+module.exports = {
+  sum,
+  subtract,
+  multiple,
+  divide,
+};
