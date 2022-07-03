50dc752852fbe29ad2467f262c0ec13adc6368e8
junzero741 <junzero741@gmail.com>
2022-07-03 23:17:59 +0900

    [FEAT] : 사칙연산 결과 model 에 선적용

--- src/js/index.js
+++ src/js/index.js
@@ -2,54 +2,75 @@ import { $, $$, addEvent } from './util.js';
 import { paintToDOM } from './view.js';
 import { DIGIT, DIGIT_WRAPPER, MODIFIER, OPERATION, OPERATION_WRAPPER, TOTAL } from './selectors.js';
 import { NUMBER_LENGTH_ALERT_MSG } from '../constants.js';
-
+import { store } from './store/calculator-store.js';
+import { add, minus, multiply, divide } from './calculate.js';
+import { setNumberA, setNumberB, setOperation, setTotal, resetAll } from './action/calculator-actions.js';
 /* 
 	필요한 함수들 정의
 	정의 후 각 파일로 모듈화 할 생각입니다.
 */
 
-const updateTotalValue = function (newValue) {
-	const totalElem = $(TOTAL);
+const isInputLengthValid = function (inputLength) {
+	const MAX_LENGTH = 3;
+	return inputLength < MAX_LENGTH;
+};
+
+const resetCalcultaor = () => {
 	const INITIAL_TOTAL_NUM = '0';
-	const currentTotal = totalElem.innerText;
-	if (newValue === INITIAL_TOTAL_NUM && currentTotal === INITIAL_TOTAL_NUM) {
-		return;
-	} else if (!isInputLengthValid(currentTotal.length)) {
-		window.alert(NUMBER_LENGTH_ALERT_MSG);
-		return;
-	} else if (currentTotal === INITIAL_TOTAL_NUM) {
-		paintToDOM(totalElem, newValue);
+	store.dispatch(resetAll());
+	paintToDOM($(TOTAL), INITIAL_TOTAL_NUM);
+};
+
+const saveNumberToStore = function (value) {
+	const { numberA, numberB, operation } = store.getState();
+	if (operation) {
+		store.dispatch(setNumberB(numberB + value));
 	} else {
-		paintToDOM(totalElem, currentTotal + newValue);
+		store.dispatch(setNumberA(numberA + value));
 	}
 };
 
-const isInputLengthValid = function (inputLength) {
-	const MAX_LENGTH = 3;
-	if (inputLength >= MAX_LENGTH) return false;
-	return true;
+const onClickDigit = function (ev) {
+	const clickedValue = ev.target.dataset.digitNumber;
+	saveNumberToStore(clickedValue);
 };
 
-const resetTotalToZero = () => {
-	const INITIAL_TOTAL_NUM = '0';
-	const total = $(TOTAL);
-	paintToDOM(total, INITIAL_TOTAL_NUM);
+const onClickOperation = function (ev) {
+	const { numberA, numberB } = store.getState();
+	if (!numberA && !numberB) return;
+	const clickedOperation = ev.target.dataset.operationRole;
+	if (clickedOperation === '=') {
+		setTotalValue(store.getState().operation);
+	}
+	store.dispatch(setOperation(clickedOperation));
 };
 
-// 초기화 IIFE
-(() => {
+const setTotalValue = function (operation) {
+	const { numberA, numberB } = store.getState();
+	switch (operation) {
+		case '+':
+			store.dispatch(setTotal(add(numberA, numberB)));
+			break;
+		case '-':
+			store.dispatch(setTotal(minus(numberA, numberB)));
+			break;
+		case 'X':
+			store.dispatch(setTotal(multiply(numberA, numberB)));
+			break;
+		case '/':
+			store.dispatch(setTotal(divide(numberA, numberB)));
+	}
+};
+
+const init = function () {
 	// 버튼 데이터 초기화
-	const digitList = $$(DIGIT);
-	const operationList = $$(OPERATION);
-	digitList.forEach((el) => (el.dataset.digitNumber = el.innerText));
-	operationList.forEach((el) => (el.dataset.operationRole = el.innerText));
+	$$(DIGIT).forEach((el) => (el.dataset.digitNumber = el.innerText));
+	$$(OPERATION).forEach((el) => (el.dataset.operationRole = el.innerText));
 
 	// 버튼 별 이벤트 부착
-	const digitsWrapper = $(DIGIT_WRAPPER);
-	const modifier = $(MODIFIER);
-	addEvent(digitsWrapper, 'click', (ev) => {
-		const clickedValue = ev.target.dataset.digitNumber;
-		updateTotalValue(clickedValue);
-	});
-	addEvent(modifier, 'click', resetTotalToZero);
-})();
+	addEvent($(DIGIT_WRAPPER), 'click', onClickDigit);
+	addEvent($(MODIFIER), 'click', resetCalcultaor);
+	addEvent($(OPERATION_WRAPPER), 'click', onClickOperation);
+};
+
+init();
