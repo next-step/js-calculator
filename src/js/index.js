0e9ca4fb9dd933b76cff09b74e0057479a05bcd5
ShimSoYoung <ellie.shim.s@gmail.com>
2022-07-04 01:11:59 +0900

    element에 클릭 이벤트리스너 추가

--- src/js/index.js
+++ src/js/index.js
@@ -54,3 +54,37 @@ class Calculator {
         return Math.floor(result)
     }
 }
+
+const calculator = new Calculator()
+
+const digitsEl = document.querySelectorAll('.digit');
+const operationsEl = document.querySelectorAll('.operation:not(#equal)');
+const equalEl = document.querySelector('#equal');
+const displayEl = document.querySelector('#total');
+const allClearEl = document.querySelector('.modifier');
+
+digitsEl.forEach(element => {
+    element.addEventListener('click', (e) => {
+        calculator.insertOperand(e.target.innerText);
+        displayEl.innerHTML = calculator.getDisplayText()
+    })
+});
+
+operationsEl.forEach(element => {
+    element.addEventListener('click', (e) => {
+        calculator.insertOperator(e.target.innerText);
+        displayEl.innerHTML = calculator.getDisplayText()
+    })
+});
+
+allClearEl.addEventListener('click', () => {
+    calculator.resetCalculator();
+    displayEl.innerHTML = 0
+})
+
+equalEl.addEventListener('click', () => {
+    const result = calculator.calculate();
+    displayEl.innerHTML = result;
+
+    calculator.resetCalculator();
+})
