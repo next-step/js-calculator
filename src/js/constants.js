b8e7a30aae248a542148f8f99669b819a6fefcdc
정대윤 <sensecodevalue@gmail.com>
2022-07-03 18:16:06 +0900

    feat: 상수 추가 파일

---
+++ src/js/constants.js
@@ -0,0 +1,10 @@
+export const DUMP_SEPARATOR = ' ';
+export const MAX_NUM_COUNT = 3;
+export const OPERATIONS_LABEL = {
+  DIV: '/',
+  MUL: 'X',
+  SUB: '-',
+  ADD: '+',
+  EQL: '=',
+};
+export const DISPLAY_ERROR = 'ERROR';
