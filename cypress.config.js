205a5147d686bf4b8e0a436f61cf5dddac9bd828
daeho <dlaeogh8572@gmail.com>
2022-07-03 15:36:20 +0900

    chore: eslint cypress plugin 추가, cypress config 설정

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  config: {
+    BASE_URL: 'http://localhost:5500/',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
