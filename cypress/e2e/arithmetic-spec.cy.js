7958e5158b31ee2179a7941ce56e574adef39bff
junzero741 <junzero741@gmail.com>
2022-07-03 23:17:10 +0900

    [TEST] : 사칙연산 함수의 인자 타입, 반환 타입을 문자열로 변경

--- cypress/e2e/arithmetic-spec.cy.js
+++ cypress/e2e/arithmetic-spec.cy.js
@@ -2,22 +2,22 @@ import { add, minus, multiply, divide } from '../../src/js/calculate';
 
 describe('arithmetic operation test', () => {
 	it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
-		const result = add(1, 2);
-		expect(result).to.equal(3);
+		const result = add('1', '2');
+		expect(result).to.equal('3');
 	});
 
 	it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
-		const result = minus(2, 1);
-		expect(result).to.equal(1);
+		const result = minus('2', '1');
+		expect(result).to.equal('1');
 	});
 
 	it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
-		const result = multiply(50, 2);
-		expect(result).to.equal(100);
+		const result = multiply('50', '2');
+		expect(result).to.equal('100');
 	});
 
 	it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
-		const result = divide(99, 3);
-		expect(result).to.equal(33);
+		const result = divide('12', '5');
+		expect(result).to.equal('2');
 	});
 });
--- src/js/calculate.js
+++ src/js/calculate.js
@@ -1,15 +1,15 @@
 export const add = function (numA, numB) {
-	return numA + numB;
+	return `${Number(numA) + Number(numB)}`;
 };
 
 export const minus = function (numA, numB) {
-	return numA - numB;
+	return `${Number(numA) - Number(numB)}`;
 };
 
 export const multiply = function (numA, numB) {
-	return numA * numB;
+	return `${Number(numA) * Number(numB)}`;
 };
 
 export const divide = function (numA, numB) {
-	return numA / numB;
+	return `${Math.floor(Number(numA) / Number(numB))}`;
 };
