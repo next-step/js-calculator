551db505e53c81c413992eca9970fe897e9c88af
ag502 <ag502@naver.com>
2022-07-03 14:54:58 +0900

    add: 덧셈 테스트 코드 작성

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://127.0.0.1:8080",
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,32 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+const clickDigits = (numbers) => {
+  [...numbers].forEach((number) => {
+    clickDigit(number);
+  });
+};
+
+const clickDigit = (number) => {
+  cy.get(".digit").contains(number).click();
+};
+
+const clickOperator = (operator) => {
+  cy.get(".operation").contains(operator).click();
+};
+
+const getAnswer = (number) => {
+  cy.get(".total");
+};
+
+describe("계산기 요구사항 테스트", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("2개의 숫자에 대해 덧셈이 가능하다", () => {
+    clickDigits("90");
+    clickOperator("+");
+    clickDigits("81");
+    clickOperator("=");
+
+    cy.get("#total").contains("171");
+  });
+});
