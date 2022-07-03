77afb18a64ad32c28e87ad425d82d4b644d4b0d7
smchoxx <grit.developer.asher@gmail.com>
2022-07-03 13:36:33 +0900

    입력 숫자가 3자리 숫자를 넘으면 경고 문구가 표시된다

--- src/js/index.js
+++ src/js/index.js
@@ -6,20 +6,35 @@ let input = '';
 
 calculator.addEventListener('click', (e) => {
   if (e.target.tagName !== 'BUTTON') return;
+  input += e.target.innerText;
+
+  // AC 버튼 클릭 시 초기화
   if (e.target.innerText === 'AC') {
     total.innerText = '0';
     input = '';
     return;
   }
 
-  input += e.target.innerText;
-
+  // input 값에 '=' 문자열은 포함 시키지 않도록
   if (input.includes('=')) {
     input = input.replace('=', '');
   }
 
+  // 입력 숫자가 3자리를 넘어가면 경고문구
+  const regex = /[X/+-]/gi;
+  const numbers = input.split(regex);
+  let num1 = numbers[0];
+  let num2 = numbers[1];
+
+  if ((num1 && num1.length >= 4) || (num2 && num2.length >= 4)) {
+    alert('세자리 숫자까지만 입력이 가능합니다.');
+    input = input.slice(0, -1);
+    return;
+  }
+
   switch (e.target.className) {
     case 'digit':
+      // 최초 0 값은 입력이 불가
       if (input[0] === '0') {
         input = '';
         return;
@@ -27,6 +42,7 @@ calculator.addEventListener('click', (e) => {
       break;
 
     case 'operation':
+      // 최초 계산식에 관한 입력은 불가
       if (
         input[0] === '/' ||
         input[0] === 'X' ||
@@ -39,11 +55,8 @@ calculator.addEventListener('click', (e) => {
         return;
       }
 
+      // = 이 클릭되면 계산이 되도록
       if (e.target.innerText === '=') {
-        const seperator = /[X/+-]/gi;
-        const numbers = input.split(seperator);
-        const num1 = numbers[0];
-        const num2 = numbers[1];
         const calc = new Calculator(num1, num2);
 
         if (input.includes('+')) {
