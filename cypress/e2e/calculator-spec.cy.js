e1cb7b329ee2acb63eda060adf6ac44f361c4d79
junzero741 <junzero741@gmail.com>
2022-07-03 18:55:41 +0900

    [TEST] : 숫자 한번에 최대 3자리수까지 입력가능한지 테스트 코드 작성

--- cypress/e2e/calculator-spec.cy.js
+++ cypress/e2e/calculator-spec.cy.js
@@ -1,4 +1,5 @@
-import { CALCULATOR, TOTAL, dataSelector, MODIFIER } from '../../src/js/selectors';
+import { CALCULATOR, TOTAL, dataSelector, MODIFIER } from '../../src/js/selectors.js';
+import { NUMBER_LENGTH_ALERT_MSG } from '../../src/constants.js';
 
 describe('calculator-spec', () => {
 	it('계산기가 메인 화면에 렌더링된다.', () => {
@@ -18,17 +19,25 @@ describe('calculator-spec', () => {
 		}
 	});
 
-	it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {});
+	it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
+		cy.get(dataSelector('digit-number', '3')).click();
+		cy.get(dataSelector('digit-number', '9')).click();
+		cy.get(dataSelector('digit-number', '1')).click();
+		cy.get(dataSelector('digit-number', '8')).click();
+		cy.on('window:alert', (t) => {
+			expect(t).to.contains(NUMBER_LENGTH_ALERT_MSG);
+		});
+	});
 
-	it('한번에 숫자를 4자리 수 이상 입력하면 alert 창이 뜬다.', () => {});
+	// it('한번에 숫자를 4자리 수 이상 입력하면 alert 창이 뜬다.', () => {});
 
-	it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {});
+	// it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {});
 
-	it('2개의 숫자에 대해 덧셈이 가능하다.', () => {});
+	// it('2개의 숫자에 대해 덧셈이 가능하다.', () => {});
 
-	it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {});
+	// it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {});
 
-	it('2개의 숫자에 대해 곱셈이 가능하다.', () => {});
+	// it('2개의 숫자에 대해 곱셈이 가능하다.', () => {});
 
-	it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {});
+	// it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {});
 });
