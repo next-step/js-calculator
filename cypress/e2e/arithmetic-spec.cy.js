21d6f53424500a711014af38a011ff35bd32a7ec
junzero741 <junzero741@gmail.com>
2022-07-03 18:41:29 +0900

    [TEST] : 테스트 설명문 한글로 수정

--- cypress/e2e/arithmetic-spec.cy.js
+++ cypress/e2e/arithmetic-spec.cy.js
@@ -1,26 +1,22 @@
 import { add, minus, multiply, divide } from '../../src/js/calculate';
-// [] 2개의 숫자에 대해 덧셈이 가능하다.
-// [] 2개의 숫자에 대해 뺄셈이 가능하다.
-// [] 2개의 숫자에 대해 곱셈이 가능하다.
-// [] 2개의 숫자에 대해 나눗셈이 가능하다.
 
 describe('arithmetic operation test', () => {
-	it('add two numbers', () => {
+	it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
 		const result = add(1, 2);
 		expect(result).to.equal(3);
 	});
 
-	it('minus two numbers', () => {
+	it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
 		const result = minus(2, 1);
 		expect(result).to.equal(1);
 	});
 
-	it('multiply two numbers', () => {
+	it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
 		const result = multiply(50, 2);
 		expect(result).to.equal(100);
 	});
 
-	it('divide two numbers', () => {
+	it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
 		const result = divide(99, 3);
 		expect(result).to.equal(33);
 	});
--- cypress/e2e/calculator-spec.cy.js
+++ cypress/e2e/calculator-spec.cy.js
@@ -1,15 +1,12 @@
 import { CALCULATOR, TOTAL, dataSelector, MODIFIER } from '../../src/js/selectors';
 
 describe('calculator-spec', () => {
-	// 계산기가 메인 화면에 렌더링된다.
-	it('calculator is rendered on index.html', () => {
+	it('계산기가 메인 화면에 렌더링된다.', () => {
 		cy.visit('/index.html');
 		cy.get(CALCULATOR);
 	});
 
-	// AC(All Clear)버튼을 누르면 0으로 초기화 한다.
-	// AC 버튼을 클릭한 후 숫자 버튼을 입력하면 결과창이 입력한 숫자로 바뀐다.
-	it('click modifier and single digit changes total to digit', () => {
+	it('AC(All Clear)버튼을 누르면 결과값을 0으로 초기화 한다.', () => {
 		const INITIAL_TOTAL_NUM = '0';
 		for (let i = 0; i <= 9; i++) {
 			cy.get(MODIFIER).click();
@@ -21,17 +18,17 @@ describe('calculator-spec', () => {
 		}
 	});
 
-	// [] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
-	it('maximum click count of number is 3', () => {});
-	// [] 계산 결과를 표현할 때 소수점 이하는 버림한다.
-	it('total number should be floored', () => {});
+	it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {});
 
-	// [] 2개의 숫자에 대해 덧셈이 가능하다.
-	it('result of add two numbers is painted on total screen', () => {});
-	// [] 2개의 숫자에 대해 뺄셈이 가능하다.
-	it('result of minus two numbers is painted on total screen', () => {});
-	// [] 2개의 숫자에 대해 곱셈이 가능하다.
-	it('result of multiply two numbers is painted on total screen', () => {});
-	// [] 2개의 숫자에 대해 나눗셈이 가능하다.
-	it('result of divide two numbers is painted on total screen', () => {});
+	it('한번에 숫자를 4자리 수 이상 입력하면 alert 창이 뜬다.', () => {});
+
+	it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {});
+
+	it('2개의 숫자에 대해 덧셈이 가능하다.', () => {});
+
+	it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {});
+
+	it('2개의 숫자에 대해 곱셈이 가능하다.', () => {});
+
+	it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {});
 });
