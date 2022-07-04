fb418eb5543fdaf0bce71106b027b1f8c292cbe5
이국신 <nw4611@naver.com>
2022-07-04 09:26:53 +0900

    feat: view 모듈 분리

---
+++ src/js/view.js
@@ -0,0 +1,5 @@
+export const $total = document.querySelector("#total");
+export const $digits = document.querySelector(".digits");
+export const $modifier = document.querySelector(".modifiers");
+export const $operations = document.querySelector(".operations");
+export const $equal = document.querySelector("#equal");
