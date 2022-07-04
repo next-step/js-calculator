dc02e5e8cdf78c5484602b7cff5b32884c9cd5dd
smchoxx <grit.developer.asher@gmail.com>
2022-07-04 23:29:22 +0900

    테스트 코드 작성

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe('계산기 테스트', () => {
-  it('계산기 데모 페이지로 이동', () => {
+  beforeEach(() => {
     cy.visit('/');
   });
 
@@ -7,11 +7,38 @@ describe('계산기 테스트', () => {
     cy.get('.digit').contains('9').click();
     cy.get('.operation').contains('+').click();
     cy.get('.digit').contains('1').click();
-
     cy.get('#total').should('have.text', '9+1');
-
     cy.get('.operation').contains('=').click();
+    cy.get('#total').should('have.text', '10');
+  });
 
+  it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+    cy.get('.digit').contains('7').click();
+    cy.get('.operation').contains('-').click();
+    cy.get('.digit').contains('2').click();
+    cy.get('#total').should('have.text', '7-2');
+    cy.get('.operation').contains('=').click();
+    cy.get('#total').should('have.text', '5');
+  });
+
+  it('2개의 숫자에 대해 곱셉이 가능하다.', () => {
+    cy.get('.digit').contains('7').click();
+    cy.get('.operation').contains('X').click();
+    cy.get('.digit').contains('7').click();
+    cy.get('#total').should('have.text', '7X7');
+    cy.get('.operation').contains('=').click();
+    cy.get('#total').should('have.text', '49');
+  });
+
+  it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+    cy.get('.digit').contains('1').click();
+    cy.get('.digit').contains('0').click();
+    cy.get('.digit').contains('0').click();
+    cy.get('.operation').contains('/').click();
+    cy.get('.digit').contains('1').click();
+    cy.get('.digit').contains('0').click();
+    cy.get('#total').should('have.text', '100/10');
+    cy.get('.operation').contains('=').click();
     cy.get('#total').should('have.text', '10');
   });
 });
