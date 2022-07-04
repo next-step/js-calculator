e0b8581886286990d27e68f21e0374dc139bbb15
이국신 <nw4611@naver.com>
2022-07-04 09:27:49 +0900

    feat: 숫자 입력 동작 구현

---
+++ src/js/caculator.js
@@ -0,0 +1,30 @@
+import { $total } from "./view.js";
+import { DEFAULT_NUMBER, MAX_DIGIT_LENGTH, operators } from "./constants.js";
+
+export const isValidNumberInput = () => {
+  const expression = $total.innerText;
+
+  const operator = expression.split("").find((el) => Object.keys(operators).includes(el));
+
+  if (!operator) {
+    return expression.length < MAX_DIGIT_LENGTH;
+  }
+
+  const operands = expression.split(operator);
+
+  return operands[1].length < MAX_DIGIT_LENGTH;
+};
+
+export const inputNumber = ({ target }) => {
+  if ($total.innerText === DEFAULT_NUMBER) {
+    $total.innerText = target.innerText;
+    return;
+  }
+
+  if (!isValidNumberInput()) {
+    alert("숫자는 3자리이상 입력할 수 없습니다.");
+    return;
+  }
+
+  $total.innerText += target.innerText;
+};
