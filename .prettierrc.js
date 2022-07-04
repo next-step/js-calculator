cfd55fb6458bfc3fc1091115a2d689161938e226
JO YUN HO <yujo.dev@gmail.com>
2022-07-04 21:01:53 +0900

    Revert "[클린코드 3기 정대윤] 계산기 미션 (#128)"
    This reverts commit 0ba2a3ffdb616b307a76a79046325c626ccdecf0.

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: true,
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'es5',
-  tabWidth: 2,
-  arrowParens: 'avoid',
-};
-  
--- src/js/constants.js
+++
@@ -1,9 +0,0 @@
-export const MAX_NUM_COUNT = 3;
-export const OPERATIONS_LABEL = {
-  DIV: '/',
-  MUL: 'X',
-  SUB: '-',
-  ADD: '+',
-  EQL: '=',
-};
-export const DISPLAY_ERROR = 'ERROR';
