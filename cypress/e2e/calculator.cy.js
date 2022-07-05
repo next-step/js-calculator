d99b7e9d58877b160342817467869c6409df6884
doinki <doinki94@gmail.com>
2022-07-05 22:16:49 +0900

    test: test case 분리

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,90 +1,89 @@
 import { errorMessages } from '../../src/js/calculator';
 
 describe('calculator', () => {
-  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
 
-    cy.clicks('123+456');
+  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+    cy.clickNumber('1');
+    cy.clickNumber('2');
+    cy.clickNumber('3');
+    cy.clickOperator('+');
+    cy.clickNumber('4');
+    cy.clickNumber('5');
+    cy.clickNumber('6');
     cy.findByRole('heading').should('have.text', '123+456');
-    cy.clicks('=');
+
+    cy.calculate();
     cy.findByRole('heading').should('have.text', 123 + 456);
-    cy.clicks('+789');
-    cy.findByRole('heading').should('have.text', `${123 + 456}+789`);
-    cy.clicks('=');
-    cy.findByRole('heading').should('have.text', 123 + 456 + 789);
-    cy.clicks('AC', { atom: true });
-    cy.findByRole('heading').should('have.text', 0);
   });
 
   it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
-    cy.clicks('789-456');
-    cy.findByRole('heading').should('have.text', '789-456');
-    cy.clicks('=');
-    cy.findByRole('heading').should('have.text', 789 - 456);
-    cy.clicks('-123');
-    cy.findByRole('heading').should('have.text', `${789 - 456}-123`);
-    cy.clicks('=');
-    cy.findByRole('heading').should('have.text', 789 - 456 - 123);
-    cy.clicks('AC', { atom: true });
-    cy.findByRole('heading').should('have.text', 0);
+    cy.clickNumber('4');
+    cy.clickNumber('5');
+    cy.clickNumber('6');
+    cy.clickOperator('-');
+    cy.clickNumber('1');
+    cy.clickNumber('2');
+    cy.clickNumber('3');
+    cy.findByRole('heading').should('have.text', '456-123');
+
+    cy.calculate();
+    cy.findByRole('heading').should('have.text', 456 - 123);
   });
 
   it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
-    cy.clicks('12X45');
-    cy.findByRole('heading').should('have.text', '12X45');
-    cy.clicks('=');
-    cy.findByRole('heading').should('have.text', 12 * 45);
-    cy.clicks('X78');
-    cy.findByRole('heading').should('have.text', `${12 * 45}X78`);
-    cy.clicks('=');
-    cy.findByRole('heading').should('have.text', 12 * 45 * 78);
-    cy.clicks('AC', { atom: true });
-    cy.findByRole('heading').should('have.text', 0);
+    cy.clickNumber('1');
+    cy.clickNumber('2');
+    cy.clickOperator('X');
+    cy.clickNumber('3');
+    cy.clickNumber('4');
+    cy.findByRole('heading').should('have.text', '12X34');
+
+    cy.calculate();
+    cy.findByRole('heading').should('have.text', 12 * 34);
   });
 
   it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
-    cy.clicks('768/2');
-    cy.findByRole('heading').should('have.text', '768/2');
-    cy.clicks('=');
-    cy.findByRole('heading').should('have.text', 768 / 2);
-    cy.clicks('/3');
-    cy.findByRole('heading').should('have.text', `${768 / 2}/3`);
-    cy.clicks('=');
-    cy.findByRole('heading').should('have.text', 768 / 2 / 3);
-    cy.clicks('AC', { atom: true });
+    cy.clickNumber('1');
+    cy.clickNumber('2');
+    cy.clickOperator('/');
+    cy.clickNumber('3');
+    cy.findByRole('heading').should('have.text', '12/3');
+
+    cy.calculate();
+    cy.findByRole('heading').should('have.text', 12 / 3);
+  });
+
+  it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+    cy.clickNumber('1');
+    cy.clickNumber('5');
+    cy.clearAll();
     cy.findByRole('heading').should('have.text', 0);
   });
 
   it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
-    cy.clicks('1/3');
-    cy.findByRole('heading').should('have.text', '1/3');
-    cy.clicks('=');
-    cy.findByRole('heading').should('have.text', parseInt(1 / 3, 10));
-    cy.clicks('AC', { atom: true });
-    cy.findByRole('heading').should('have.text', 0);
-    cy.clicks('15/4');
+    cy.clickNumber('1');
+    cy.clickNumber('5');
+    cy.clickOperator('/');
+    cy.clickNumber('4');
     cy.findByRole('heading').should('have.text', '15/4');
-    cy.clicks('=');
+
+    cy.calculate();
     cy.findByRole('heading').should('have.text', parseInt(15 / 4, 10));
-    cy.clicks('AC', { atom: true });
-    cy.findByRole('heading').should('have.text', 0);
   });
 
   it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
-    const string = '1234';
+    const inputs = '1234';
 
-    cy.clicks(string);
     cy.on('window:alert', (text) => {
       expect(text).to.equal(errorMessages.MAX_LENGTH_ERROR);
     });
-    cy.findByRole('heading').should('have.text', string.substring(0, 3));
-    cy.clicks('X9=');
-    cy.findByRole('heading').should('have.text', +string.substring(0, 3) * 9);
-    cy.clicks('0');
-    cy.on('window:alert', (text) => {
-      expect(text).to.equal(errorMessages.MAX_LENGTH_ERROR);
+
+    inputs.split('').forEach((number) => {
+      cy.clickNumber(number);
     });
-    cy.clicks('AC', { atom: true });
-    cy.findByRole('heading').should('have.text', 0);
+    cy.findByRole('heading').should('have.text', inputs.substring(0, 3));
   });
 });
