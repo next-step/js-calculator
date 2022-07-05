1eb259fcc7355340342377dd60c5127f993dd923
jlee0505 <hj960322@gmail.com>
2022-07-05 18:36:38 +0900

    필요없는 else 문 제거

--- src/js/handlers/handleCalClicked.js
+++ src/js/handlers/handleCalClicked.js
@@ -1,15 +1,18 @@
-import { INITAL_VALUE, ERROR_MESSAGES, OPERATORS } from "../utils/constants.js";
+import {
+  INITIAL_VALUE,
+  ERROR_MESSAGES,
+  OPERATORS,
+} from "../utils/constants.js";
 import { TOTAL, DIGITS, OPERATIONS, ALLCLEAR } from "../utils/DOM.js";
-import { isValidInput, isValidLength } from "./validator.js";
+import { isAbleOperator, isValidLength } from "./validator.js";
 
 const displayDigit = (digit) => {
-  if (isValidLength()) {
+  if (isValidLength(TOTAL)) {
     TOTAL.textContent = digit;
     return;
-  } else {
-    alert(`${ERROR_MESSAGES.INVALID_DIGIT_LENGTH}`);
-    return;
   }
+  alert(`${ERROR_MESSAGES.INVALID_DIGIT_LENGTH}`);
+  return;
 };
 
 const displayOperation = (operator) => {
@@ -18,12 +21,11 @@ const displayOperation = (operator) => {
     return;
   }
 
-  if (isValidInput()) {
-    TOTAL.textContent = operator;
-  } else {
-    alert(`${ERROR_MESSAGES.INVALID_INPUT}`);
-    return;
+  if (isAbleOperator(TOTAL)) {
+    TOTAL.innerText = operator;
   }
+  alert(`${ERROR_MESSAGES.INVALID_INPUT}`);
+  return;
 };
 
 const displayTotal = () => {
@@ -36,16 +38,16 @@ const displayTotal = () => {
 
 // 하나의 함수가 여러 일을 하고 있다. -> 분리
 export const handleCalClicked = ({ target }) => {
-  if (target.classList.contains(".digit")) {
+  if (target.classList.contains("digit")) {
     displayDigit(target.textContent);
     return;
   }
-  if (target.classList.contains(".operation")) {
+  if (target.classList.contains("operation")) {
     displayOperation(target.textContent);
     return;
   }
-  if (target.classList.contains(".modifier")) {
-    TOTAL.textContent = INITAL_VALUE;
+  if (target.classList.contains("modifier")) {
+    TOTAL.textContent = INITIAL_VALUE;
     return;
   }
 };
