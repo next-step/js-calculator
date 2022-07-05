21de34534f7f0931f0b488b10604363d51d64fce
smchoxx <grit.developer.asher@gmail.com>
2022-07-05 23:22:15 +0900

    Custom Commands를 이용해 테스트 코드 리펙토링

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,41 +4,37 @@ describe('계산기 테스트', () => {
   });
 
   it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
-    cy.get('.digit').contains('9').click();
-    cy.get('.operation').contains('+').click();
-    cy.get('.digit').contains('1').click();
-    cy.get('#total').should('have.text', '9+1');
-    cy.get('.operation').contains('=').click();
-    cy.get('#total').should('have.text', '10');
+    cy.clickDigit('9');
+    cy.clickOperation('+');
+    cy.clickDigit('1');
+    cy.clickOperation('=');
+    cy.checkTotalValue('10');
   });
 
   it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
-    cy.get('.digit').contains('7').click();
-    cy.get('.operation').contains('-').click();
-    cy.get('.digit').contains('2').click();
-    cy.get('#total').should('have.text', '7-2');
-    cy.get('.operation').contains('=').click();
-    cy.get('#total').should('have.text', '5');
+    cy.clickDigit('7');
+    cy.clickOperation('-');
+    cy.clickDigit('2');
+    cy.clickOperation('=');
+    cy.checkTotalValue('5');
   });
 
   it('2개의 숫자에 대해 곱셉이 가능하다.', () => {
-    cy.get('.digit').contains('7').click();
-    cy.get('.operation').contains('X').click();
-    cy.get('.digit').contains('7').click();
-    cy.get('#total').should('have.text', '7X7');
-    cy.get('.operation').contains('=').click();
-    cy.get('#total').should('have.text', '49');
+    cy.clickDigit('7');
+    cy.clickOperation('X');
+    cy.clickDigit('7');
+    cy.clickOperation('=');
+    cy.checkTotalValue('49');
   });
 
   it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
-    cy.get('.digit').contains('1').click();
-    cy.get('.digit').contains('0').click();
-    cy.get('.digit').contains('0').click();
-    cy.get('.operation').contains('/').click();
-    cy.get('.digit').contains('1').click();
-    cy.get('.digit').contains('0').click();
-    cy.get('#total').should('have.text', '100/10');
-    cy.get('.operation').contains('=').click();
-    cy.get('#total').should('have.text', '10');
+    cy.clickDigit('1');
+    cy.clickDigit('0');
+    cy.clickDigit('0');
+    cy.clickOperation('/');
+    cy.clickDigit('1');
+    cy.clickDigit('0');
+    cy.clickOperation('=');
+    cy.checkTotalValue('10');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('clickDigit', (number) => {
+  cy.get('.digit').contains(number).click();
+});
+Cypress.Commands.add('clickOperation', (operator) => {
+  cy.get('.operation').contains(operator).click();
+});
+Cypress.Commands.add('checkTotalValue', (number) => {
+  cy.get('#total').should('have.text', number);
+});
