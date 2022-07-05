7aac1851e913c86052a8f651dc9dd755888dde65
won junho <wnsgh1993@gmail.com>
2022-07-05 13:15:31 +0900

    seperate into modules and set eslintrc config file

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,48 @@
-import operate from './modules/operator.js'
+import { clickOperator } from './modules/clickOperator.js';
+import { MAX_DIGITS_OF_NUMBERS } from './modules/const.js';
+import { clickNumbers } from './modules/clickNumber.js';
+// import {init} from './modules/initialize.js'
 
-let number = ''
-let operator = ''
+let number = '' //누적된 display number
+let currNumber = '' //선택한 숫자
 let numberList = []
-
-function caculator() {
-    clickNumber()
-    clickOperator()
-    modifier()
-}
-caculator()
-
-function clickNumber() {
-    document.querySelectorAll('.digit').forEach(function(btn) {
-        btn.addEventListener('click', function() {
-
-            if(operator === '=') {
-                initialize()
-            }
-
-            if(number.split('').length <= 2) {
-                if(!(number === '' && btn.innerText === '0')) {
-                    number = number.concat(btn.innerText)
-                    document.getElementById('total').innerText = number
-                }
-            }
-        })
-    })
-}
-
-function clickOperator() {
-    document.querySelectorAll('.operation').forEach(function(btn) {
-        btn.addEventListener('click', function() {
-
-            if(number !== '') {
-                numberList.push(number)
-                number = ''
-            }
-            if(numberList.length === 1) {
-                operator = btn.innerText
-            }
-            
-            if(numberList.length === 2) {
-                let result = operate(numberList, operator) //operator : previous operator to avoid "=" operator
-                numberList = []
-                numberList.push(result) //get a result number in numberList index 0
-                operator = btn.innerText
-            }
-        })
-    })
-}
-function modifier() {
-    document.querySelector('.modifier').addEventListener('click', function(btn) {
-        initialize()
-        document.getElementById('total').innerText = 0
-    })
-}
+let operators = []
 
 
-function initialize() {
-    operator = ''
-    number = ''
-    numberList = []
+
+// number = clickNumbers(MAX_DIGITS_OF_NUMBERS, OPERATOR, currOperator, number);
+// currOperator = clickOperator(number, numberList, currOperator);
+
+
+document.querySelector('.digits').addEventListener('click', function(event){
+
+	currNumber = event.target.innerText
+	number = document.getElementById('total').innerText
+	number = clickNumbers(MAX_DIGITS_OF_NUMBERS, numberList, number, currNumber)
+	console.log('number after clickNumbers is ', number)
+})
+
+document.querySelector('.operations').addEventListener('click', function(event) {
+	operators.push(event.target.innerText);
+
+	numberList = clickOperator(number, numberList, operators)
+	console.log('numberList is ', numberList, ' and operators = ', operators)
+})
+
+document.querySelector('.modifiers').addEventListener('click', function() {
+	console.log('init before is -> number = ', number, 'currNumber = ', currNumber, 'numberList = ', numberList, 'operators = ', operators)
+	
+	// eslint-disable-next-line no-unexpected-multiline
+	init()
+
+	console.log('init after is -> number = ', number, 'currNumber = ', currNumber, 'numberList = ', numberList, 'operators = ', operators)
+	
+})
+
+function init() {
+	document.getElementById('total').innerText = 0
+	number = '' //누적된 display number
+	currNumber = '' //선택한 숫자
+	numberList = []
+	operators = []
 }
