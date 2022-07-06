a9edb75ffd3de647b9ceeb4a0d96c823881a22f7
Leebonggu <leebonggu12@hanmail.net>
2022-07-06 14:16:22 +0900

    [리뷰반영] 필요없는 주석  제거

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,20 +1,5 @@
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { 
-
 import { SELECTORS_CLASS, SELECTORS_ID } from '../../src/js/constants';
 
-  
 Cypress.Commands.add('userClick', (selector,  value) => {
   cy.get(selector).contains(value).click();
 });
