8e16213feffff85f443eb6264dc17da6948cc92f
임 창묵 cmlim <limlim980625@gmail.com>
2022-07-27 00:13:16 +0900

    feat: 초기화 로직 적용

--- src/js/modifier.js
+++ src/js/modifier.js
@@ -1,8 +1,11 @@
 import { setState } from './state.js'
+import { render } from './index.js'
 
 const reset = () => {
   setState('input', '')
-  setState('inputArr', [])
+  setState('inputArr', ['0'])
+
+  render()
 }
 
 export { reset }
