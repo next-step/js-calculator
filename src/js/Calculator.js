0a364147eb4623821c51311a8d4743c006c94f3d
Hojoong Choi <inegg.apps@gmail.com>
2022-11-03 22:50:14 +0900

    feat: 계산기 클래스 작성 + 간단한 테스트(실패)

---
+++ src/js/Calculator.js
@@ -0,0 +1,16 @@
+class Calculator {
+  add(n, m) {
+    return n + m;
+  }
+  subtract(n, m) {
+    return n + m; // TODO:
+  }
+  multiply(n, m) {
+    return n + m; // TODO:
+  }
+  divide(n, m) {
+    return n + m; // TODO:
+  }
+}
+
+export default Calculator;
