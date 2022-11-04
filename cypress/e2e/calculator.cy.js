2fea6a6ffcd454a2d2a6c7dcd5ae1124c0b0635a
cloud <cloud@oysterable.com>
2022-11-05 08:25:46 +0900

    [Add] E2E test case - 덧셈, 뺄셈, 곱셈, 나눗셈,초기화, 3자리수, 소수점이하 절삭

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,7 +1,74 @@
-/// <reference types="cypress" />
-context('<Calculator />', () => {
-  it('test', () => {
+describe('<Calculator />', () => {
+  beforeEach(() => {
     cy.visit('/');
-    expect(1 + 1).equal(2);
+  });
+
+  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+    const [INPUT, RESULT] = ['234+123', '357'];
+    INPUT.forEach((value) => {
+      cy.contains('button', value).click();
+    });
+    cy.contains('button', '=').click();
+    cy.get('#total').should('eq', RESULT);
+  });
+
+  it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+    const [INPUT, RESULT] = ['234-123', '111'];
+    INPUT.forEach((value) => {
+      cy.contains('button', value).click();
+    });
+    cy.contains('button', '=').click();
+    cy.get('#total').should('eq', RESULT);
+  });
+
+  it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+    const [INPUT, RESULT] = ['234*123', '28782'];
+    INPUT.forEach((value) => {
+      cy.contains('button', value).click();
+    });
+    cy.contains('button', '=').click();
+    cy.get('#total').should('eq', RESULT);
+  });
+
+  it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+    const [INPUT, RESULT] = ['222/111', '2'];
+    INPUT.forEach((value) => {
+      cy.contains('button', value).click();
+    });
+    cy.contains('button', '=').click();
+    cy.get('#total').should('eq', RESULT);
+  });
+
+  it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+    const [INPUT, CLEAR_VALUE] = ['234', '0'];
+
+    INPUT.forEach((value) => {
+      cy.contains('button', value).click();
+    });
+    cy.contains('button', 'AC').click();
+    cy.get('#total').should('eq', CLEAR_VALUE);
+  });
+
+  it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
+    const [INPUT, EXTRA] = ['234', '5'];
+
+    INPUT.forEach((value) => {
+      cy.contains('button', value).click();
+    });
+    cy.contains('button', EXTRA).click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains('숫자는 세 자리까지만 입력 가능합니다!');
+    });
+    cy.get('#total').should('eq', INPUT);
+  });
+
+  it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+    const [INPUT, RESULT] = ['234/123', '2'];
+
+    INPUT.forEach((value) => {
+      cy.contains('button', value).click();
+    });
+    cy.contains('button', '=').click();
+    cy.get('#total').should('eq', RESULT);
   });
 });
