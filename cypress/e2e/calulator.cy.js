b450ecdbeb1da95a5fa44245e61dd26f662f9713
oyg0420 <oyg900420@gmail.com>
2022-11-06 20:29:00 +0900

    feat: calculator 테스트 코드 추가 작성
    1. cypress custom command 추가
    2. custom command 적용

--- cypress/e2e/calulator.cy.js
+++ cypress/e2e/calulator.cy.js
@@ -6,58 +6,67 @@ const OPERATOR = {
   ASSIGNMENT: '=',
 };
 
-/**
- * @todo 테스트 코드 작성
- * - [ ]  AC(All Clear)버튼을 누르면 0으로 초기화 한다.
- * - [ ]  숫자는 한번에 최대 3자리 수까지 입력 가능하다.
- * - [ ]  계산 결과를 표현할 때 소수점 이하는 버림한다.
- */
-
 describe('계산기 테스트', () => {
-  let OPERRATIONS = {};
-
   beforeEach(() => {
     cy.visit('/');
-    cy.get('.operations > :nth-child(4)').as(OPERATOR.SUM);
-    cy.get('.operations > :nth-child(3)').as(OPERATOR.SUBSTRACT);
-    cy.get('.operations > :nth-child(2)').as(OPERATOR.MULTIPLY);
-    cy.get('.operations > :nth-child(1)').as(OPERATOR.DIVIDE);
-    cy.get('.operations > :nth-child(5)').as(OPERATOR.ASSIGNMENT);
   });
 
   it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
-    cy.get('.digits > :nth-child(5)').click();
-    cy.get(`@${OPERATOR.SUM}`).click();
-    cy.get('.digits > :nth-child(7)').click();
-    cy.get(`@${OPERATOR.ASSIGNMENT}`).click();
-
-    cy.get('#total').should('have.text', '8');
+    cy.clickDigit(5);
+    cy.clickOperator(OPERATOR.SUM);
+    cy.clickDigit(3);
+    cy.clickOperator(OPERATOR.ASSIGNMENT);
+    cy.getTotal('8');
   });
 
   it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
-    cy.get('.digits > :nth-child(5)').click();
-    cy.get(`@${OPERATOR.SUBSTRACT}`).click();
-    cy.get('.digits > :nth-child(7)').click();
-    cy.get(`@${OPERATOR.ASSIGNMENT}`).click();
-
-    cy.get('#total').should('have.text', '2');
+    cy.clickDigit(5);
+    cy.clickOperator(OPERATOR.SUBSTRACT);
+    cy.clickDigit(3);
+    cy.clickOperator(OPERATOR.ASSIGNMENT);
+    cy.getTotal('2');
   });
 
   it('2개의 숫자에 대해 곱셈이 가능하다', () => {
-    cy.get('.digits > :nth-child(5)').click();
-    cy.get(`@${OPERATOR.MULTIPLY}`).click();
-    cy.get('.digits > :nth-child(7)').click();
-    cy.get(`@${OPERATOR.ASSIGNMENT}`).click();
-
-    cy.get('#total').should('have.text', '15');
+    cy.clickDigit(4);
+    cy.clickOperator(OPERATOR.MULTIPLY);
+    cy.clickDigit(7);
+    cy.clickOperator(OPERATOR.ASSIGNMENT);
+    cy.getTotal('28');
   });
 
   it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
-    cy.get('.digits > :nth-child(5)').click();
-    cy.get(`@${OPERATOR.DIVIDE}`).click();
-    cy.get('.digits > :nth-child(7)').click();
-    cy.get(`@${OPERATOR.ASSIGNMENT}`).click();
+    cy.clickDigit(9);
+    cy.clickOperator(OPERATOR.DIVIDE);
+    cy.clickDigit(3);
+    cy.clickOperator(OPERATOR.ASSIGNMENT);
+    cy.getTotal('3');
+  });
 
-    cy.get('#total').should('have.text', '1');
+  it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+    cy.clickDigit(5);
+    cy.clickOperator(OPERATOR.SUM);
+    cy.clickDigit(3);
+    cy.clickOperator(OPERATOR.ASSIGNMENT);
+    cy.getTotal('8');
+    cy.clickAllClear();
+    cy.getTotal('0');
+  });
+
+  it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
+    cy.clickDigit(5);
+    cy.clickDigit(6);
+    cy.clickDigit(3);
+    cy.clickDigit(1);
+    cy.clickDigit(1);
+    cy.getTotal('563');
+  });
+
+  it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+    cy.clickDigit(9);
+    cy.clickOperator(OPERATOR.DIVIDE);
+    cy.clickDigit(4);
+    cy.clickOperator(OPERATOR.ASSIGNMENT);
+    cy.getTotal('2');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/**
+ * @see https://docs.cypress.io/api/cypress-api/custom-commands#Syntax
+ */
+Cypress.Commands.add('clickDigit', (digit) => {
+  cy.get('.digit').contains(digit).click();
+});
+Cypress.Commands.add('clickOperator', (operator) => {
+  cy.get('.operation').contains(operator).click();
+});
+Cypress.Commands.add('clickAllClear', () => {
+  cy.get('.modifier').click();
+});
+Cypress.Commands.add('getTotal', (total) => {
+  cy.get('#total').should('have.text', total);
+});
