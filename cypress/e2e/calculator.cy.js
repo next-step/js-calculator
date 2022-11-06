89a4af9a22825e354ac2d5cdab4b126e16b25b24
cameo29 <wodls0209@naver.com>
2022-11-07 00:08:33 +0900

    테스트 케이스 추가

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -27,4 +27,46 @@ describe("Calculator", () => {
     cy.contains("=").click();
     cy.get("#total").should("have.text", "113");
   });
+
+  it("2개의 숫자에 대해 뺄셈이 가능하다.", () => {
+    cy.visit(Cypress.config().baseUrl);
+
+    cy.get(".digit").eq(0).click();
+    cy.contains("-").click();
+    cy.get(".digit").eq(7).click();
+    cy.contains("=").click();
+    cy.get("#total").should("have.text", "7");
+  });
+
+  it("2개의 숫자에 대해 곱셈이 가능하다.", () => {
+    cy.visit(Cypress.config().baseUrl);
+
+    cy.get(".digit").eq(1).click();
+    cy.contains("X").click();
+    cy.get(".digit").eq(5).click();
+    cy.contains("=").click();
+    cy.get("#total").should("have.text", "32");
+  });
+
+  it("2개의 숫자에 대해 나눗셈이 가능하다.", () => {
+    cy.visit(Cypress.config().baseUrl);
+
+    cy.get(".digit").eq(6).click();
+    cy.get(".digit").eq(7).click();
+    cy.contains("/").click();
+    cy.get(".digit").eq(1).click();
+    cy.contains("=").click();
+    cy.get("#total").should("have.text", "4");
+  });
+
+  it("계산 결과를 표현할 때 소수점 이하는 버림한다.", () => {
+    cy.visit(Cypress.config().baseUrl);
+
+    cy.get(".digit").eq(6).click();
+    cy.get(".digit").eq(7).click();
+    cy.contains("/").click();
+    cy.get(".digit").eq(4).click();
+    cy.contains("=").click();
+    cy.get("#total").should("have.text", "6");
+  });
 });
