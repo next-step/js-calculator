9923f5f6a1ce4624e5d2c9deedbf72a0369d370e
9603100 <imgoing222@gmail.com>
2022-11-07 00:50:39 +0900

    feat: 2개의 숫자에 대한 덧셈 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,69 @@
-import { $ } from "./utils";
+import { $ } from "./utils.js";
 
 class Calculator {
-	showResult(result) {
-		const total = $("#total");
-		total.innerText = result;
+	result = 0;
+	current = "";
+	operator = null;
+
+	add() {
+		this.result += Number(this.current);
 	}
 
-	setEventListener() {
-		const digits = $(".digits");
-		const operators = $(".operations");
-		const allClearButton = $(".modifiers");
+	substract() {}
 
-		digits.addEventListener("click", (e) =>
-			console.log(e.target.closest(".digit").innerText)
-		);
+	multiply() {}
 
-		operators.addEventListener("click", (e) => {
-			const operator = e.target.closest(".operation").innerText;
-			if (operator === "=") this.showResult(5);
-		});
-
-		allClearButton.addEventListener("click", (e) =>
-			console.log(e.target.innerText)
-		);
-	}
-
-	init() {
-		this.setEventListener();
-	}
+	divide() {}
 }
 
 const calculator = new Calculator();
-calculator.init();
+
+const showResult = () => {
+	const total = $("#total");
+	total.innerText = calculator.result;
+};
+
+const showCurrentNumber = () => {
+	const total = $("#total");
+	total.innerText = calculator.current;
+};
+
+const setEventListener = () => {
+	const digits = $(".digits");
+	const operators = $(".operations");
+	const allClearButton = $(".modifiers");
+
+	digits.addEventListener("click", (e) => {
+		const digit = e.target.closest(".digit").innerText;
+		calculator.current += digit;
+		showCurrentNumber();
+	});
+
+	operators.addEventListener("click", (e) => {
+		const operator = e.target.closest(".operation").innerText;
+
+		if (operator === "=") {
+			switch (calculator.operator) {
+				case "+":
+					calculator.add();
+					break;
+			}
+		} else {
+			calculator.result = Number(calculator.current);
+		}
+
+		calculator.operator = operator;
+		showResult();
+		calculator.current = "";
+	});
+
+	allClearButton.addEventListener("click", (e) =>
+		console.log(e.target.innerText)
+	);
+};
+
+const init = () => {
+	setEventListener();
+};
+
+init();
