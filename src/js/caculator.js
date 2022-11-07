9659c60cac2330b8771a40c81eef278845b8a442
이국신 <53499412+GunBros@users.noreply.github.com>
2022-11-07 21:10:44 +0900

    [클린코드 3기 이국신] 계산기 미션 (#132)
    * feat: 프로젝트 초기 설정
    
    * feat: '=' 버튼에 id 추가
    
    * feat: eslint, prettier 추가 설정
    
    * feat: eslint 추가 설정
    
    * feat: script 태그 위치 변경
    
    * feat: view 모듈 분리
    
    * feat: 상수 모듈 분리
    
    * feat: 숫자 입력 동작 구현
    
    * feat: 초기화 버튼 동작 구현
    
    * feat: 연산자 입력 동작 구현
    
    * feat: 연산 결과 동작 구현
    
    * feat: 각 View에 맞는 이벤트 바인딩

---
+++ src/js/caculator.js
@@ -0,0 +1,71 @@
+import { $total } from "./view.js";
+import { DEFAULT_NUMBER, MAX_DIGIT_LENGTH, operators } from "./constants.js";
+
+export const isValidNumberInput = () => {
+  const expression = $total.innerText;
+
+  const operator = expression.split("").find((el) => Object.keys(operators).includes(el));
+
+  if (!operator) {
+    return expression.length < MAX_DIGIT_LENGTH;
+  }
+
+  const operands = expression.split(operator);
+
+  return operands[1].length < MAX_DIGIT_LENGTH;
+};
+
+export const inputNumber = ({ target }) => {
+  if ($total.innerText === DEFAULT_NUMBER) {
+    $total.innerText = target.innerText;
+    return;
+  }
+
+  if (!isValidNumberInput()) {
+    alert("숫자는 3자리이상 입력할 수 없습니다.");
+    return;
+  }
+
+  $total.innerText += target.innerText;
+};
+
+export const clearNumber = () => {
+  $total.innerText = DEFAULT_NUMBER;
+};
+
+export const inputOperator = ({ target }) => {
+  const expression = $total.innerText;
+  const selectedOperator = target.innerText;
+
+  if (selectedOperator === "=") {
+    return;
+  }
+
+  const existingOperator = expression.split("").find((el) => Object.keys(operators).includes(el));
+
+  if (existingOperator || expression === DEFAULT_NUMBER) {
+    alert("잘못된 입력입니다.");
+    return;
+  }
+
+  $total.innerText += selectedOperator;
+};
+
+export const setResult = () => {
+  const expression = $total.innerText;
+
+  const operator = expression.split("").find((el) => Object.keys(operators).includes(el));
+
+  if (!operator) {
+    return;
+  }
+
+  const [num1, num2] = expression.split(operator);
+
+  if (!num2) {
+    $total.innerText = num1;
+    return;
+  }
+
+  $total.innerText = operators[operator](Number(num1), Number(num2));
+};
