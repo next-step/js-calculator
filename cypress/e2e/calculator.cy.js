c2fddfd5a06d87fd5d960d306309f213ebab3b97
semin <awesummer.1021@gmail.com>
2022-11-08 00:54:15 +0900

    feat : 상세 기능 구현

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,24 +1,99 @@
 import Calculator from '../../src/js/calculator.js';
 
-describe('계산기 테스트', () => {
-  const calculator = new Calculator();
+describe('계산기 기본 기능', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
   it('더하기', () => {
-    expect(calculator.sum(1, 2)).equal(3);
+    cy.contains('.digit', '1').click();
+    cy.contains('.operation', '+').click();
+    cy.contains('.digit', '1').click();
+    cy.contains('.operation', '=').click();
+    cy.get('#total').should('have.text', '2');
   });
 
   it('빼기', () => {
-    expect(calculator.subtract(1, 2)).equal(-1);
-    expect(calculator.subtract(2, 2)).equal(0);
+    cy.contains('.digit', '1').click();
+    cy.contains('.operation', '-').click();
+    cy.contains('.digit', '1').click();
+    cy.contains('.operation', '=').click();
+    cy.get('#total').should('have.text', '0');
   });
 
+  it('빼기 (음수 결과)', () => {
+    cy.contains('.digit', '1').click();
+    cy.contains('.operation', '-').click();
+    cy.contains('.digit', '3').click();
+    cy.contains('.operation', '=').click();
+    cy.get('#total').should('have.text', '-2');
+  });
   it('곱하기', () => {
-    expect(calculator.multiple(1, 2)).equal(2);
-    expect(calculator.multiple(-1, 2)).equal(-2);
+    cy.contains('.digit', '1').click();
+    cy.contains('.digit', '0').click();
+    cy.contains('.operation', 'X').click();
+    cy.contains('.digit', '2').click();
+    cy.contains('.operation', '=').click();
+    cy.get('#total').should('have.text', '20');
   });
 
   it('나누기', () => {
-    expect(calculator.divide(1, 2)).equal(0.5);
-    expect(calculator.divide(4, 2)).equal(2);
+    cy.contains('.digit', '4').click();
+    cy.contains('.operation', '/').click();
+    cy.contains('.digit', '2').click();
+    cy.contains('.operation', '=').click();
+    cy.get('#total').should('have.text', '2');
+  });
+
+  it('나누기 (소수점 버림)', () => {
+    cy.contains('.digit', '7').click();
+    cy.contains('.operation', '/').click();
+    cy.contains('.digit', '2').click();
+    cy.contains('.operation', '=').click();
+    cy.get('#total').should('have.text', '3');
+  });
+
+  it('AC 버튼 클릭 시 값 초기화', () => {
+    cy.get('#total').invoke('text', '100');
+    cy.contains('AC').click();
+    cy.get('#total').should('have.text', '0');
+  });
+});
+
+describe('숫자 입력 기능 테스트', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('최대 3자리 수까지 입력, 첫 번째 수의 경우', () => {
+    cy.contains('.digit', '1').click();
+    cy.contains('.digit', '0').click();
+    cy.contains('.digit', '9').click();
+    cy.contains('.digit', '8').click();
+    cy.get('#total').should('have.text', '109');
+  });
+
+  it('최대 3자리 수까지 입력, 두 번째 수의 경우', () => {
+    cy.contains('.operation', '+').click();
+    cy.contains('.digit', '2').click();
+    cy.contains('.digit', '9').click();
+    cy.contains('.digit', '8').click();
+    cy.contains('.digit', '1').click();
+    cy.get('#total').should('have.text', '298');
+  });
+
+  it('0 중복 입력 방지, 첫 번째 수의 경우', () => {
+    cy.contains('.digit', '0').click();
+    cy.contains('.digit', '0').click();
+    cy.contains('.digit', '1').click();
+    cy.get('#total').should('have.text', '1');
+  });
+
+  it('0 중복 입력 방지, 두 번째 수의 경우', () => {
+    cy.contains('.operation', '+').click();
+    cy.contains('.digit', '0').click();
+    cy.contains('.digit', '0').click();
+    cy.contains('.digit', '2').click();
+    cy.get('#total').should('have.text', '2');
   });
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,17 +1,109 @@
 export default class Calculator {
+  constructor() {
+    this.prev = '';
+    this.current = '';
+    this.operation = null;
+    this.isOperating = false;
+    this.total = document.getElementById('total');
+
+    this.setEventHandler();
+  }
+
   sum(num1, num2) {
-    return num1 + num2;
+    return +num1 + +num2;
   }
 
   subtract(num1, num2) {
-    return num1 - num2;
+    return +num1 - +num2;
   }
 
   multiple(num1, num2) {
-    return num1 * num2;
+    return +num1 * +num2;
   }
 
   divide(num1, num2) {
-    return num1 / num2;
+    const res = +num1 / +num2;
+    return Math.floor(res);
+  }
+
+  setEventHandler() {
+    const digits = document.querySelector('.digits');
+    const modifiers = document.querySelector('.modifiers');
+    const operations = document.querySelector('.operations');
+
+    digits?.addEventListener('click', (e) => this.setDigit(e));
+    modifiers?.addEventListener('click', () => this.reset());
+    operations?.addEventListener('click', (e) => this.setOperator(e));
+  }
+
+  setDigit(e) {
+    const value = e.target.innerHTML;
+
+    if ('0' === value && !this.prev) {
+      return;
+    }
+
+    if (!this.isOperating && this.prev?.length < 3) {
+      this.prev += value;
+      this.total.innerHTML = this.prev;
+
+      return;
+    }
+
+    if ('0' === value && '0' === this.current) {
+      return;
+    }
+
+    if (this.isOperating && this.current?.length < 3) {
+      this.current += value;
+      this.total.innerHTML = this.current;
+    }
+  }
+
+  setOperator(e) {
+    const value = e.target.innerHTML;
+    let result;
+
+    if ('=' !== value) {
+      this.isOperating = true;
+      this.operation = value;
+
+      return;
+    }
+
+    switch (this.operation) {
+    case '+':
+      result = this.sum(this.prev, this.current);
+      break;
+
+    case '-':
+      result = this.subtract(this.prev, this.current);
+      break;
+
+    case 'X':
+      result = this.multiple(this.prev, this.current);
+      break;
+
+    case '/':
+      result = this.divide(this.prev, this.current);
+      break;
+
+    default:
+      return;
+    }
+
+    this.prev = '';
+    this.current = '';
+    this.operation = null;
+    this.isOperating = false;
+    this.total.innerHTML = String(result);
+  }
+
+  reset() {
+    this.prev = '';
+    this.current = '';
+    this.operation = null;
+    this.isOperating = false;
+    this.total.innerHTML = '0';
   }
 }
