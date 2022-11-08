898bfe48261ed917aac7f3c8d66c8ecad5e40837
hyeonbang <hyeonkyeong.bang815@gmail.com>
2022-11-08 22:59:24 +0900

    refactor :: view와 function을 분리

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,27 +1,16 @@
 export class Calculator {
-    constructor({ total, modifiers, operations, digits }) {
+    constructor(total) {
         this.prevDigit = 0;
         this.result = [];
-        this.operatorType = ['+', '-', 'X', '/'];
         this.resultSign = '=';
         this.total = total;
-        this.modifiers = modifiers;
-        this.operations = operations;
-        this.digits = digits;
-        this.setEventListeners();
-
         this.operation = {
             '+': (a, b) => a + b,
             '-': (a, b) => a - b,
             'X': (a, b) => a * b,
             '/': (a, b) => Math.floor(a / b),
         };
-    }
-
-    setEventListeners() {
-        this.modifiers.addEventListener('click', () => this.reset());
-        this.digits.addEventListener('click', e => this.insertDigit(e));
-        this.operations.addEventListener('click', e => this.insertOperator(e));
+        this.operatorType = Object.keys(this.operation);
     }
 
     reset() {
@@ -31,36 +20,30 @@ export class Calculator {
     }
 
     insertDigit(e) {
-        if (!this.prevDigit) this.total.innerText = 0;
+        if (!this.prevDigit) this.changeResult(0);
         if (this.prevDigit.toString().length > 2) return alert('숫자는 한번에 최대 3자리 수까지 입력할 수 있습니다.');
 
-        this.changeResult(Number(this.total.innerText+ e.target.innerText))
+        this.changeResult(Number(this.total.innerText + e.target.innerText))
         this.prevDigit = this.total.innerText;
     }
 
     insertOperator(e) {
         this.result.push(+this.prevDigit);
-
+        if (e.target.innerText === this.resultSign) return this.calculate();
         // 바로 직전 입력값이 연산자라면 해당 연산자를 제거하고 현재 입력한 연산자로 교체함
         if (this.operatorType.includes(this.result[this.result.length - 1])) this.result.pop();
-        if (e.target.innerText === this.resultSign) return this.calculate();
         this.result.push(e.target.innerText);
         this.prevDigit = 0;
     }
 
     calculate() {
-        this.validateOperator();
-        this.validateDigit();
+        if (!this.validateOperator()) return alert('연산을 입력해주세요.');
         this.prevDigit = this.changeResult(this.operate());
         this.result = [];
     }
 
-    validateOperator() {
-        if (!this.result.some(row => this.operatorType.includes(row))) return alert('연산을 입력해주세요.');
-    }
-
-    validateDigit() {
-        if (this.result.filter(row => !this.operatorType.includes(row)).length < 2) return alert('비교 대상 값을 입력해주세요.');
+    validateOperator = () => {
+        return this.result.some(row => this.operatorType.includes(row));
     }
 
     operate = () => {
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,19 @@
 import { Calculator } from "./calculator.js";
 
 window.onload = () => {
-    const calculator = new Calculator({
+    const $selectors = {
         total: document.querySelector('#total'),
         modifiers: document.querySelector('.modifiers'),
         operations: document.querySelector('.operations'),
         digits: document.querySelector('.digits')
-    });
+    }
+    const calculator = new Calculator($selectors.total);
+
+    function setEventListeners() {
+        $selectors.modifiers.addEventListener('click', () => calculator.reset());
+        $selectors.digits.addEventListener('click', e => calculator.insertDigit(e));
+        $selectors.operations.addEventListener('click', e => calculator.insertOperator(e));
+    }
+
+    setEventListeners();
 };
