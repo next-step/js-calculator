77a00f0b78b574db2e19b2c0660d769480f8cb9a
pyuung <kyuungah@gmail.com>
2022-11-09 12:28:08 +0900

    feat: :sparkles: 사칙연산 테스트 작성

--- cypress/e2e/1-calculate.cy.js
+++ cypress/e2e/1-calculate.cy.js
@@ -2,8 +2,40 @@ describe('계산기 테스트', () => {
   it('visit', () => {
     cy.visit('http://localhost:9000/')
   })
-  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {})
-  it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {})
-  it('2개의 숫자에 대해 곱셈이 가능하다.', () => {})
-  it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {})
+  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+    cy.get('#modifier').click()
+    cy.get('#num-0').click()
+    cy.get('#add').click()
+    cy.get('#num-1').click()
+    cy.get('#operate').click()
+
+    cy.get('#total').contains(1)
+  })
+  it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+    cy.get('#modifier').click()
+    cy.get('#num-0').click()
+    cy.get('#subtract').click()
+    cy.get('#num-1').click()
+    cy.get('#operate').click()
+
+    cy.get('#total').contains(-1)
+  })
+  it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+    cy.get('#modifier').click()
+    cy.get('#num-0').click()
+    cy.get('#multiplication').click()
+    cy.get('#num-1').click()
+    cy.get('#operate').click()
+
+    cy.get('#total').contains(0)
+  })
+  it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+    cy.get('#modifier').click()
+    cy.get('#num-4').click()
+    cy.get('#division').click()
+    cy.get('#num-2').click()
+    cy.get('#operate').click()
+
+    cy.get('#total').contains(2)
+  })
 })
