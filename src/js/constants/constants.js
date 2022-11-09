0cb2d8c657bf84db4bf9faa98579bd969ce45d46
Geuni <dlrmsgnl620@naver.com>
2022-11-10 00:02:59 +0900

    Add: Operator AC 추가

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -2,16 +2,14 @@ export const MAX_DIGIT_NUMBER = 3;
 
 export const MESSAGE = {
   INVALID_NUMBER_SIZE: `숫자는 한번에 최대 ${MAX_DIGIT_NUMBER}자리 수까지 입력 가능합니다!`,
+  ENTER_NUMBER_BEFORE_OPERATOR: `숫자부터 입력해주세요!`,
 };
 
-/** @todo:
- * 가령 연산자가 20개쯤 된다면?
- * 연산자인지 검증하는 로직이 있다면?
- */
 export const OPERATOR = {
   PLUS: "+",
   MINUS: "-",
   MULTIPLICATION: "X",
   DIVISION: "/",
   EQUALS: "=",
+  AC: "AC",
 };
