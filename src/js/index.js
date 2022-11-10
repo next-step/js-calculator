f2cfc87eeb6a229b1e5df4d5ed84283b08b797b7
keunnaLee <keunnalee@gmail.com>
2022-11-10 18:23:19 +0900

    feat: add event listeners to each buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 const calculator = document.querySelectorAll(".calculator");
 const total = document.getElementById("total");
+
+// Initial Setting
+calculator.forEach((button) => {
+    button.addEventListener("click", handleClickBtn);
+});
+
+// Functions
+function handleClickBtn(event) {
+  }
