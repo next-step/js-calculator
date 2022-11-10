b6c2e3eade32486ca156646f4973d4d05cdba2cf
Dongmin Ahn <eamon3481@naver.com>
2022-11-10 14:05:44 +0900

    feat : add calculate logic

---
+++ src/js/calculate.js
@@ -0,0 +1,15 @@
+export default class Calculate {
+ plus(num1, num2) {
+  return num1 + num2;
+ }
+
+ minus(num1, num2) {
+  return num1 - num2;
+ }
+ multiple(num1, num2) {
+  return num1 * num2;
+ }
+ divide(num1, num2) {
+  return num1 / num2;
+ }
+}
