1a8f502510e33319f9674ea0b2c3553e5a516986
Dongmin Ahn <eamon3481@naver.com>
2022-11-12 00:33:15 +0900

    test : add test about AC button

--- cypress/e2e/calculate.cy.js
+++ cypress/e2e/calculate.cy.js
@@ -9,30 +9,35 @@
 describe('숫자를 입력하면 화면에 보인다.', () => {
  beforeEach(() => {
   cy.visit('http://localhost:3000');
+
+  cy.get('.digit').contains('7').click();
+
+  cy.get('.digit').contains('8').click();
+
+  cy.get('.digit').contains('9').click();
  });
+
  it('숫자 버튼들이 존재한다.', () => {
   cy.get('.digit').should('have.length', 10);
  });
 
  it('숫자 버튼을 연속해서 클릭하면 숫자가 입력된다.', () => {
-  cy.get('.digit').contains('7').click();
-
-  cy.get('.digit').contains('8').click();
-
-  cy.get('.digit').contains('9').click();
-
   cy.get('#total').contains('789');
  });
 
  it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
-  cy.get('.digit').contains('7').click();
-
-  cy.get('.digit').contains('8').click();
-
-  cy.get('.digit').contains('9').click();
-
   cy.get('.digit').contains('0').click();
 
   cy.get('#total').should('have.text', '789');
+ });
+
+ it('AC(All Clear)버튼이 존재한다.', () => {
+  cy.get('.modifier').should('exist');
+ });
+
+ it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+  cy.get('.modifier').click();
+
+  cy.get('#total').should('have.text', '0');
  });
 });
