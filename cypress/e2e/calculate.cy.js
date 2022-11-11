25641e8744cfbc118fee2a9b8de7b38a7da5ba3e
Dongmin Ahn <eamon3481@naver.com>
2022-11-12 00:25:49 +0900

    test: add calculator number test

--- cypress/e2e/calculate.cy.js
+++ cypress/e2e/calculate.cy.js
@@ -1,17 +1,38 @@
-import Calculate from '../../src/js/calculate';
+// - [ ] 2개의 숫자에 대해 덧셈이 가능하다.
+// - [ ] 2개의 숫자에 대해 뺄셈이 가능하다.
+// - [ ] 2개의 숫자에 대해 곱셈이 가능하다.
+// - [ ] 2개의 숫자에 대해 나눗셈이 가능하다.
+// - [ ] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
+// - [ ] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
+// - [ ] 계산 결과를 표현할 때 소수점 이하는 버림한다.
 
-describe('사칙 연산 테스트', () => {
- const calculate = new Calculate();
- it('plus', () => {
-  expect(calculate.plus(1, 1)).to.equal(2);
+describe('숫자를 입력하면 화면에 보인다.', () => {
+ beforeEach(() => {
+  cy.visit('http://localhost:3000');
  });
- it('minus', () => {
-  expect(calculate.minus(2, 1)).to.equal(1);
+ it('숫자 버튼들이 존재한다.', () => {
+  cy.get('.digit').should('have.length', 10);
  });
- it('multiple', () => {
-  expect(calculate.multiple(3, 3)).to.equal(9);
+
+ it('숫자 버튼을 연속해서 클릭하면 숫자가 입력된다.', () => {
+  cy.get('.digit').contains('7').click();
+
+  cy.get('.digit').contains('8').click();
+
+  cy.get('.digit').contains('9').click();
+
+  cy.get('#total').contains('789');
  });
- it('divide', () => {
-  expect(calculate.divide(28, 2)).to.equal(14);
+
+ it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
+  cy.get('.digit').contains('7').click();
+
+  cy.get('.digit').contains('8').click();
+
+  cy.get('.digit').contains('9').click();
+
+  cy.get('.digit').contains('0').click();
+
+  cy.get('#total').should('have.text', '789');
  });
 });
