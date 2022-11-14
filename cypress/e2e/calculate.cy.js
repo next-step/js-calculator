b44daca6ed374df2552c1f1b548ac002c1fa567a
Dongmin Ahn <eamon3481@naver.com>
2022-11-14 17:06:08 +0900

    refactor : use Cypress.Commands

--- cypress/e2e/calculate.cy.js
+++ cypress/e2e/calculate.cy.js
@@ -1,20 +1,18 @@
-// - [ ] 2개의 숫자에 대해 덧셈이 가능하다.
-// - [ ] 2개의 숫자에 대해 뺄셈이 가능하다.
-// - [ ] 2개의 숫자에 대해 곱셈이 가능하다.
-// - [ ] 2개의 숫자에 대해 나눗셈이 가능하다.
-// - [ ] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
-// - [ ] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
-// - [ ] 계산 결과를 표현할 때 소수점 이하는 버림한다.
+Cypress.Commands.add('typeNumber', (numbers) => {
+ for (const number of numbers) {
+  cy.get('.digit').contains(number).click();
+ }
+});
+
+Cypress.Commands.add('operate', (operator, nextNumber) => {
+ cy.get('.operation').contains(operator).click();
+ cy.typeNumber(nextNumber);
+});
 
 describe('숫자를 입력하면 화면에 보인다.', () => {
  beforeEach(() => {
   cy.visit('http://localhost:3000');
-
-  cy.get('.digit').contains('7').click();
-
-  cy.get('.digit').contains('8').click();
-
-  cy.get('.digit').contains('9').click();
+  cy.typeNumber(['7', '8', '9']);
  });
 
  it('숫자 버튼들이 존재한다.', () => {
@@ -46,20 +44,15 @@ describe('2개의 숫자에 대해 뺄셈이 가능하다', () => {
  beforeEach(() => {
   cy.visit('http://localhost:3000');
 
-  cy.get('.digit').contains('7').click();
-
-  cy.get('.digit').contains('8').click();
+  cy.typeNumber(['7', '8']);
+  cy.operate('-', ['2']);
  });
 
  it('뺄셈 연산 버튼을 누르고 숫자를 눌렀을 때는 새롭게 숫자를 입력한다.', () => {
-  cy.get('.operation').contains('-').click();
-  cy.get('.digit').contains('2').click();
   cy.get('#total').should('have.text', '2');
  });
 
  it('등호 연산버튼을 눌렀을 때 계산 결과가 나와야한다.', () => {
-  cy.get('.operation').contains('-').click();
-  cy.get('.digit').contains('2').click();
   cy.get('.operation').contains('=').click();
   cy.get('#total').should('have.text', '76');
  });
@@ -69,20 +62,15 @@ describe('2개의 숫자에 대해 곱셈이 가능하다', () => {
  beforeEach(() => {
   cy.visit('http://localhost:3000');
 
-  cy.get('.digit').contains('7').click();
-
-  cy.get('.digit').contains('8').click();
+  cy.typeNumber(['7', '8']);
+  cy.operate('X', ['2']);
  });
 
  it('곱셈 연산 버튼을 누르고 숫자를 눌렀을 때는 새롭게 숫자를 입력한다.', () => {
-  cy.get('.operation').contains('X').click();
-  cy.get('.digit').contains('2').click();
   cy.get('#total').should('have.text', '2');
  });
 
  it('등호 연산버튼을 눌렀을 때 곱셈 계산 결과가 나와야한다.', () => {
-  cy.get('.operation').contains('X').click();
-  cy.get('.digit').contains('2').click();
   cy.get('.operation').contains('=').click();
   cy.get('#total').should('have.text', '156');
  });
@@ -91,28 +79,22 @@ describe('2개의 숫자에 대해 곱셈이 가능하다', () => {
 describe('2개의 숫자에 대해 나누셈이 가능하다', () => {
  beforeEach(() => {
   cy.visit('http://localhost:3000');
-
-  cy.get('.digit').contains('7').click();
-
-  cy.get('.digit').contains('8').click();
+  cy.typeNumber(['7', '8']);
  });
 
  it('나누셈 연산 버튼을 누르고 숫자를 눌렀을 때는 새롭게 숫자를 입력한다.', () => {
-  cy.get('.operation').contains('/').click();
-  cy.get('.digit').contains('2').click();
+  cy.operate('/', ['2']);
   cy.get('#total').should('have.text', '2');
  });
 
  it('등호 연산버튼을 눌렀을 때 계산 결과가 나와야한다.', () => {
-  cy.get('.operation').contains('/').click();
-  cy.get('.digit').contains('2').click();
+  cy.operate('/', ['2']);
   cy.get('.operation').contains('=').click();
   cy.get('#total').should('have.text', '39');
  });
 
  it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
-  cy.get('.operation').contains('/').click();
-  cy.get('.digit').contains('5').click();
+  cy.operate('/', ['5']);
   cy.get('.operation').contains('=').click();
   cy.get('#total').should('have.text', '15');
  });
