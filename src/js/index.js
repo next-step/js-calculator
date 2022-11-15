a82d3a342abe18ed90aceb1ad71eab56b4c56900
publee <publizm@gmail.com>
2022-11-16 08:49:03 +0900

    refactor: 불필요한 onload 호출 제거

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,24 @@
 import Ui from "./ui.js";
 
-window.onload = () => {
-  const digits = document.querySelectorAll(".digit");
-  const operators = document.querySelectorAll(".operation");
-  const modifier = document.querySelector(".modifier");
-  const total = document.querySelector("#total");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operation");
+const modifier = document.querySelector(".modifier");
+const total = document.querySelector("#total");
 
-  const ui = new Ui(total);
+const ui = new Ui(total);
 
-  digits.forEach((digit) => {
-    digit.addEventListener("click", () => {
-      ui.onClickDigit(digit.innerText);
-    });
+digits.forEach((digit) => {
+  digit.addEventListener("click", () => {
+    ui.onClickDigit(digit.innerText);
   });
+});
 
-  operators.forEach((operator) => {
-    operator.addEventListener("click", () => {
-      ui.onClickOperator(operator.innerText);
-    });
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    ui.onClickOperator(operator.innerText);
   });
+});
 
-  modifier.addEventListener("click", () => {
-    ui.initialize();
-  });
-};
+modifier.addEventListener("click", () => {
+  ui.initialize();
+});
