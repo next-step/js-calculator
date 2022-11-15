5808bce0826801a18547a71dd180b31d245d757f
publee <publizm@gmail.com>
2022-11-16 07:51:32 +0900

    docs: prettier 적용 안됨 > eslint.js로 확장자 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
-{
-  "env": {
-    "browser": true,
-    "es2022": true,
-    "node": true
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
   },
-  "parserOptions": {
-    "ecmaVersion": 2021,
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
   },
-  "extends": ["airbnb", "plugin:prettier/recommended", "plugin:cypress/recommended"],
-  "rules": {
-    "import/extensions": [
-      "off"
-    ],
+  extends: [
+    "airbnb",
+    "plugin:prettier/recommended",
+    "plugin:cypress/recommended",
+  ],
+  rules: {
+    "import/extensions": ["off"],
     "max-depth": ["error", 2],
-    "lines-between-class-members": "off"
-  }
-}
+    "lines-between-class-members": "off",
+  },
+};
